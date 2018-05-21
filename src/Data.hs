e8070b1cf08f74864097f9b9293523939048a46b
u2zv1wx <u2zv1wx@protonmail.ch>
2018-05-22 00:26:55 +0900

    Macro expansion (sketch)

--- src/Data.hs
+++ src/Data.hs
@@ -5,11 +5,14 @@ import Control.Monad.Identity
 import Control.Monad.State
 import Control.Monad.Trans.Except
 
+import Data.Maybe (fromMaybe)
+
 import qualified Text.Show.Pretty as Pr
 
 data Symbol
   = S String
   | Hole
+  | Dots
   deriving (Show, Eq)
 
 -- positive symbol
@@ -215,48 +218,83 @@ data ConsDef = ConsDef
   , consCod :: Type
   } deriving (Show)
 
--- strong-form ::= _ | (strong-form weak-form1 ... weak-formn)
-data StrongForm
-  = StrongFormHole
-  | StrongFormApp StrongForm
-                  [WeakForm]
-  deriving (Show)
-
--- weak-form ::= _ | x | (weak-form weak-form1 .... weak-formn)
 data WeakForm
   = WeakFormHole
-  | WeakFormSymbol Symbol
+  | WeakFormSymbol String
   | WeakFormApp WeakForm
                 [WeakForm]
   deriving (Show)
 
 -- form ::= strong-form | (strong-form weak-form1 ... weak-formn)
-data Form =
-  Form StrongForm
-       [WeakForm]
+data Form
+  = Form String
+         [WeakForm]
+  | FormWithRest String
+                 [WeakForm]
   deriving (Show)
 
--- v-notation ::= (notation name form body+)
-data VNotation = VNotation
-  { vNotationName :: Symbol
-  , vNotationForm :: Form
-  , vNotationBody :: V
-  } deriving (Show)
+unifyFormV :: Form -> V -> Either String ([(String, Term)], Maybe [Term])
+unifyFormV (Form s ws) (APP (VPosSym (PosSym (S s'))) xs)
+  | s /= s' = Left $ "Cannot match " ++ show s ++ " with " ++ show s'
+  | length ws /= length xs =
+    Left $ "Cannot match " ++ show ws ++ " with " ++ show xs
+  | otherwise = do
+    ys <- zipWithM unifyEWeakForm ws xs
+    return (concat ys, Nothing)
+unifyFormV (FormWithRest s ws) (APP (VPosSym (PosSym (S s'))) xs)
+  | s /= s' = Left $ "Cannot match " ++ show s ++ " with " ++ show s'
+  | length ws > length xs =
+    Left $ "Cannot match " ++ show ws ++ " with " ++ show xs
+  | otherwise = do
+    ys <- zipWithM unifyEWeakForm ws xs
+    let rest = drop (length ws) xs
+    return (concat ys, Just (map NegTerm rest))
+unifyFormV f v = Left $ "Cannot match " ++ show f ++ " with " ++ show v
 
--- e-notation ::= (notation name form body-)
-data ENotation = ENotation
-  { eNotationName :: Symbol
-  , eNotationForm :: Form
-  , eNotationBody :: E
-  } deriving (Show)
+unifyFormE :: Form -> E -> Either String ([(String, Term)], Maybe [Term])
+unifyFormE (Form s ws) (App (ENegSym (NegSym (S s'))) xs)
+  | s /= s' = Left $ "Cannot match " ++ show s ++ " with " ++ show s'
+  | length ws /= length xs =
+    Left $ "Cannot match " ++ show ws ++ " with " ++ show xs
+  | otherwise = do
+    ys <- zipWithM unifyVWeakForm ws xs
+    return (concat ys, Nothing)
+unifyFormE (FormWithRest s ws) (App (ENegSym (NegSym (S s'))) xs)
+  | s /= s' = Left $ "Cannot match " ++ show s ++ " with " ++ show s'
+  | length ws > length xs =
+    Left $ "Cannot match " ++ show ws ++ " with " ++ show xs
+  | otherwise = do
+    ys <- zipWithM unifyVWeakForm ws xs
+    let rest = drop (length ws) xs
+    return (concat ys, Just (map PosTerm rest))
+unifyFormE f v = Left $ "Cannot match " ++ show f ++ " with " ++ show v
+
+unifyVWeakForm :: WeakForm -> V -> Either String [(String, Term)]
+unifyVWeakForm WeakFormHole _ = return []
+unifyVWeakForm (WeakFormSymbol s) v = return [(s, PosTerm v)]
+unifyVWeakForm (WeakFormApp s args) (APP v es) = do
+  subst1 <- unifyVWeakForm s v
+  subst2 <- zipWithM unifyEWeakForm args es
+  return $ subst1 ++ concat subst2
+unifyVWeakForm f v = Left $ "Cannot match " ++ show f ++ " against " ++ show v
+
+unifyEWeakForm :: WeakForm -> E -> Either String [(String, Term)]
+unifyEWeakForm WeakFormHole _ = return []
+unifyEWeakForm (WeakFormSymbol s) e = return [(s, NegTerm e)]
+unifyEWeakForm (WeakFormApp s args) (App e vs) = do
+  subst1 <- unifyEWeakForm s e
+  subst2 <- zipWithM unifyVWeakForm args vs
+  return $ subst1 ++ concat subst2
+unifyEWeakForm f v = Left $ "Cannot match " ++ show f ++ " against " ++ show v
 
 data Env = Env
   { i :: Int
   , valueTypeEnv :: [ValueDef]
   , compTypeEnv :: [CompDef]
   , consEnv :: [ConsDef]
-  , vNotationEnv :: [VNotation]
-  , eNotationEnv :: [ENotation]
+  , vNotationEnv :: [(String, Form, V)]
+  , eNotationEnv :: [(String, Form, E)]
+  , reservedEnv :: [String]
   } deriving (Show)
 
 initialEnv :: Env
@@ -268,6 +306,7 @@ initialEnv =
     , consEnv = []
     , vNotationEnv = []
     , eNotationEnv = []
+    , reservedEnv = []
     }
 
 type WithEnv a = StateT Env (ExceptT String IO) a
