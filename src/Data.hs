07ee840480d96c7a268e22d22cca0f71a5f0e413
u2zv1wx <u2zv1wx@protonmail.ch>
2018-07-21 17:58:10 +0900

    update

--- src/Data.hs
+++ src/Data.hs
@@ -1,314 +1,189 @@
 module Data where
 
-import Control.Monad.Except
-import Control.Monad.Identity
-import Control.Monad.State
-import Control.Monad.Trans.Except
+import           Control.Monad.Except
+import           Control.Monad.Identity
+import           Control.Monad.State
+import           Control.Monad.Trans.Except
 
-import Data.Maybe (fromMaybe)
+import           Data.Maybe                 (fromMaybe)
 
-import qualified Text.Show.Pretty as Pr
+import qualified Text.Show.Pretty           as Pr
 
-data Symbol
-  = S String
-  | Hole
-  | Dots
-  deriving (Show, Eq)
+type Symbol = String
 
--- positive symbol
--- x+ ::= symbol+
---      | (ascribe x+ P)
-data PosSym
-  = PosSym Symbol
-  | PosSymAsc PosSym
-              PosType
-  deriving (Show)
-
--- negative symbol
--- x- ::= symbol-
---      | (ascribe x- N)
-data NegSym
-  = NegSym Symbol
-  | NegSymAsc NegSym
-              NegType
-  deriving (Show)
-
--- type
--- T ::= P
---     | N
-data Type
-  = PosType PosType
-  | NegType NegType
-  deriving (Show)
-
--- positive type
--- P ::= p
---     | {defined value}
---     | {inverted form of defined computation}
---     | (FORALL ((x1- N1) ... (xn- Nn)) P)
---     | (SWITCH x- N (r1 P1) ... (rn Pn))
---     | (closure N)
---     | positive
-data PosType
-  = PosTypeSym PosSym
-  | ValueApp ValueDef
-             [Type]
-  | CoCompApp CompDef
-              [Type]
-  | FORALL [(NegSym, NegType)]
-           PosType
-  | SWITCH NegSym
-           NegType
-           [(Refutation, PosType)]
-  | Closure NegType
-  | PosUniv
-  deriving (Show)
-
--- negative type
--- N ::= n
---     | {defined computation type}
---     | {inverted form of defined value}
---     | (forall ((x1+ P1) ... (xn+ Pn)) N)
---     | (switch x+ P (a1 N1) ... (an Nn))
---     | (CLOSURE P)
---     | negative
-data NegType
-  = NegTypeSym NegSym
-  | CompApp CompDef
-            [Type]
-  | CoValueApp ValueDef
-               [Type]
-  | ForAll [(PosSym, PosType)]
-           NegType
-  | Switch PosSym
-           PosType
-           [(Assertion, NegType)]
-  | CLOSURE PosType
-  | NegUniv
-  deriving (Show)
-
--- program
--- z ::= (thread t1) ... (thread tn)
-newtype Program =
-  Thread [Term]
-  deriving (Show)
-
--- term
--- t ::= v | e
-data Term
-  = PosTerm V
-  | NegTerm E
-  deriving (Show)
+type Level = Int
 
 -- positive term
 -- v ::= x
---     | {pattern}
---     | {pattern application}
---     | (LAMBDA (x1 ... xn) v)
---     | (v e1 ... en)
---     | (ZETA x (r1 v1) ... (rn vn))
---     | (ELIM e v)
---     | (thunk e)
---     | (FORCE e)
 --     | (quote e)
+--     | {defined constant} <- such as nat, succ, etc.
+--     | (v v)
 --     | (ascribe v P)
-data V
-  = VPosSym PosSym
-  | VConsApp ConsDef
-             [Term]
-  | LAM [NegSym]
-        V
-  | APP V
-        [E]
-  | ZETA NegSym
-         [(Refutation, V)]
-  | APPZETA E
-            V
-  | Thunk E
-  | FORCE E
-  | VAsc V
-         PosType
-  deriving (Show)
+data Value
+  = Var Symbol
+  | Quote Comp
+  | Atomic ConsDef
+  | AtomicApp Value
+              Value
+  | Ascribe Value
+            P
+  deriving (Show, Eq)
 
 -- negative term
--- e ::= x
---     | {pattern}
---     | (lambda (x1 ... xn) e)
---     | (e v1 ... vn)
---     | (zeta x (a1 e1) ... (an en))
---     | (elim v e)
---     | (THUNK v)
---     | (force v)
+-- e ::= (lambda x e)
+--     | (e v)
+--     | (return v)
+--     | (bind x e1 e2)
 --     | (unquote v)
+--     | (send x e)
+--     | (receive x e)
+--     | (dispatch e1 ... en)
+--     | (select i e)
+--     | (mu x e)
+--     | (case e (a1 e1) ... (an en))
 --     | (ascribe e N)
-data E
-  = ENegSym NegSym
-  | EConsApp ConsDef
-             [Term]
-  | Lam [PosSym]
-        E
-  | App E
-        [V]
-  | Zeta PosSym
-         [(Assertion, E)]
-  | AppZeta V
-            E
-  | THUNK V
-  | Force V
-  | EAsc E
-         NegType
-  deriving (Show)
+data Comp
+  = Lam Symbol
+        Comp
+  | App Comp
+        Value
+  | Ret Value
+  | Bind Symbol
+         Comp
+         Comp
+  | Unquote Value
+  | Send Symbol
+         Comp
+  | Receive Symbol
+            Comp
+  | Dispatch [Comp]
+  | Select Int
+           Comp
+  | Mu Symbol
+       Comp
+  | Case Comp
+         [(Value, Comp)]
+  deriving (Show, Eq)
 
--- pattern ::= a | r
-data Pat
-  = PosPat Assertion
-  | NegPat Refutation
-  deriving (Show)
+-- positive type
+-- P ::= p
+--     | (down N)
+--     | {defined constant type}
+--     | (constructor (x P) P)
+--     | (universe i)
+data P
+  = PVar Symbol
+  | Down N
+  | ConsAtom Symbol
+  | ConsImp Symbol
+            P
+            P
+  | Universe Level
+  deriving (Show, Eq)
 
--- assertion pattern
--- a ::= x
---     | [return r]
---     | {etc.}
-data Assertion
-  = PosAtom PosSym
-  | PosConsApp ConsDef
-               [Pat]
-  deriving (Show)
-
--- refutation pattern
--- r ::= x
---     | (return a)
---     | {etc.}
-data Refutation
-  = NegAtom NegSym
-  | NegConsApp ConsDef
-               [Pat]
-  deriving (Show)
-
-inclusionA :: Assertion -> V
-inclusionA (PosAtom s) = VPosSym s
-inclusionA (PosConsApp c args) = VConsApp c (map inclusionPat args)
-
-inclusionR :: Refutation -> E
-inclusionR (NegAtom s) = ENegSym s
-inclusionR (NegConsApp c args) = EConsApp c (map inclusionPat args)
-
-inclusionPat :: Pat -> Term
-inclusionPat (PosPat a) = PosTerm (inclusionA a)
-inclusionPat (NegPat r) = NegTerm (inclusionR r)
-
-data TypeDef = TypeDef
-  { specName :: Symbol
-  , specArg :: [(Symbol, Type)]
-  } deriving (Show)
-
--- value-type definition
--- valuedef ::= (value s (x1 t1) ... (xn tn))
-type ValueDef = TypeDef
-
--- computation-type definition
--- compdef ::= (computation s (x1 t1) ... (xn tn))
-type CompDef = TypeDef
+-- negative type
+-- N ::= n
+--     | (forall (x P) N)
+--     | (par N1 ... Nn)
+--     | (up P)
+data N
+  = NVar Symbol
+  | Forall Symbol
+           P
+           N
+  | Par [N]
+  | Up P
+  deriving (Show, Eq)
 
 -- constructor definition
--- consdef ::= (constructor s ((x1 t1) ... (xn tn)) t)
+-- C ::= (constructor x P)
 data ConsDef = ConsDef
   { consName :: Symbol
-  , consArg :: [(Symbol, Type)]
-  , consCod :: Type
-  } deriving (Show)
+  , consType :: P
+  } deriving (Show, Eq)
 
-data WeakForm
-  = WeakFormHole
-  | WeakFormSymbol String
-  | WeakFormApp WeakForm
-                [WeakForm]
-  deriving (Show)
-
--- form ::= strong-form | (strong-form weak-form1 ... weak-formn)
-data Form
-  = Form String
-         [WeakForm]
-  | FormWithRest String
-                 [WeakForm]
-  deriving (Show)
-
-unifyFormV :: Form -> V -> Either String ([(String, Term)], Maybe [Term])
-unifyFormV (Form s ws) (APP (VPosSym (PosSym (S s'))) xs)
-  | s /= s' = Left $ "Cannot match " ++ show s ++ " with " ++ show s'
-  | length ws /= length xs =
-    Left $ "Cannot match " ++ show ws ++ " with " ++ show xs
-  | otherwise = do
-    ys <- zipWithM unifyEWeakForm ws xs
-    return (concat ys, Nothing)
-unifyFormV (FormWithRest s ws) (APP (VPosSym (PosSym (S s'))) xs)
-  | s /= s' = Left $ "Cannot match " ++ show s ++ " with " ++ show s'
-  | length ws > length xs =
-    Left $ "Cannot match " ++ show ws ++ " with " ++ show xs
-  | otherwise = do
-    ys <- zipWithM unifyEWeakForm ws xs
-    let rest = drop (length ws) xs
-    return (concat ys, Just (map NegTerm rest))
-unifyFormV f v = Left $ "Cannot match " ++ show f ++ " with " ++ show v
-
-unifyFormE :: Form -> E -> Either String ([(String, Term)], Maybe [Term])
-unifyFormE (Form s ws) (App (ENegSym (NegSym (S s'))) xs)
-  | s /= s' = Left $ "Cannot match " ++ show s ++ " with " ++ show s'
-  | length ws /= length xs =
-    Left $ "Cannot match " ++ show ws ++ " with " ++ show xs
-  | otherwise = do
-    ys <- zipWithM unifyVWeakForm ws xs
-    return (concat ys, Nothing)
-unifyFormE (FormWithRest s ws) (App (ENegSym (NegSym (S s'))) xs)
-  | s /= s' = Left $ "Cannot match " ++ show s ++ " with " ++ show s'
-  | length ws > length xs =
-    Left $ "Cannot match " ++ show ws ++ " with " ++ show xs
-  | otherwise = do
-    ys <- zipWithM unifyVWeakForm ws xs
-    let rest = drop (length ws) xs
-    return (concat ys, Just (map PosTerm rest))
-unifyFormE f v = Left $ "Cannot match " ++ show f ++ " with " ++ show v
-
-unifyVWeakForm :: WeakForm -> V -> Either String [(String, Term)]
-unifyVWeakForm WeakFormHole _ = return []
-unifyVWeakForm (WeakFormSymbol s) v = return [(s, PosTerm v)]
-unifyVWeakForm (WeakFormApp s args) (APP v es) = do
-  subst1 <- unifyVWeakForm s v
-  subst2 <- zipWithM unifyEWeakForm args es
-  return $ subst1 ++ concat subst2
-unifyVWeakForm f v = Left $ "Cannot match " ++ show f ++ " against " ++ show v
-
-unifyEWeakForm :: WeakForm -> E -> Either String [(String, Term)]
-unifyEWeakForm WeakFormHole _ = return []
-unifyEWeakForm (WeakFormSymbol s) e = return [(s, NegTerm e)]
-unifyEWeakForm (WeakFormApp s args) (App e vs) = do
-  subst1 <- unifyEWeakForm s e
-  subst2 <- zipWithM unifyVWeakForm args vs
-  return $ subst1 ++ concat subst2
-unifyEWeakForm f v = Left $ "Cannot match " ++ show f ++ " against " ++ show v
-
+-- data WeakForm
+--   = WeakFormHole
+--   | WeakFormSymbol String
+--   | WeakFormApp WeakForm
+--                 [WeakForm]
+--   deriving (Show)
+-- -- form ::= strong-form | (strong-form weak-form1 ... weak-formn)
+-- data Form
+--   = Form String
+--          [WeakForm]
+--   | FormWithRest String
+--                  [WeakForm]
+--   deriving (Show)
+-- unifyFormV :: Form -> V -> Either String ([(String, Term)], Maybe [Term])
+-- unifyFormV (Form s ws) (APP (VPosSym (PosSym (S s'))) xs)
+--   | s /= s' = Left $ "Cannot match " ++ show s ++ " with " ++ show s'
+--   | length ws /= length xs =
+--     Left $ "Cannot match " ++ show ws ++ " with " ++ show xs
+--   | otherwise = do
+--     ys <- zipWithM unifyEWeakForm ws xs
+--     return (concat ys, Nothing)
+-- unifyFormV (FormWithRest s ws) (APP (VPosSym (PosSym (S s'))) xs)
+--   | s /= s' = Left $ "Cannot match " ++ show s ++ " with " ++ show s'
+--   | length ws > length xs =
+--     Left $ "Cannot match " ++ show ws ++ " with " ++ show xs
+--   | otherwise = do
+--     ys <- zipWithM unifyEWeakForm ws xs
+--     let rest = drop (length ws) xs
+--     return (concat ys, Just (map NegTerm rest))
+-- unifyFormV f v = Left $ "Cannot match " ++ show f ++ " with " ++ show v
+-- unifyFormE :: Form -> E -> Either String ([(String, Term)], Maybe [Term])
+-- unifyFormE (Form s ws) (App (ENegSym (NegSym (S s'))) xs)
+--   | s /= s' = Left $ "Cannot match " ++ show s ++ " with " ++ show s'
+--   | length ws /= length xs =
+--     Left $ "Cannot match " ++ show ws ++ " with " ++ show xs
+--   | otherwise = do
+--     ys <- zipWithM unifyVWeakForm ws xs
+--     return (concat ys, Nothing)
+-- unifyFormE (FormWithRest s ws) (App (ENegSym (NegSym (S s'))) xs)
+--   | s /= s' = Left $ "Cannot match " ++ show s ++ " with " ++ show s'
+--   | length ws > length xs =
+--     Left $ "Cannot match " ++ show ws ++ " with " ++ show xs
+--   | otherwise = do
+--     ys <- zipWithM unifyVWeakForm ws xs
+--     let rest = drop (length ws) xs
+--     return (concat ys, Just (map PosTerm rest))
+-- unifyFormE f v = Left $ "Cannot match " ++ show f ++ " with " ++ show v
+-- unifyVWeakForm :: WeakForm -> V -> Either String [(String, Term)]
+-- unifyVWeakForm WeakFormHole _ = return []
+-- unifyVWeakForm (WeakFormSymbol s) v = return [(s, PosTerm v)]
+-- unifyVWeakForm (WeakFormApp s args) (APP v es) = do
+--   subst1 <- unifyVWeakForm s v
+--   subst2 <- zipWithM unifyEWeakForm args es
+--   return $ subst1 ++ concat subst2
+-- unifyVWeakForm f v = Left $ "Cannot match " ++ show f ++ " against " ++ show v
+-- unifyEWeakForm :: WeakForm -> E -> Either String [(String, Term)]
+-- unifyEWeakForm WeakFormHole _ = return []
+-- unifyEWeakForm (WeakFormSymbol s) e = return [(s, NegTerm e)]
+-- unifyEWeakForm (WeakFormApp s args) (App e vs) = do
+--   subst1 <- unifyEWeakForm s e
+--   subst2 <- zipWithM unifyVWeakForm args vs
+--   return $ subst1 ++ concat subst2
+-- unifyEWeakForm f v = Left $ "Cannot match " ++ show f ++ " against " ++ show v
 data Env = Env
-  { i :: Int
-  , valueTypeEnv :: [ValueDef]
-  , compTypeEnv :: [CompDef]
-  , consEnv :: [ConsDef]
-  , vNotationEnv :: [(String, Form, V)]
-  , eNotationEnv :: [(String, Form, E)]
+  { i           :: Int
+  -- , valueTypeEnv :: [ValueDef]
+  , consEnv     :: [ConsDef]
+  -- , vNotationEnv :: [(String, Form, V)]
+  -- , eNotationEnv :: [(String, Form, E)]
   , reservedEnv :: [String]
-  , termEnv :: [Term]
+  , compEnv     :: [Comp]
   } deriving (Show)
 
 initialEnv :: Env
 initialEnv =
   Env
     { i = 0
-    , valueTypeEnv = []
-    , compTypeEnv = []
     , consEnv = []
-    , vNotationEnv = []
-    , eNotationEnv = []
+    -- , vNotationEnv = []
+    -- , eNotationEnv = []
     , reservedEnv = []
-    , termEnv = []
+    , compEnv = []
     }
 
 type WithEnv a = StateT Env (ExceptT String IO) a
