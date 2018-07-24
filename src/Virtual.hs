325e6bb1b0c117b0057a54a394bb9cb481abaa95
vekatze <vekatze@icloud.com>
2018-07-24 21:15:44 +0900

    emit virtual code (sketchy)

--- src/Virtual.hs
+++ src/Virtual.hs
@@ -22,7 +22,7 @@ virtualV (VConsApp v1 v2, _) = do
       return $ ConstCell (CellCons cell1 (CellReg regName))
     _ -> lift $ throwE $ "virtualV : " ++ show v1 ++ "\n" ++ show v2
 virtualV (VThunk c, _) = do
-  let fvs = freeVarN c
+  let fvs = varN c
   asm <- virtualC c
   return $ Alloc asm fvs
 virtualV (VAsc v _, _) = virtualV v
@@ -45,14 +45,13 @@ virtualC (CRet v, _) = do
 virtualC (CBind (S s _) c1 c2, _) = do
   operation1 <- virtualC c1
   operation2 <- virtualC c2
-  case operation1 of
-    Ans operand -> return $ Let s operand operation2
-    e           -> lift $ throwE $ "CBind: " ++ show e
+  return $ traceLet s operation1 operation2
 virtualC (CUnthunk v, _) = do
   operand <- virtualV v
   case operand of
     Register s -> return $ Jump s
     Alloc op _ -> return op
+    _          -> lift $ throwE "virtualC.CUnthunk"
 virtualC (CSend s c, _) = undefined
 virtualC (CRecv s c, _) = undefined
 virtualC (CDispatch c1 c2, _) = undefined
@@ -68,35 +67,40 @@ funAndArgs (CApp e v, _) = do
   (fun, v : args)
 funAndArgs e = (e, [])
 
+traceLet :: String -> Operation -> Operation -> Operation
+traceLet s (Ans o) cont       = Let s o cont
+traceLet s (Jump addr) cont   = LetCall s addr cont
+traceLet s (Let k o1 o2) cont = Let k o1 (traceLet s o2 cont)
+
 getArgs :: MC -> [String]
 getArgs (CLam (S s _) e, _) = s : getArgs e
 getArgs _                   = []
 
-freeVarP :: MV -> [String]
-freeVarP (VVar s, _)         = [s]
-freeVarP (VConst _, _)       = []
-freeVarP (VConsApp v1 v2, _) = freeVarP v1 ++ freeVarP v2
-freeVarP (VThunk e, _)       = freeVarN e
-freeVarP (VAsc e t, _)       = freeVarP e
+varP :: MV -> [String]
+varP (VVar s, _)         = [s]
+varP (VConst _, _)       = []
+varP (VConsApp v1 v2, _) = varP v1 ++ varP v2
+varP (VThunk e, _)       = varN e
+varP (VAsc e t, _)       = varP e
 
-freeVarN :: MC -> [String]
-freeVarN (CLam (S s t) e, _) = filter (/= s) (freeVarN e)
-freeVarN (CApp e v, _) = freeVarN e ++ freeVarP v
-freeVarN (CRet v, _) = freeVarP v
-freeVarN (CBind (S s t) e1 e2, _) = freeVarN e1 ++ filter (/= s) (freeVarN e2)
-freeVarN (CUnthunk v, _) = freeVarP v
-freeVarN (CSend (S s t) e, _) = s : freeVarN e
-freeVarN (CRecv (S s t) e, _) = filter (/= s) (freeVarN e)
-freeVarN (CDispatch e1 e2, _) = freeVarN e1 ++ freeVarN e2
-freeVarN (CColeft e, _) = freeVarN e
-freeVarN (CCoright e, _) = freeVarN e
-freeVarN (CMu (S s t) e, _) = filter (/= s) (freeVarN e)
-freeVarN (CCase e ves, _) = do
-  let efs = freeVarN e
+varN :: MC -> [String]
+varN (CLam (S s t) e, _) = varN e -- do not filter s out
+varN (CApp e v, _) = varN e ++ varP v
+varN (CRet v, _) = varP v
+varN (CBind (S s t) e1 e2, _) = varN e1 ++ filter (/= s) (varN e2)
+varN (CUnthunk v, _) = varP v
+varN (CSend (S s t) e, _) = s : varN e
+varN (CRecv (S s t) e, _) = filter (/= s) (varN e)
+varN (CDispatch e1 e2, _) = varN e1 ++ varN e2
+varN (CColeft e, _) = varN e
+varN (CCoright e, _) = varN e
+varN (CMu (S s t) e, _) = filter (/= s) (varN e)
+varN (CCase e ves, _) = do
+  let efs = varN e
   vefss <-
     forM ves $ \(pat, body) -> do
-      bound <- freeVarP pat
-      fs <- freeVarN body
+      bound <- varP pat
+      fs <- varN body
       return $ filter (`notElem` bound) fs
   efs ++ vefss
-freeVarN (CAsc e t, _) = freeVarN e
+varN (CAsc e t, _) = varN e
