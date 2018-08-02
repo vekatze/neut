bf1bfe715274131a00373a1b2aec1845a2e8ed89
vekatze <vekatze@icloud.com>
2018-08-02 10:23:28 +0900

    update (parse)

--- src/Parse.hs
+++ src/Parse.hs
@@ -9,6 +9,8 @@ import           Control.Monad.Identity
 import           Control.Monad.State
 import           Control.Monad.Trans.Except
 
+import           Control.Comonad.Cofree
+
 import           Data
 import           Data.List
 import           Data.Maybe
@@ -16,113 +18,110 @@ import           Text.Read                  (readMaybe)
 
 import qualified Text.Show.Pretty           as Pr
 
-parseTerm :: MTree -> WithEnv MTerm
-parseTerm (Atom "_", i) = do
+parseTerm :: Tree -> WithEnv WeakTerm
+parseTerm (meta :< TreeAtom "_") = do
   name <- newNameWith "hole"
-  return (Var name, i)
-parseTerm (Atom s, i) = do
+  return (meta :< WeakTermVar name)
+parseTerm (meta :< TreeAtom s) = do
   msym <- definedConst s
   case msym of
     Nothing -> do
       s' <- strToName s
-      return (Var s', i)
-    Just (s, _) -> return (Const s, i)
-parseTerm (Node [(Atom "thunk", _), te], i) = do
+      return (meta :< WeakTermVar s')
+    Just (s, _) -> return (meta :< WeakTermConst s)
+parseTerm (meta :< TreeNode [_ :< TreeAtom "thunk", te]) = do
   e <- parseTerm te
-  return (Thunk e, i)
-parseTerm (Node [(Atom "lambda", _), (Node [(Atom s, _), tp], _), te], i) = do
+  return (meta :< WeakTermThunk e)
+parseTerm (meta :< TreeNode [_ :< TreeAtom "lambda", _ :< TreeNode [_ :< TreeAtom s, tp], te]) = do
   s' <- strToName s
   p <- parseType tp
   e <- parseTerm te
-  return (Lam (s', p) e, i)
-parseTerm (Node [(Atom "return", _), tv], i) = do
+  return (meta :< WeakTermLam (s', p) e)
+parseTerm (meta :< TreeNode [_ :< TreeAtom "return", tv]) = do
   v <- parseTerm tv
-  return (Ret v, i)
-parseTerm (Node [(Atom "bind", _), (Node [(Atom s, _), tp], _), te1, te2], i) = do
+  return (meta :< WeakTermRet v)
+parseTerm (meta :< TreeNode [_ :< TreeAtom "bind", _ :< TreeNode [_ :< TreeAtom s, tp], te1, te2]) = do
   s' <- strToName s
   p <- parseType tp
   e1 <- parseTerm te1
   e2 <- parseTerm te2
-  return (Bind (s', p) e1 e2, i)
-parseTerm (Node [(Atom "unthunk", _), tv], i) = do
+  return (meta :< WeakTermBind (s', p) e1 e2)
+parseTerm (meta :< TreeNode [_ :< TreeAtom "unthunk", tv]) = do
   v <- parseTerm tv
-  return (Unthunk v, i)
-parseTerm (Node [(Atom "mu", _), (Node [(Atom s, _), tp], _), te], i) = do
+  return (meta :< WeakTermUnthunk v)
+parseTerm (meta :< TreeNode [_ :< TreeAtom "mu", _ :< TreeNode [_ :< TreeAtom s, tp], te]) = do
   s' <- strToName s
   p <- parseType tp
   e <- parseTerm te
-  return (Mu (s', p) e, i)
-parseTerm (Node ((Atom "case", _):te:tves), i)
+  return (meta :< WeakTermMu (s', p) e)
+parseTerm (meta :< TreeNode ((_ :< TreeAtom "case"):te:tves))
   | not (null tves) = do
     e <- parseTerm te
     ves <- mapM parseClause tves
-    return (Case e ves, i)
-parseTerm (Node [(Atom "ascribe", _), te, tn], i) = do
+    return (meta :< WeakTermCase e ves)
+parseTerm (meta :< TreeNode [_ :< TreeAtom "ascribe", te, tn]) = do
   e <- parseTerm te
   n <- parseType tn
-  return (Asc e n, i)
-parseTerm (Node (te:tvs), i)
+  return (meta :< WeakTermAsc e n)
+parseTerm (meta :< TreeNode (te:tvs))
   | not (null tvs) = do
     e <- parseTerm te
     vs <- mapM parseTerm tvs
-    tmp <- foldMTerm App e vs
-    return (fst tmp, i)
+    foldMTerm WeakTermApp e vs
 parseTerm t = lift $ throwE $ "parseTerm: syntax error:\n" ++ Pr.ppShow t
 
 foldMTerm ::
-     ((a, Meta) -> (a, Meta) -> a)
-  -> (a, Meta)
-  -> [(a, Meta)]
-  -> WithEnv (a, Meta)
+     (Cofree f Meta -> a -> f (Cofree f Meta))
+  -> Cofree f Meta
+  -> [a]
+  -> StateT Env (ExceptT String IO) (Cofree f Meta)
 foldMTerm f e [] = return e
 foldMTerm f e (t:ts) = do
   let tmp = f e t
   i <- newName
-  foldMTerm f (tmp, Meta {ident = i}) ts
+  foldMTerm f (Meta {ident = i} :< tmp) ts
 
-parseClause :: MTree -> WithEnv (MTerm, MTerm)
-parseClause (Node [tv, te], i) = do
-  v <- parseTerm tv
+parsePat :: Tree -> WithEnv Pat
+parsePat = undefined
+
+parseClause :: Tree -> WithEnv (Pat, WeakTerm)
+parseClause (meta :< TreeNode [tv, te]) = do
+  v <- parsePat tv
   e <- parseTerm te
   return (v, e)
 parseClause t = lift $ throwE $ "parseClause: syntax error:\n" ++ Pr.ppShow t
 
-parseType :: MTree -> WithEnv Type
-parseType (Atom "_", i) = do
+parseType :: Tree -> WithEnv WeakType
+parseType (meta :< TreeAtom "_") = do
   name <- newNameWith "hole"
-  return (THole name)
-parseType (Atom "universe", i) = TUniv . LHole <$> newName
-parseType (Atom s, i) = do
+  return (WeakTypeHole name)
+parseType (meta :< TreeAtom "universe") =
+  WeakTypeUniv . WeakLevelHole <$> newName
+parseType (meta :< TreeAtom s) = do
   msym <- definedConst s
   case msym of
     Nothing -> do
       s' <- strToName s
-      return $ TVar s'
-    Just (s, _) -> return $ TConst s
-parseType (Node [(Atom "down", _), tn], i) = do
+      return $ WeakTypeVar s'
+    Just (s, _) -> return $ WeakTypeConst s
+parseType (meta :< TreeNode [_ :< TreeAtom "down", tn]) = do
   n <- parseType tn
-  return $ TDown n
-parseType (Node [(Atom "universe", _), (Atom si, _)], i) =
+  return $ WeakTypeDown n
+parseType (meta :< TreeNode [_ :< TreeAtom "universe", _ :< TreeAtom si]) =
   case readMaybe si of
     Nothing -> lift $ throwE $ "not a number: " ++ si
-    Just j  -> return $ TUniv (Fixed j)
-parseType (Node [(Atom "forall", _), (Node [(Atom s, _), tp], _), tn], i) = do
+    Just j  -> return $ WeakTypeUniv (WeakLevelFixed j)
+parseType (meta :< TreeNode [_ :< TreeAtom "forall", _ :< TreeNode [_ :< TreeAtom s, tp], tn]) = do
   s' <- strToName s
   p <- parseType tp
   n <- parseType tn
-  return $ TForall (s', p) n
-parseType (Node [(Atom "up", _), tp], i) = do
+  return $ WeakTypeForall (s', p) n
+parseType (meta :< TreeNode [_ :< TreeAtom "up", tp]) = do
   p <- parseType tp
-  return $ TUp p
+  return $ WeakTypeUp p
 parseType t = lift $ throwE $ "parseType: syntax error:\n" ++ Pr.ppShow t
 
-parseVDef :: MTree -> WithEnv ()
-parseVDef (Node [(Atom "value", _), (Atom x, _), tp], i) = do
-  p <- parseType tp
-  modify (\e -> e {valueEnv = (x, p) : valueEnv e})
-parseVDef t = lift $ throwE $ "parseVDef: syntax error:\n" ++ Pr.ppShow t
-
-definedConst :: String -> WithEnv (Maybe (String, Type))
+definedConst :: String -> WithEnv (Maybe (String, ValueType))
 definedConst s = do
   env <- get
   let vEnv = valueEnv env
