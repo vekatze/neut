5df3c44fd0ccb4186bda76b2e791e5d0264da36f
vekatze <vekatze@icloud.com>
2018-08-21 22:45:50 +0900

    reconstruct Infer (putting aside region-inference)

--- src/Infer.hs
+++ src/Infer.hs
@@ -13,119 +13,84 @@ import qualified Text.Show.Pretty           as Pr
 
 import           Data
 
-check :: Identifier -> QuasiComp -> WithEnv ()
+check :: Identifier -> Term -> WithEnv ()
 check main e = do
-  t <- inferC e
-  insWTEnv main t -- insert the type of main function
+  t <- infer e
+  insTypeEnv main t -- insert the type of main function
   env <- get
   sub <- unify $ constraintEnv env
-  env' <- get
-  let aenv = argEnv env'
-  argSubst <- unifyArg aenv
-  let tenv' =
-        map (\(s, t) -> (s, applyArgSubst argSubst $ sType sub t)) $
-        weakTypeEnv env
-  modify (\e -> e {weakTypeEnv = tenv', constraintEnv = []})
+  let tenv' = map (\(s, t) -> (s, sType sub t)) $ typeEnv env
+  modify (\e -> e {typeEnv = tenv', constraintEnv = []})
 
-inferV :: QuasiValue -> WithEnv WeakType
-inferV (QuasiValue (Meta {ident = i} :< ValueVar s)) = do
-  mt <- lookupWTEnv s
+infer :: Term -> WithEnv Type
+infer (meta :< TermVar s) = do
+  mt <- lookupTypeEnv s
   case mt of
-    Just t -> do
-      insWTEnv i t
-      return t
+    Just t -> return t
     Nothing -> do
-      new <- WeakTypePosHole <$> newName
-      insWTEnv s new
-      insWTEnv i new
-      return new
-inferV (QuasiValue (Meta {ident = j} :< ValueNodeApp s vs)) = do
-  mt <- lookupVEnv s
-  case mt of
-    Nothing -> lift $ throwE $ "const " ++ s ++ " is not defined"
-    Just (_, xts, t) -> do
-      tvs <- mapM (inferV . QuasiValue) vs
-      forM_ (zip xts tvs) $ \(xt, t2) -> insCEnv (weakenValueType $ snd xt) t2
-      insWTEnv j $ weakenValueType t
-      return $ weakenValueType t
-inferV (QuasiValue (Meta {ident = i} :< ValueThunk e)) = do
-  t <- inferC e
-  let result = WeakTypeDown t i
-  insWTEnv i result
+      new <- TypeHole <$> newName
+      insTypeEnv s $ meta :< new
+      return $ meta :< new
+infer (meta :< TermThunk e) = do
+  t <- infer e
+  let result = meta :< TypeDown t
   return result
-
-inferC :: QuasiComp -> WithEnv WeakType
-inferC (QuasiComp (Meta {ident = i} :< QuasiCompLam s e)) = do
-  t <- WeakTypePosHole <$> newName
-  insWTEnv s t
-  te <- inferC (QuasiComp e)
-  let result = WeakTypeForall (Ident s, t) te
-  insWTEnv i result
+infer (meta :< TermLam s e) = do
+  (tdom, arg) <- inferArg s
+  te <- infer e
+  let result = meta :< TypeForall (arg, tdom) te
   return result
-inferC (QuasiComp (Meta {ident = l} :< QuasiCompApp e v)) = do
-  te <- inferC (QuasiComp e)
-  tv <- inferV v
+infer (meta :< TermApp e v) = do
+  te@(funMeta :< _) <- infer e
+  tv <- infer v
   i <- newName
-  insWTEnv i (WeakTypeNegHole i)
+  let result = meta :< TypeHole i
   j <- newName
-  insCEnv te (WeakTypeForall (Hole j, tv) (WeakTypeNegHole i))
-  let result = WeakTypeNegHole i
-  insWTEnv l result
+  insConstraintEnv te (funMeta :< TypeForall (j, tv) result)
   return result
-inferC (QuasiComp (Meta {ident = i} :< QuasiCompRet v)) = do
-  tv <- inferV v
-  let result = WeakTypeUp tv
-  insWTEnv i result
-  return result
-inferC (QuasiComp (Meta {ident = i} :< QuasiCompBind s e1 e2)) = do
-  t <- WeakTypePosHole <$> newName
-  insWTEnv s t
-  t1 <- inferC (QuasiComp e1)
-  t2 <- inferC (QuasiComp e2)
-  insCEnv (WeakTypeUp t) t1
-  insWTEnv i t2
-  return t2
-inferC (QuasiComp (Meta {ident = l} :< QuasiCompUnthunk v)) = do
-  t <- inferV v
+infer (meta :< TermLift v) = do
+  tv <- infer v
+  return $ meta :< TypeUp tv
+infer (meta :< TermColift v) = do
+  tv <- infer v
+  return $ meta :< TypeDown tv
+infer (meta :< TermUnthunk v) = do
+  t@(metaT :< _) <- infer v
   i <- newName
-  insCEnv t (WeakTypeDown (WeakTypeNegHole i) l)
-  let result = WeakTypeNegHole i
-  insWTEnv l result
+  let result = meta :< TypeHole i
+  insConstraintEnv t (metaT :< TypeDown result)
   return result
-inferC (QuasiComp (Meta {ident = i} :< QuasiCompMu s e)) = do
-  t <- WeakTypePosHole <$> newName
-  insWTEnv s t
-  te <- inferC (QuasiComp e)
-  insCEnv (WeakTypeDown te i) t
-  insWTEnv i te
+infer (meta :< TermMu s e) = do
+  (trec, ident) <- inferArg s
+  insTypeEnv ident trec
+  te <- infer e
+  insConstraintEnv (meta :< TypeDown te) trec
   return te
-inferC (QuasiComp (Meta {ident = i} :< QuasiCompCase vs vses)) = do
-  ts <- mapM inferV vs
+infer (meta :< TermCase vs vses) = do
+  ts <- mapM infer vs
   let (vss, es) = unzip vses
   tvss <- mapM (mapM inferPat) vss
-  forM_ tvss $ \tvs -> do forM_ (zip ts tvs) $ \(t1, t2) -> do insCEnv t1 t2
-  ans <- WeakTypeNegHole <$> newName
-  tes <- mapM (inferC . QuasiComp) es
-  forM_ tes $ \te -> insCEnv ans te
-  insWTEnv i ans
-  return ans
+  forM_ tvss $ \tvs -> do
+    forM_ (zip ts tvs) $ \(t1, t2) -> do insConstraintEnv t1 t2
+  ans <- TypeHole <$> newName
+  tes <- mapM (infer) es
+  forM_ tes $ \te -> insConstraintEnv (meta :< ans) te
+  return (meta :< ans)
+infer t = error $ "Infer.infer: illegal argument: " ++ Pr.ppShow t
 
-inferPat :: Pat -> WithEnv WeakType
-inferPat (Meta {ident = i} :< PatHole) = do
-  t <- WeakTypePosHole <$> newName
-  insWTEnv i t
-  return t
-inferPat (Meta {ident = i} :< PatVar s) = do
-  mt <- lookupWTEnv s
+inferPat :: Pat -> WithEnv Type
+inferPat (meta :< PatHole) = do
+  t <- TypeHole <$> newName
+  return $ meta :< t
+inferPat (meta :< PatVar s) = do
+  mt <- lookupTypeEnv s
   case mt of
     Just t -> do
-      insWTEnv i t
       return t
     Nothing -> do
-      new <- WeakTypePosHole <$> newName
-      insWTEnv s new
-      insWTEnv i new
-      return new
+      new <- TypeHole <$> newName
+      insTypeEnv s $ meta :< new
+      return $ meta :< new
 inferPat (Meta {ident = j} :< PatApp s vs) = do
   mt <- lookupVEnv s
   case mt of
@@ -133,41 +98,45 @@ inferPat (Meta {ident = j} :< PatApp s vs) = do
     Just (_, xts, t) -> do
       tvs <- mapM inferPat vs
       -- need to add explicit parametrization
-      forM_ (zip xts tvs) $ \(xt, t2) -> insCEnv (weakenValueType $ snd xt) t2
-      insWTEnv j $ weakenValueType t
-      return $ weakenValueType t
+      forM_ (zip xts tvs) $ \(xt, t2) -> insConstraintEnv (snd xt) t2
+      insTypeEnv j t
+      return t
 
-type Subst = [(String, WeakType)]
+inferArg :: Arg -> WithEnv (Type, Identifier)
+inferArg (ArgIdent x) = do
+  i <- newNameWith x
+  t <- newName
+  return (Meta {ident = i} :< TypeHole t, x)
+inferArg (ArgLift arg) = do
+  (t@(meta :< _), ident) <- inferArg arg
+  return (meta :< TypeUp t, ident)
+inferArg (ArgColift arg) = do
+  (t@(meta :< _), ident) <- inferArg arg
+  return (meta :< TypeDown t, ident)
 
-type Constraint = [(WeakType, WeakType)]
+type Subst = [(String, Type)]
+
+type Constraint = [(Type, Type)]
 
 unify :: Constraint -> WithEnv Subst
 unify [] = return []
-unify ((WeakTypePosHole s, t2):cs) = do
+unify ((_ :< TypeHole s, t2):cs) = do
   sub <- unify (sConstraint [(s, t2)] cs)
   return $ compose sub [(s, t2)]
-unify ((t1, WeakTypePosHole s):cs) = do
+unify ((t1, _ :< TypeHole s):cs) = do
   sub <- unify (sConstraint [(s, t1)] cs)
   return $ compose sub [(s, t1)]
-unify ((WeakTypeNegHole s, t2):cs) = do
-  sub <- unify (sConstraint [(s, t2)] cs)
-  return $ compose sub [(s, t2)]
-unify ((t1, WeakTypeNegHole s):cs) = do
-  sub <- unify (sConstraint [(s, t1)] cs)
-  return $ compose sub [(s, t1)]
-unify ((WeakTypeVar s1, WeakTypeVar s2):cs)
+unify ((_ :< TypeVar s1, _ :< TypeVar s2):cs)
   | s1 == s2 = unify cs
-unify ((WeakTypeForall (i, tdom1) tcod1, WeakTypeForall (j, tdom2) tcod2):cs) = do
-  insAEnv i j
+unify ((_ :< TypeForall (_, tdom1) tcod1, _ :< TypeForall (_, tdom2) tcod2):cs) = do
   unify $ (tdom1, tdom2) : (tcod1, tcod2) : cs
-unify ((WeakTypeNode x ts1, WeakTypeNode y ts2):cs)
+unify ((_ :< TypeNode x ts1, _ :< TypeNode y ts2):cs)
   | x == y = unify $ (zip ts1 ts2) ++ cs
-unify ((WeakTypeUp t1, WeakTypeUp t2):cs) = do
+unify ((_ :< TypeUp t1, _ :< TypeUp t2):cs) = do
   unify $ (t1, t2) : cs
-unify ((WeakTypeDown t1 i, WeakTypeDown t2 j):cs) = do
-  insThunkEnv i j
+unify ((_ :< TypeDown t1, _ :< TypeDown t2):cs) = do
   unify $ (t1, t2) : cs
-unify ((WeakTypeUniv i, WeakTypeUniv j):cs) = do
+unify ((_ :< TypeUniv i, _ :< TypeUniv j):cs) = do
   insLEnv i j
   unify cs
 unify ((t1, t2):_) =
@@ -183,89 +152,26 @@ compose s1 s2 = do
   let fromS1 = filter (\(ident, _) -> ident `notElem` domS2) s1
   fromS1 ++ zip domS2 codS2'
 
-sType :: Subst -> WeakType -> WeakType
-sType _ (WeakTypeVar s) = WeakTypeVar s
-sType sub (WeakTypePosHole s) =
+sType :: Subst -> Type -> Type
+sType _ (meta :< TypeVar s) = meta :< TypeVar s
+sType sub (meta :< TypeHole s) =
   case lookup s sub of
-    Nothing -> WeakTypePosHole s
+    Nothing -> meta :< TypeHole s
     Just t  -> t
-sType sub (WeakTypeNegHole s) =
-  case lookup s sub of
-    Nothing -> WeakTypeNegHole s
-    Just t  -> t
-sType sub (WeakTypeUp t) = do
+sType sub (meta :< TypeUp t) = do
   let t' = sType sub t
-  WeakTypeUp t'
-sType sub (WeakTypeDown t i) = do
+  meta :< TypeUp t'
+sType sub (meta :< TypeDown t) = do
   let t' = sType sub t
-  WeakTypeDown t' i
-sType _ (WeakTypeUniv i) = WeakTypeUniv i
-sType sub (WeakTypeForall (s, tdom) tcod) = do
+  meta :< TypeDown t'
+sType _ (meta :< TypeUniv i) = meta :< TypeUniv i
+sType sub (meta :< TypeForall (s, tdom) tcod) = do
   let tdom' = sType sub tdom
   let tcod' = sType sub tcod
-  WeakTypeForall (s, tdom') tcod'
-sType sub (WeakTypeNode s ts) = do
+  meta :< TypeForall (s, tdom') tcod'
+sType sub (meta :< TypeNode s ts) = do
   let ts' = map (sType sub) ts
-  WeakTypeNode s ts'
+  meta :< TypeNode s ts'
 
 sConstraint :: Subst -> Constraint -> Constraint
 sConstraint s = map (\(t1, t2) -> (sType s t1, sType s t2))
-
-type ArgConstraint = [(IdentOrHole, IdentOrHole)]
-
-type ArgSubst = [(Identifier, IdentOrHole)]
-
-unifyArg :: ArgConstraint -> WithEnv ArgSubst
-unifyArg [] = return []
-unifyArg ((Hole s1, j):cs) = do
-  sub <- unifyArg (sArgConstraint [(s1, j)] cs)
-  return $ argCompose sub [(s1, j)]
-unifyArg ((i, Hole s2):cs) = do
-  sub <- unifyArg (sArgConstraint [(s2, i)] cs)
-  return $ argCompose sub [(s2, i)]
-unifyArg ((Ident s1, Ident s2):cs)
-  | s1 == s2 = unifyArg cs
-unifyArg ((x, y):_) =
-  lift $
-  throwE $
-  "arg-unification failed for:\n" ++ Pr.ppShow x ++ "\nand:\n" ++ Pr.ppShow y
-
-sArgConstraint :: ArgSubst -> ArgConstraint -> ArgConstraint
-sArgConstraint s = map (\(t1, t2) -> (sArg s t1, sArg s t2))
-
-sArg :: ArgSubst -> IdentOrHole -> IdentOrHole
-sArg cs (Ident s) =
-  case lookup s cs of
-    Nothing -> Ident s
-    Just s' -> s'
-sArg cs (Hole s) =
-  case lookup s cs of
-    Nothing -> Hole s
-    Just s' -> s'
-
-argCompose :: ArgSubst -> ArgSubst -> ArgSubst
-argCompose s1 s2 = do
-  let domS2 = map fst s2
-  let codS2 = map snd s2
-  let codS2' = map (sArg s1) codS2
-  let fromS1 = filter (\(ident, _) -> ident `notElem` domS2) s1
-  fromS1 ++ zip domS2 codS2'
-
-applyArgSubst :: ArgSubst -> WeakType -> WeakType
-applyArgSubst _ (WeakTypeVar s) = WeakTypeVar s
-applyArgSubst _ (WeakTypePosHole s) = WeakTypePosHole s
-applyArgSubst _ (WeakTypeNegHole s) = WeakTypeNegHole s
-applyArgSubst sub (WeakTypeNode x ts) = do
-  let ts' = map (applyArgSubst sub) ts
-  WeakTypeNode x ts'
-applyArgSubst sub (WeakTypeUp t) = do
-  let t' = applyArgSubst sub t
-  WeakTypeUp t'
-applyArgSubst sub (WeakTypeDown t i) = do
-  let t' = applyArgSubst sub t
-  WeakTypeDown t' i
-applyArgSubst _ (WeakTypeUniv i) = WeakTypeUniv i
-applyArgSubst sub (WeakTypeForall (s, tdom) tcod) = do
-  let tdom' = applyArgSubst sub tdom
-  let tcod' = applyArgSubst sub tcod
-  WeakTypeForall (sArg sub s, tdom') tcod'
