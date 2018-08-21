dfbd2660526ee2f4c1ab6d2aa0ed50ba8d8099f8
vekatze <vekatze@icloud.com>
2018-08-21 21:37:45 +0900

    adjoint restruction (Parse)

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,7 +1,7 @@
 module Parse
   ( parseType
-  , parseComp
-  , parseNodeTypeArg
+  , parse
+  , parseTypeArg
   ) where
 
 import           Control.Monad              (void)
@@ -15,76 +15,49 @@ import           Control.Comonad.Cofree
 import           Data
 import           Data.List
 import           Data.Maybe
-import           Pattern
+
 import           Text.Read                  (readMaybe)
 
 import qualified Text.Show.Pretty           as Pr
 
-parseValue :: Tree -> WithEnv QuasiValue
-parseValue (meta :< TreeAtom "_") = do
+parse :: Tree -> WithEnv Term
+parse (meta :< TreeAtom "_") = do
   name <- newNameWith "hole"
-  return $ QuasiValue $ meta :< ValueVar name
-parseValue (meta :< TreeAtom s) = return $ QuasiValue (meta :< ValueVar s)
-parseValue (meta :< TreeNode [_ :< TreeAtom "thunk", te]) = do
-  e <- parseComp te
-  return $ QuasiValue (meta :< ValueThunk e)
-parseValue (meta :< TreeNode ((_ :< TreeAtom s):ts)) = do
-  msym <- lookupVEnv s
-  case msym of
-    Nothing -> lift $ throwE "(polarity)"
-    Just (s, args, _) -- "zero" should be written as `(zero)`, for example.
-      | length args == length ts -> do
-        vs <- mapM parseValue ts
-        let vs' = map (\(QuasiValue v) -> v) vs
-        return $ QuasiValue $ meta :< ValueNodeApp s vs'
-    Just (s, args, _) -> do
-      lift $
-        throwE $
-        "the arity of " ++
-        show s ++ " is " ++ show (length args) ++ ", not " ++ show (length ts)
-parseValue t = lift $ throwE $ "parseValue: syntax error:\n" ++ Pr.ppShow t
-
-parseComp :: Tree -> WithEnv QuasiComp
-parseComp (meta :< TreeNode [_ :< TreeAtom "lambda", _ :< TreeNode ts, te]) = do
+  return $ meta :< TermVar name
+parse (meta :< TreeAtom s) = return $ meta :< TermVar s
+parse (meta :< TreeNode [_ :< TreeAtom "thunk", te]) = do
+  e <- parse te
+  return $ meta :< TermThunk e
+parse (meta :< TreeNode [_ :< TreeAtom "lambda", _ :< TreeNode ts, te]) = do
   xs <- parseIdentSeq ts
-  QuasiComp e <- parseComp te
-  _ :< term <- foldMTermR QuasiCompLam e xs
-  return $ QuasiComp $ meta :< term
-parseComp (meta :< TreeNode [_ :< TreeAtom "return", tv]) = do
-  v <- parseValue tv
-  return $ QuasiComp $ meta :< QuasiCompRet v
-parseComp (meta :< TreeNode [_ :< TreeAtom "bind", _ :< TreeAtom s, te1, te2]) = do
-  s' <- strOrNewName s
-  QuasiComp e1 <- parseComp te1
-  QuasiComp e2 <- parseComp te2
-  return $ QuasiComp $ meta :< QuasiCompBind s' e1 e2
-parseComp (meta :< TreeNode [_ :< TreeAtom "unthunk", tv]) = do
-  v <- parseValue tv
-  return $ QuasiComp $ meta :< QuasiCompUnthunk v
-parseComp (meta :< TreeNode [_ :< TreeAtom "mu", _ :< TreeAtom s, te]) = do
-  s' <- strOrNewName s
-  QuasiComp e <- parseComp te
-  return $ QuasiComp $ meta :< QuasiCompMu s' e
-parseComp (meta :< TreeNode ((_ :< TreeAtom "match"):(_ :< TreeNode tvs):tves))
+  e <- parse te
+  _ :< term <- foldMTermR TermLam e xs
+  return $ meta :< term
+parse (meta :< TreeNode [_ :< TreeAtom "lift", tv]) = do
+  v <- parse tv
+  return $ meta :< TermLift v
+parse (meta :< TreeNode [_ :< TreeAtom "colift", tv]) = do
+  v <- parse tv
+  return $ meta :< TermColift v
+parse (meta :< TreeNode [_ :< TreeAtom "unthunk", tv]) = do
+  v <- parse tv
+  return $ meta :< TermUnthunk v
+parse (meta :< TreeNode [_ :< TreeAtom "mu", _ :< TreeAtom s, te]) = do
+  s' <- strToName s
+  e <- parse te
+  return $ meta :< TermMu s' e
+parse (meta :< TreeNode ((_ :< TreeAtom "match"):(_ :< TreeNode tvs):tves))
   | not (null tves) = do
-    vs <- mapM parseValue tvs
+    vs <- mapM parse tvs
     ves <- mapM parseClause tves
-    return $ QuasiComp $ meta :< QuasiCompCase vs ves
-parseComp (_ :< TreeNode (te@(_ :< TreeAtom s):ts)) = do
-  msym <- lookupVEnv s
-  case msym of
-    Nothing -> parseCompApp te ts
-    Just _ -> do
-      lift $ throwE "parseComp.compapp"
-parseComp (_ :< TreeNode (te:tvs))
-  | not (null tvs) = parseCompApp te tvs
-parseComp t = lift $ throwE $ "parseComp: syntax error:\n" ++ Pr.ppShow t
-
-parseCompApp :: Tree -> [Tree] -> WithEnv QuasiComp
-parseCompApp te tvs = do
-  QuasiComp e <- parseComp te
-  vs <- mapM parseValue tvs
-  QuasiComp <$> foldMTerm QuasiCompApp e vs
+    return $ meta :< TermCase vs ves
+parse (meta :< TreeNode (te:tvs))
+  | not (null tvs) = do
+    e <- parse te
+    vs <- mapM parse tvs
+    _ :< tmp <- foldMTermL TermApp e vs
+    return $ meta :< tmp
+parse t = lift $ throwE $ "parse: syntax error:\n" ++ Pr.ppShow t
 
 parseIdentSeq :: [Tree] -> WithEnv [Identifier]
 parseIdentSeq [] = return []
@@ -101,9 +74,9 @@ parsePat (meta :< TreeAtom s) = do
   msym <- lookupVEnv s
   case msym of
     Nothing -> do
-      s' <- strOrNewName s
+      s' <- strToName s
       return (meta :< PatVar s')
-    Just (s, _, _) -> return (meta :< PatApp s [])
+    Just (s, _, _) -> return $ meta :< PatApp s []
 parsePat (meta :< TreeNode ((_ :< TreeAtom s):ts)) = do
   msym <- lookupVEnv s
   case msym of
@@ -114,41 +87,44 @@ parsePat (meta :< TreeNode ((_ :< TreeAtom s):ts)) = do
       return $ meta :< PatApp s ts'
 parsePat t = lift $ throwE $ "parsePat: syntax error:\n" ++ Pr.ppShow t
 
-parseClause :: Tree -> WithEnv ([Pat], PreQuasiComp)
+parseClause :: Tree -> WithEnv ([Pat], Term)
 parseClause (_ :< TreeNode [(_ :< TreeAtom "with"), (_ :< TreeNode tps), tbody]) = do
   ps <- mapM parsePat tps
-  QuasiComp body <- parseComp tbody
+  body <- parse tbody
   return (ps, body)
 parseClause t = lift $ throwE $ "parseClause: syntax error:\n" ++ Pr.ppShow t
 
-parseType :: Tree -> WithEnv WeakType
-parseType (_ :< TreeAtom "_") = do
+parseType :: Tree -> WithEnv Type
+parseType (meta :< TreeAtom "_") = do
   name <- newNameWith "hole"
-  return (WeakTypePosHole name)
-parseType (_ :< TreeAtom "universe") = WeakTypeUniv . WeakLevelHole <$> newName
-parseType (_ :< TreeAtom s) = return $ WeakTypeVar s
-parseType (Meta {ident = i} :< TreeNode [_ :< TreeAtom "down", tn]) = do
+  return (meta :< TypeHole name)
+parseType (meta :< TreeAtom "universe") = do
+  t <- TypeUniv . WeakLevelHole <$> newName
+  return $ meta :< t
+parseType (meta :< TreeAtom s) = return $ meta :< TypeVar s -- ?
+parseType (meta :< TreeNode [_ :< TreeAtom "down", tn]) = do
   n <- parseType tn
-  return $ WeakTypeDown n i
-parseType (_ :< TreeNode [_ :< TreeAtom "universe", _ :< TreeAtom si]) =
+  return $ meta :< TypeDown n
+parseType (meta :< TreeNode [_ :< TreeAtom "universe", _ :< TreeAtom si]) =
   case readMaybe si of
     Nothing -> lift $ throwE $ "not a number: " ++ si
-    Just j  -> return $ WeakTypeUniv (WeakLevelFixed j)
-parseType (_ :< TreeNode [_ :< TreeAtom "forall", _ :< TreeNode ts, tn]) = do
+    Just j  -> return $ meta :< TypeUniv (WeakLevelFixed j)
+parseType (meta :< TreeNode [_ :< TreeAtom "forall", _ :< TreeNode ts, tn]) = do
   its <- mapM parseTypeArg ts
   n <- parseType tn
-  return $ foldr WeakTypeForall n its
-parseType (_ :< TreeNode [_ :< TreeAtom "up", tp]) = do
+  _ :< t <- foldMTermR TypeForall n its
+  return $ meta :< t
+parseType (meta :< TreeNode [_ :< TreeAtom "up", tp]) = do
   p <- parseType tp
-  return $ WeakTypeUp p
-parseType (_ :< TreeNode ((_ :< TreeAtom s):ts)) = do
+  return $ meta :< TypeUp p
+parseType (meta :< TreeNode ((_ :< TreeAtom s):ts)) = do
   msym <- lookupVEnv s
   case msym of
     Nothing -> lift $ throwE $ "the constant " ++ show s ++ " is not defined"
     Just (s, args, _) -- "nat" should be written as `(nat)`, for example.
       | length args == length ts -> do
         es <- mapM parseType ts
-        return $ WeakTypeNode s es
+        return $ meta :< TypeNode s es
     Just (s, args, _) -> do
       lift $
         throwE $
@@ -156,27 +132,13 @@ parseType (_ :< TreeNode ((_ :< TreeAtom s):ts)) = do
         show s ++ " is " ++ show (length args) ++ ", not " ++ show (length ts)
 parseType t = lift $ throwE $ "parseType: syntax error:\n" ++ Pr.ppShow t
 
-parseTypeArg :: Tree -> WithEnv (IdentOrHole, WeakType)
+parseTypeArg :: Tree -> WithEnv (Identifier, Type)
 parseTypeArg (_ :< TreeNode [_ :< TreeAtom s, tp]) = do
   s' <- strToName s
   t <- parseType tp
   return (s', t)
 parseTypeArg t = lift $ throwE $ "parseTypeArg: syntax error:\n" ++ Pr.ppShow t
 
-parseNodeTypeArg :: Tree -> WithEnv (Identifier, WeakType)
-parseNodeTypeArg (_ :< TreeNode [_ :< TreeAtom s, tp]) = do
-  s' <- strOrNewName s
-  t <- parseType tp
-  return (s', t)
-parseNodeTypeArg t =
-  lift $ throwE $ "parseTypeArg: syntax error:\n" ++ Pr.ppShow t
-
-strToName :: String -> WithEnv IdentOrHole
-strToName "_" = do
-  s <- newNameWith "hole"
-  return $ Hole s
-strToName s = return $ Ident s
-
-strOrNewName :: String -> WithEnv String
-strOrNewName "_" = newNameWith "hole"
-strOrNewName s   = return s
+strToName :: String -> WithEnv Identifier
+strToName "_" = newNameWith "hole"
+strToName s   = return $ s
