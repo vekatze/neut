afde011e8b8111ace7eab18868a7c155a9de468a
u2zv1wx <u2zv1wx@protonmail.ch>
2018-09-14 16:46:53 +0900

    rename dependent types/terms

--- src/Rename.hs
+++ src/Rename.hs
@@ -9,113 +9,53 @@ import           Data
 
 import qualified Text.Show.Pretty           as Pr
 
-rename :: Term -> WithEnv Term
-rename (i :< TermVar s) = do
-  t <- TermVar <$> lookupNameEnv s
+rename :: Neut -> WithEnv Neut
+rename (i :< NeutVar s) = do
+  t <- NeutVar <$> lookupNameEnv s
   return $ i :< t
-rename (i :< TermConst s) = return $ i :< TermConst s
-rename (i :< TermLam s e) =
+rename (i :< NeutForall (s, tdom) tcod) = do
+  tdom' <- rename tdom
+  local $ do
+    s' <- newNameWith s
+    tcod' <- rename tcod
+    return $ i :< NeutForall (s', tdom') tcod'
+rename (i :< NeutLam (s, tdom) e) = do
+  tdom' <- rename tdom
   local $ do
     s' <- newNameWith s
     e' <- rename e
-    return $ i :< TermLam s' e'
-rename (i :< TermApp e v) = do
+    return $ i :< NeutLam (s', tdom') e'
+rename (i :< NeutApp e v) = do
   e' <- rename e
   v' <- rename v
-  return $ i :< TermApp e' v'
-rename (i :< TermProduct v1 v2) = do
+  return $ i :< NeutApp e' v'
+rename (i :< NeutPair v1 v2) = do
   v1' <- rename v1
   v2' <- rename v2
-  return $ i :< TermProduct v1' v2'
-rename (i :< TermLift v) = do
-  v' <- rename v
-  return $ i :< TermLift v'
-rename (i :< TermBind x e1 e2) = do
+  return $ i :< NeutPair v1' v2'
+rename (i :< NeutExists (s, tdom) tcod) = do
+  tdom' <- rename tdom
+  local $ do
+    s' <- newNameWith s
+    tcod' <- rename tcod
+    return $ i :< NeutExists (s', tdom') tcod'
+rename (i :< NeutMu s e) =
+  local $ do
+    s' <- newNameWith s
+    e' <- rename e
+    return $ i :< NeutMu s' e'
+rename (i :< NeutCase e1 (x, y) e2) = do
   e1' <- rename e1
   local $ do
     x' <- newNameWith x
+    y' <- newNameWith y
     e2' <- rename e2
-    return $ i :< TermBind x' e1' e2'
-rename (i :< TermThunk e) = do
+    return $ i :< NeutCase e1' (x', y') e2'
+rename (i :< NeutTop) = return $ i :< NeutTop
+rename (i :< NeutUnit) = return $ i :< NeutUnit
+rename (i :< NeutBottom) = return $ i :< NeutBottom
+rename (i :< NeutAbort e) = do
   e' <- rename e
-  return $ i :< TermThunk e'
-rename (i :< TermUnthunk v) = do
-  v' <- rename v
-  return $ i :< TermUnthunk v'
-rename (i :< TermMu s e) =
-  local $ do
-    s' <- newNameWith s
-    e' <- rename e
-    return $ i :< TermMu s' e'
-rename (i :< TermCase vs ves) = do
-  vs' <- mapM rename vs
-  ves' <-
-    forM ves $ \(pat, body) ->
-      local $ do
-        env <- get
-        patEnvOrErr <-
-          liftIO $ runWithEnv (mapM renamePat pat) (env {nameEnv = []})
-        case patEnvOrErr of
-          Left err -> lift $ throwE err
-          Right (pat', env') -> do
-            put
-              (env {nameEnv = nameEnv env' ++ nameEnv env, count = count env'})
-            body' <- rename body
-            return (pat', body')
-  return $ i :< TermCase vs' ves'
-
-renameType :: Type -> WithEnv Type
-renameType (Fix TypeUnit) = return $ Fix TypeUnit
-renameType (Fix (TypeInt i)) = return $ Fix (TypeInt i)
-renameType (Fix (TypeVar s)) = do
-  t' <- TypeVar <$> lookupNameEnv s
-  return $ Fix t'
-renameType (Fix (TypeHole i)) = return $ Fix $ TypeHole i
-renameType (Fix (TypeUp t)) = do
-  t' <- TypeUp <$> renameType t
-  return $ Fix t'
-renameType (Fix (TypeDown t)) = do
-  t' <- renameType t
-  return $ Fix $ TypeDown t'
-renameType (Fix (TypeUniv level)) = return $ Fix $ TypeUniv level
-renameType (Fix (TypeForall (s, tdom) tcod)) = do
-  tdom' <- renameType tdom
-  local $ do
-    s' <- newNameWith s
-    tcod' <- renameType tcod
-    return $ Fix $ TypeForall (s', tdom') tcod'
-renameType (Fix (TypeExists (s, tdom) tcod)) = do
-  tdom' <- renameType tdom
-  local $ do
-    s' <- newNameWith s
-    tcod' <- renameType tcod
-    return $ Fix $ TypeExists (s', tdom') tcod'
-renameType (Fix (TypeNode s ts)) = do
-  ts' <- mapM renameType ts
-  return $ Fix $ TypeNode s ts'
-renameType (Fix (TypeStruct ts)) = do
-  ts' <- mapM renameType ts
-  return $ Fix $ TypeStruct ts'
-
-renameNodeType ::
-     [(Identifier, Type)] -> Type -> WithEnv ([(Identifier, Type)], Type)
-renameNodeType [] t = do
-  t' <- renameType t
-  return ([], t')
-renameNodeType ((i, wt):wts) t = do
-  wt' <- renameType wt
-  local $ do
-    i' <- newNameWith i
-    (wts', t') <- renameNodeType wts t
-    return ((i', wt') : wts', t')
-
-renamePat :: Pat -> WithEnv Pat
-renamePat (i :< PatHole) = return $ i :< PatHole
-renamePat (i :< PatConst x) = return $ i :< PatConst x
-renamePat (i :< PatVar s) = do
-  t <- PatVar <$> lookupNameEnv' s
-  return (i :< t)
-renamePat (i :< PatProduct v1 v2) = do
-  v1' <- renamePat v1
-  v2' <- renamePat v2
-  return $ i :< PatProduct v1' v2'
+  return $ i :< NeutAbort e'
+rename (i :< NeutUniv) = return $ i :< NeutUniv
+rename (i :< NeutHole x) = return $ i :< NeutHole x
