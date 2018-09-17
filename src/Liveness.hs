7321639fd9446740c41c1e8b9a529be19aa67d17
vekatze <vekatze@icloud.com>
2018-09-17 23:20:38 +0900

    update

--- src/Liveness.hs
+++ src/Liveness.hs
@@ -17,26 +17,25 @@ import           Debug.Trace
 
 import qualified Text.Show.Pretty           as Pr
 
--- annotAsmEnv :: WithEnv ()
--- annotAsmEnv = do
---   env <- get
---   forM_ (codeEnv env) $ \(_, (_, asmRef)) -> do
---     asm <- liftIO $ readIORef asmRef
---     asm' <- annotAsm asm
---     liftIO $ writeIORef asmRef asm'
 annotAsm :: Asm -> WithEnv Asm
 annotAsm (meta :< AsmReturn x) = return $ meta {asmMetaUse = [x]} :< AsmReturn x
 annotAsm (meta :< AsmMov x y cont) = do
   cont' <- annotAsm cont
   return $
     meta {asmMetaUse = varsInAsmArg y, asmMetaDef = [x]} :< AsmMov x y cont'
+annotAsm (meta :< AsmLoadWithOffset x y i cont) = do
+  cont' <- annotAsm cont
+  return $
+    meta {asmMetaUse = [y], asmMetaDef = [x]} :< AsmLoadWithOffset x y i cont'
+annotAsm (meta :< AsmStoreWithOffset x arg i cont) = do
+  cont' <- annotAsm cont
+  return $
+    meta {asmMetaUse = varsInAsmArg arg, asmMetaDef = [x]} :<
+    AsmStoreWithOffset x arg i cont'
 annotAsm (meta :< AsmCall x fun args cont) = do
   cont' <- annotAsm cont
   return $
     meta {asmMetaUse = fun : args, asmMetaDef = [x]} :< AsmCall x fun args cont'
-annotAsm (meta :< AsmLoadAddr x addr cont) = do
-  let used = varsInAddr addr
-  return $ meta {asmMetaUse = used, asmMetaDef = [x]} :< AsmLoadAddr x addr cont
 annotAsm (meta :< AsmPush x cont) = do
   cont' <- annotAsm cont
   return $ meta {asmMetaDef = [x]} :< AsmPush x cont'
@@ -44,22 +43,10 @@ annotAsm (meta :< AsmPop x cont) = do
   cont' <- annotAsm cont
   return $ meta {asmMetaUse = [x]} :< AsmPop x cont'
 
-varsInAddr :: Addr -> [Identifier]
-varsInAddr (AddrReg x)     = [x]
-varsInAddr (AddrInt _)     = []
-varsInAddr (AddrAdd a1 a2) = varsInAddr a1 ++ varsInAddr a2
-
 varsInAsmArg :: AsmArg -> [Identifier]
 varsInAsmArg (AsmArgReg x)       = [x]
 varsInAsmArg (AsmArgImmediate _) = []
 
--- livenessAnalysis :: WithEnv ()
--- livenessAnalysis = do
---   env <- get
---   forM_ (asmEnv env) $ \(_, (_, asmRef)) -> do
---     asm <- liftIO $ readIORef asmRef
---     asm' <- computeLiveness asm
---     liftIO $ writeIORef asmRef asm'
 computeLiveness :: Asm -> WithEnv Asm
 computeLiveness (meta :< AsmReturn x) = do
   contElems <- computeSuccAll (meta :< AsmReturn x)
@@ -68,14 +55,18 @@ computeLiveness (meta :< AsmMov x y cont) = do
   cont' <- computeLiveness cont
   contElemList <- computeSuccAll (meta :< AsmMov x y cont')
   computeLiveness' meta contElemList (AsmMov x y cont')
+computeLiveness (meta :< AsmLoadWithOffset x y i cont) = do
+  cont' <- computeLiveness cont
+  contElemList <- computeSuccAll (meta :< AsmLoadWithOffset x y i cont')
+  computeLiveness' meta contElemList (AsmLoadWithOffset x y i cont')
+computeLiveness (meta :< AsmStoreWithOffset x y i cont) = do
+  cont' <- computeLiveness cont
+  contElemList <- computeSuccAll (meta :< AsmStoreWithOffset x y i cont')
+  computeLiveness' meta contElemList (AsmStoreWithOffset x y i cont')
 computeLiveness (meta :< AsmCall x fun args cont) = do
   cont' <- computeLiveness cont
   contElemList <- computeSuccAll (meta :< AsmCall x fun args cont')
   computeLiveness' meta contElemList (AsmCall x fun args cont')
-computeLiveness (meta :< AsmLoadAddr x addr cont) = do
-  cont' <- computeLiveness cont
-  contElemList <- computeSuccAll (meta :< AsmLoadAddr x addr cont')
-  computeLiveness' meta contElemList (AsmLoadAddr x addr cont')
 computeLiveness (meta :< AsmPush x cont) = do
   cont' <- computeLiveness cont
   contElemList <- computeSuccAll (meta :< AsmPush x cont')
@@ -107,10 +98,13 @@ computeSuccAll (meta :< AsmReturn _) = return $ computeCurrent' meta
 computeSuccAll (meta :< AsmMov _ _ cont) = do
   contLvs <- computeSuccAll cont
   return $ next meta contLvs
-computeSuccAll (meta :< AsmCall _ _ _ cont) = do
+computeSuccAll (meta :< AsmLoadWithOffset _ _ _ cont) = do
   contLvs <- computeSuccAll cont
   return $ next meta contLvs
-computeSuccAll (meta :< AsmLoadAddr _ _ cont) = do
+computeSuccAll (meta :< AsmStoreWithOffset _ _ _ cont) = do
+  contLvs <- computeSuccAll cont
+  return $ next meta contLvs
+computeSuccAll (meta :< AsmCall _ _ _ cont) = do
   contLvs <- computeSuccAll cont
   return $ next meta contLvs
 computeSuccAll (meta :< AsmPush _ cont) = do
