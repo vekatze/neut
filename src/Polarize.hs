5cbf23eca9af8f5a2ccd46de5fe68458533b1f01
vekatze <vekatze@icloud.com>
2018-10-14 11:23:50 +0900

    preserve type information when polarizing terms

--- src/Polarize.hs
+++ src/Polarize.hs
@@ -20,91 +20,204 @@ import Data.Maybe (maybeToList)
 polarize :: [(Identifier, Neut)] -> Neut -> WithEnv Neg
 polarize [] final = polarizeNeg final
 polarize ((x, e):rest) final = do
-  cont <- polarize rest final
-  e' <- polarizeNeg e
-  return $ NegUpElim x e' cont
+  lookupTypeEnv' x >>= polarizePos >>= \(Pos t) -> insPolTypeEnv x t
+  Neg cont@(codMeta :< _) <- polarize rest final
+  Neg e' <- polarizeNeg e
+  codType <- lookupPolTypeEnv' codMeta
+  meta <- newNameWith "meta"
+  insPolTypeEnv meta codType
+  return $ Neg $ meta :< NegUpElim x e' cont
 
 polarizeNeg :: Neut -> WithEnv Neg
-polarizeNeg (_ :< NeutVar s) = return $ NegUpIntro $ PosVar s
-polarizeNeg (_ :< NeutConst s _) = return $ NegUpIntro $ PosConst s
-polarizeNeg pi@(_ :< NeutPi _ _) = NegUpIntro <$> polarizePos pi
-polarizeNeg lam@(_ :< NeutPiIntro _ _) = NegUpIntro <$> polarizePos lam
-polarizeNeg (_ :< NeutPiElim e1 e2) = do
+polarizeNeg e@(_ :< NeutVar _) = posRet e
+polarizeNeg e@(_ :< NeutConst _ _) = posRet e
+polarizeNeg pi@(_ :< NeutPi _ _) = posRet pi
+polarizeNeg lam@(_ :< NeutPiIntro _ _) = posRet lam
+polarizeNeg (meta :< NeutPiElim e1@(meta1 :< _) e2@(meta2 :< _)) = do
+  (_, retMeta) <- lookupTypeEnv' meta >>= polarizePos >>= toRetType'
   f <- newNameWith "pi"
   v <- newNameWith "arg"
-  bindSeq [(v, e2), (f, e1)] (NegPiElim (NegDownElim (PosVar f)) (PosVar v))
-polarizeNeg exists@(_ :< NeutSigma _ _) = NegUpIntro <$> polarizePos exists
-polarizeNeg (_ :< NeutSigmaIntro es) = do
+  Pos t1 <- lookupTypeEnv' meta1 >>= polarizePos
+  Pos t2 <- lookupTypeEnv' meta2 >>= polarizePos
+  case t1 of
+    _ :< PosDown tfun -> do
+      insPolTypeEnv meta1 t1
+      insPolTypeEnv meta2 t2
+      funMeta <- newNameWith "meta"
+      insPolTypeEnv funMeta tfun
+      bindSeq
+        [(v, e2), (f, e1)]
+        (Neg $
+         retMeta :<
+         NegPiElim
+           (funMeta :< NegDownElim (Pos $ meta1 :< PosVar f))
+           (Pos $ meta2 :< PosVar v))
+    _ -> lift $ throwE "Polarize.polarize: type error"
+polarizeNeg e@(_ :< NeutSigma _ _) = posRet e
+polarizeNeg (meta :< NeutSigmaIntro es) = do
   nameList <- mapM (const newName) es
-  let nameList' = map PosVar nameList
-  bindSeq (zip nameList es) (NegUpIntro (PosSigmaIntro nameList'))
-polarizeNeg (_ :< NeutSigmaElim e1 xs e2) = do
-  e2' <- polarizeNeg e2
+  nameList' <- mapM toPosVar nameList
+  (_, retMeta) <- lookupTypeEnv' meta >>= polarizePos >>= toRetType'
+  Pos tSigma <- lookupTypeEnv' meta >>= polarizePos
+  sigmaMeta <- newNameWith "meta"
+  insPolTypeEnv sigmaMeta tSigma
+  bindSeq
+    (zip nameList es)
+    (Neg $ retMeta :< NegUpIntro (Pos $ sigmaMeta :< PosSigmaIntro nameList'))
+polarizeNeg (meta :< NeutSigmaElim e1@(sigmaMeta :< _) xs e2) = do
+  Neg e2' <- polarizeNeg e2
   z <- newNameWith "sigma"
-  bindSeq [(z, e1)] (NegSigmaElim (PosVar z) xs e2')
-polarizeNeg box@(_ :< NeutBox _) = NegUpIntro <$> polarizePos box
-polarizeNeg (_ :< NeutBoxIntro e) = do
-  e' <- polarizeNeg e
-  return $ NegUpIntro $ PosDownIntro $ NegBoxIntro e'
-polarizeNeg (_ :< NeutBoxElim e) = do
+  (_, retMeta) <- lookupTypeEnv' meta >>= polarizePos >>= toRetType'
+  Pos tSigma <- lookupTypeEnv' sigmaMeta >>= polarizePos
+  insPolTypeEnv sigmaMeta tSigma
+  bindSeq
+    [(z, e1)]
+    (Neg $ retMeta :< NegSigmaElim (Pos $ sigmaMeta :< PosVar z) xs e2')
+polarizeNeg e@(_ :< NeutBox _) = posRet e
+polarizeNeg e@(_ :< NeutBoxIntro _) = posRet e
+polarizeNeg (meta :< NeutBoxElim e@(boxMeta :< _)) = do
   z <- newNameWith "box"
-  bindSeq [(z, e)] (NegBoxElim $ NegDownElim (PosVar z))
-polarizeNeg t@(_ :< NeutIndex _) = NegUpIntro <$> polarizePos t
-polarizeNeg e@(_ :< NeutIndexIntro _) = NegUpIntro <$> polarizePos e
-polarizeNeg (_ :< NeutIndexElim e branchList) = do
+  (_, retMeta) <- lookupTypeEnv' meta >>= polarizePos >>= toRetType'
+  Pos boxType <- lookupTypeEnv' boxMeta >>= polarizePos
+  insPolTypeEnv boxMeta boxType
+  bindSeq
+    [(z, e)]
+    (Neg $
+     retMeta :<
+     (NegBoxElim $ retMeta :< NegDownElim (Pos $ boxMeta :< PosVar z)))
+polarizeNeg t@(_ :< NeutIndex _) = posRet t
+polarizeNeg e@(_ :< NeutIndexIntro _) = posRet e
+polarizeNeg (meta :< NeutIndexElim e@(indexMeta :< _) branchList) = do
   let (labelList, es) = unzip branchList
   cs <- mapM polarizeNeg es
+  let cs' = map toPreNeg cs
   x <- newName
-  bindSeq [(x, e)] $ NegIndexElim (PosVar x) (zip labelList cs)
+  (_, retMeta) <- lookupTypeEnv' meta >>= polarizePos >>= toRetType'
+  Pos indexType <- lookupTypeEnv' indexMeta >>= polarizePos
+  insPolTypeEnv indexMeta indexType
+  bindSeq
+    [(x, e)]
+    (Neg $
+     retMeta :< NegIndexElim (Pos $ indexMeta :< PosVar x) (zip labelList cs'))
 polarizeNeg (_ :< NeutMu _ _) = error "polarizeNeg.polarizeNeg: unreachable: Mu"
-polarizeNeg t@(_ :< NeutUniv _) = NegUpIntro <$> polarizePos t
+polarizeNeg t@(_ :< NeutUniv _) = do
+  t' <- polarizePos t
+  meta <- newNameWith "meta"
+  return $ Neg $ meta :< NegUpIntro t'
 polarizeNeg (_ :< NeutHole x) =
   error $ "PolarizeNeg.polarizeNeg: remaining hole: " ++ x
 
 polarizePos :: Neut -> WithEnv Pos
-polarizePos (_ :< NeutVar s) = return $ PosVar s
-polarizePos (_ :< NeutConst s _) = return $ PosConst s
-polarizePos (_ :< NeutPi (x, tdom) tcod) = do
-  tdom' <- polarizePos tdom
-  tcod' <- PosUp <$> polarizePos tcod
-  return $ PosDown $ PosPi (x, tdom') tcod'
-polarizePos (_ :< NeutPiIntro (x, _) e) = do
-  e' <- polarizeNeg e
-  return $ PosDownIntro (NegPiIntro x e')
+polarizePos (meta :< NeutVar s) = do
+  Pos t <- lookupTypeEnv' meta >>= polarizePos
+  insPolTypeEnv meta t
+  insPolTypeEnv s t
+  return $ Pos $ meta :< PosVar s
+polarizePos (meta :< NeutConst s _) = do
+  Pos t <- lookupTypeEnv' meta >>= polarizePos
+  insPolTypeEnv meta t
+  insPolTypeEnv s t
+  return $ Pos $ meta :< PosConst s
+polarizePos (meta :< NeutPi (x, tdom) tcod) = do
+  Pos tdom' <- polarizePos tdom
+  insPolTypeEnv x tdom'
+  Pos tcod' <- polarizePos tcod
+  tcod'' <- wrap $ PosUp tcod'
+  newMeta <- newNameWith "meta"
+  return $ Pos $ newMeta :< (PosDown $ meta :< PosPi (x, tdom') tcod'')
+polarizePos (meta :< NeutPiIntro (x, _) e) = do
+  Neg e'@(eMeta :< _) <- polarizeNeg e
+  Pos tx <- lookupTypeEnv' x >>= polarizePos
+  insPolTypeEnv x tx
+  eType <- lookupPolTypeEnv' eMeta
+  lamMeta <- newNameWith "meta"
+  tmp <- newNameWith "meta"
+  insPolTypeEnv lamMeta $ tmp :< PosPi (x, tx) eType
+  lookupTypeEnv' meta >>= polarizePos >>= \(Pos te) -> insPolTypeEnv meta te
+  return $ Pos $ meta :< PosDownIntro (Neg $ lamMeta :< NegPiIntro x e')
 polarizePos (_ :< NeutPiElim _ _) =
   lift $ throwE "Polarize.polarizePos.NeutPiElim"
-polarizePos (_ :< NeutSigma xts body) = do
-  body' <- polarizePos body
+polarizePos (meta :< NeutSigma xts body) = do
+  Pos body' <- polarizePos body
   let (xs, ts) = unzip xts
   ts' <- mapM polarizePos ts
-  let xts' = zip xs ts'
-  return $ PosSigma xts' body'
-polarizePos (_ :< NeutSigmaIntro es) = do
+  let xts' = zip xs $ map toPrePos ts'
+  forM_ xts' $ uncurry insPolTypeEnv
+  return $ Pos $ meta :< PosSigma xts' body'
+polarizePos (meta :< NeutSigmaIntro es) = do
   es' <- mapM polarizePos es
-  return $ PosSigmaIntro es'
+  let es'' = map toPrePos es'
+  lookupTypeEnv' meta >>= polarizePos >>= \(Pos te) -> insPolTypeEnv meta te
+  return $ Pos $ meta :< PosSigmaIntro es''
 polarizePos (_ :< NeutSigmaElim {}) =
   lift $ throwE "Polarize.polarizePos.NeutSigmaElim"
-polarizePos (_ :< NeutBox e) = do
-  e' <- polarizePos e
-  return $ PosDown $ PosBox $ PosUp e'
-polarizePos (_ :< NeutBoxIntro e) = do
-  e' <- polarizeNeg e
-  return $ PosDownIntro $ NegBoxIntro e'
+polarizePos (meta :< NeutBox e) = do
+  Pos e' <- polarizePos e
+  e'' <- wrap $ PosUp e'
+  newMeta <- newNameWith "meta"
+  return $ Pos $ meta :< (PosDown $ newMeta :< PosBox e'')
+polarizePos (meta :< NeutBoxIntro e) = do
+  Neg e'@(eMeta :< _) <- polarizeNeg e
+  eType <- lookupPolTypeEnv' eMeta
+  boxMeta <- newNameWith "meta"
+  tmp <- newNameWith "meta"
+  insPolTypeEnv boxMeta $ tmp :< PosBox eType
+  lookupTypeEnv' meta >>= polarizePos >>= \(Pos te) -> insPolTypeEnv meta te
+  return $ Pos $ meta :< (PosDownIntro $ Neg $ boxMeta :< NegBoxIntro e')
 polarizePos (_ :< NeutBoxElim _) =
   lift $ throwE "Polarize.polarizePos.NeutBoxElim"
-polarizePos (_ :< NeutIndex l) = return $ PosIndex l
-polarizePos (_ :< NeutIndexIntro x) = return $ PosIndexIntro x
+polarizePos (meta :< NeutIndex l) = return $ Pos $ meta :< PosIndex l
+polarizePos (meta :< NeutIndexIntro l) = do
+  Pos t <- lookupTypeEnv' meta >>= polarizePos
+  insPolTypeEnv meta t
+  case l of
+    IndexLabel x -> do
+      insPolTypeEnv x t
+      return $ Pos $ meta :< PosIndexIntro l
+    _ -> return $ Pos $ meta :< PosIndexIntro l
 polarizePos (_ :< NeutIndexElim _ _) =
   lift $ throwE "Polarize.polarizePos.NeutIndexElim"
 polarizePos (_ :< NeutMu _ _) = lift $ throwE "Polarize.polarizePos.NeutMu"
-polarizePos (_ :< NeutUniv _) = return PosUniv
+polarizePos (meta :< NeutUniv _) = return $ Pos $ meta :< PosUniv
 polarizePos (_ :< NeutHole _) = lift $ throwE "Polarize.polarizePos.NeutHole"
 
 bindSeq :: [(Identifier, Neut)] -> Neg -> WithEnv Neg
 bindSeq [] fun = return fun
-bindSeq ((formalArg, arg@(argMeta :< _)):rest) fun = do
-  arg' <- polarizeNeg arg
-  fun' <- bindSeq rest fun
+bindSeq ((formalArg, arg@(argMeta :< _)):rest) fun@(Neg (funMeta :< _)) = do
+  Neg arg' <- polarizeNeg arg
+  Neg fun' <- bindSeq rest fun
   argType <- lookupTypeEnv' argMeta
   insTypeEnv formalArg argType
-  return $ NegUpElim formalArg arg' fun'
+  codType <- lookupPolTypeEnv' funMeta
+  meta <- newNameOfPolType codType
+  return $ Neg $ meta :< NegUpElim formalArg arg' fun'
+
+toPrePos :: Pos -> PrePos
+toPrePos (Pos e) = e
+
+toPreNeg :: Neg -> PreNeg
+toPreNeg (Neg e) = e
+
+toRetType' :: Pos -> WithEnv (PrePos, Identifier)
+toRetType' (Pos t) = toRetType t
+
+toRetType :: PrePos -> WithEnv (PrePos, Identifier)
+toRetType t = do
+  tmp <- newNameWith "meta"
+  let retType = tmp :< PosUp t
+  retMeta <- newNameWith "meta"
+  insPolTypeEnv retMeta retType
+  return (tmp :< PosUp t, retMeta)
+
+toPosVar :: Identifier -> WithEnv PrePos
+toPosVar x = do
+  Pos t <- lookupTypeEnv' x >>= polarizePos
+  meta <- newNameWith "meta"
+  insPolTypeEnv meta t
+  return $ meta :< PosVar x
+
+posRet :: Neut -> WithEnv Neg
+posRet e@(meta :< _) = do
+  e' <- polarizePos e
+  (_, retMeta) <- lookupTypeEnv' meta >>= polarizePos >>= toRetType'
+  return $ Neg $ retMeta :< NegUpIntro e'
