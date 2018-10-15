b2d7880deb857e8be13e993a4def5b797593d516
u2zv1wx <u2zv1wx@protonmail.ch>
2018-10-15 14:43:52 +0900

    reimplement Asm

--- src/Asm.hs
+++ src/Asm.hs
@@ -12,7 +12,7 @@ import Data.IORef
 
 import Data
 import Reduce
-import Register
+import Util
 
 import Control.Comonad.Cofree
 
@@ -24,112 +24,135 @@ asmCodeEnv :: WithEnv ()
 asmCodeEnv = do
   env <- get
   forM_ (codeEnv env) $ \(name, (args, codeRef)) -> do
+    xs <- mapM (const createIntVar) args
     code <- liftIO $ readIORef codeRef
-    initRegVar
-    argRegList <- getArgRegList
     asm <- asmCode code
-    asm' <- bindArgs (zip args argRegList) asm
-    insAsmEnv name asm'
-    regAlloc 15 asm'
+    asm' <- asmCastFromInt64 (zip xs args) asm
+    insAsmEnv name xs asm'
+
+createIntVar :: WithEnv Identifier
+createIntVar = do
+  intMeta <- newNameWith "meta"
+  x <- newNameWith "arg"
+  insValueTypeEnv x $ Value $ intMeta :< ValueIndex "i64"
+  return x
 
 asmCode :: Code -> WithEnv Asm
-asmCode (CodeReturn d) = do
-  rax <- getRAX
-  tmp <- addMeta $ AsmReturn rax
-  asmData rax d tmp
-asmCode (CodeLet i d cont) = do
-  cont' <- asmCode cont
-  asmData i d cont'
+asmCode (CodeReturn (d, t)) = do
+  castedResult <- newNameWith "cast"
+  intMeta <- newNameWith "meta"
+  insValueTypeEnv castedResult $ Value $ intMeta :< ValueIndex "i64"
+  ret <- addMeta $ AsmReturn (DataLocal castedResult, t)
+  result <- newNameWith "tmp"
+  insValueTypeEnv result t
+  asmCastToInt64 [((d, t), castedResult)] ret
 asmCode (CodeCall x fun args cont) = do
   cont' <- asmCode cont
-  if length args > 6
-    then lift $ throwE "Asm.asmCode: the number of arguments exceeds 6 (FIXME)"
-    else asmCodeCall x (AsmDataReg fun) args cont'
+  asmCodeCall x fun args cont'
+asmCode (CodeCallTail fun args) = do
+  xs <- mapM (const createIntVar) args
+  let ts = map snd args
+  call <- addMeta $ AsmCallTail fun $ zip (map DataLocal xs) ts
+  asmCastToInt64 (zip args xs) call
 asmCode (CodeSwitch x branchList) = asmSwitch x branchList
 asmCode (CodeExtractValue x basePointer i cont) = do
-  undefined
-  -- let offset = sum $ map sizeOfLowType $ take i ts
-  -- cont' <- asmCode cont
-  -- addMeta $ AsmExtractValue x basePointer offset cont'
-asmCode (CodeFree x cont) = do
-  tmp <- newName
   cont' <- asmCode cont
-  asmCodeCall tmp (AsmDataLabel "_free") [x] cont'
+  addMeta $ AsmGetElementPtr x basePointer i cont'
+asmCode (CodeFree x cont) = do
+  cont' <- asmCode cont
+  addMeta $ AsmFree x cont'
 
-asmData :: Identifier -> Data -> Asm -> WithEnv Asm
-asmData reg (DataLocal x) cont = addMeta $ AsmLet reg (AsmDataReg x) cont
-asmData reg (DataLabel x) cont = addMeta $ AsmLet reg (AsmDataLabel x) cont
-asmData reg (DataInt32 i) cont = addMeta $ AsmLet reg (AsmDataImmediate i) cont
-asmData reg (DataStruct ds) cont = do
-  xs <- mapM (const newName) ds
-  let sizeList = map sizeOfData ds
-  let structSize = sum sizeList
-  cont' <- setContent reg sizeList (zip [0 ..] xs) cont
-  rdi <- getRDI
-  callThenCont <- asmCodeCall reg (AsmDataLabel "_malloc") [rdi] cont'
-  tmp <- addMeta $ AsmLet rdi (AsmDataImmediate structSize) callThenCont
-  asmStruct (zip xs ds) tmp
-asmData reg (DataClosure cls envName fv) cont = do
-  let sizeList = map (const 8) fv
-  cont' <- asmData reg (DataStruct [DataLabel cls, DataLocal envName]) cont
-  cont'' <- setContent envName sizeList (zip [0 ..] fv) cont'
-  rdi <- getRDI
-  tmp <- asmCodeCall envName (AsmDataLabel "_malloc.closure.fvEnv") [rdi] cont''
-  addMeta $ AsmLet rdi (AsmDataImmediate 16) tmp
+constructSwitch :: DataPlus -> [(Index, Code)] -> WithEnv (Asm, [(Int, Asm)])
+constructSwitch _ [] = lift $ throwE "empty branch"
+constructSwitch _ [(_, code)] = do
+  code' <- asmCode code
+  return (code', [])
+constructSwitch (name, _) ((IndexLabel x, code):_) = do
+  code' <- asmCode $ substCode [(x, name)] code
+  return (code', [])
+constructSwitch _ ((IndexDefault, code):_) = do
+  code' <- asmCode code
+  return (code', [])
+constructSwitch name ((IndexInteger i, code):rest) = do
+  code' <- asmCode code
+  (defaultCase, caseList) <- constructSwitch name rest
+  return (defaultCase, (i, code') : caseList)
 
-asmStruct :: [(Identifier, Data)] -> Asm -> WithEnv Asm
-asmStruct [] cont = return cont
-asmStruct ((x, d):xds) cont = do
-  cont' <- asmStruct xds cont
-  asmData x d cont'
+asmSwitch :: DataPlus -> [(Index, Code)] -> WithEnv Asm
+asmSwitch name branchList = do
+  (defaultCase, caseList) <- constructSwitch name branchList
+  addMeta $ AsmSwitch name defaultCase caseList
 
-asmSwitch :: Identifier -> [(Index, Code)] -> WithEnv Asm
-asmSwitch _ [] = lift $ throwE "empty branch"
-asmSwitch _ [(_, code)] = asmSwitchLast code -- FIXME (bind the argument)
-asmSwitch _ ((IndexDefault, code):_) = asmSwitchLast code
-asmSwitch name ((i, code):rest) = do
-  cont <- asmSwitch name rest
-  asm <- asmCode code
-  x <- newNameWith "case"
-  let label = x ++ "." ++ showIndex i
-  cont' <- addMeta $ AsmJumpIfZero (label, asm) cont
-  addMeta $ AsmCompare name label cont'
-
-asmSwitchLast :: Code -> WithEnv Asm
-asmSwitchLast code = do
-  asm <- asmCode code
-  label <- newNameWith "default"
-  addMeta $ AsmJump (label, asm)
-
-setContent :: Identifier -> [Int] -> [(Int, Identifier)] -> Asm -> WithEnv Asm
-setContent _ _ [] cont = return cont
-setContent basePointer sizeList ((i, dataAtIndex):sizeDataList) cont = do
-  cont' <- setContent basePointer sizeList sizeDataList cont
-  let offset = sum $ take i sizeList
-  addMeta $ AsmInsertValue (AsmDataReg dataAtIndex) basePointer offset cont'
-
-asmCodeCall :: Identifier -> AsmData -> [Identifier] -> Asm -> WithEnv Asm
+asmCodeCall :: Identifier -> DataPlus -> [DataPlus] -> Asm -> WithEnv Asm
 asmCodeCall x fun args cont = do
-  argRegList <- getArgRegList
-  rax <- getRAX
-  cont' <- addMeta $ AsmLet x (AsmDataReg rax) cont
-  call <- addMeta $ AsmCall rax fun args cont'
-  bindArgs (zip argRegList args) call
+  resInt <- createIntVar
+  cont' <- asmCastFromInt64 [(resInt, x)] cont
+  xs <- mapM (const createIntVar) args
+  let xs' = map DataLocal xs
+  let ts = map snd args
+  call <- addMeta $ AsmCall resInt fun (zip xs' ts) cont'
+  asmCastToInt64 (zip args xs) call
 
-bindArgs :: [(Identifier, Identifier)] -> Asm -> WithEnv Asm
-bindArgs [] asm = return asm
-bindArgs ((to, from):rest) asm = do
-  asm' <- bindArgs rest asm
-  addMeta $ AsmLet to (AsmDataReg from) asm'
+asmCastToInt64 :: [(DataPlus, Identifier)] -> Asm -> WithEnv Asm
+asmCastToInt64 [] asm = return asm
+asmCastToInt64 (((from, Value fromType), to):rest) asm = do
+  asm' <- asmCastToInt64 rest asm
+  toInt64 (from, fromType) to asm'
 
-showIndex :: Index -> String
-showIndex (IndexInteger i) = show i
-showIndex (IndexLabel s) = s
-showIndex IndexDefault = "default"
+toInt64 :: (Data, PreValue) -> Identifier -> Asm -> WithEnv Asm
+toInt64 (from, fromType@(_ :< ValueIndex i)) to cont
+  | i `elem` intTypeList = do
+    int64 <- getInt64Type
+    addMeta $ AsmZeroExtend to (from, Value fromType) int64 cont
+toInt64 (from, fromType@(_ :< ValueIndex "f32")) to cont = do
+  int32 <- getInt32Type
+  int64 <- getInt64Type
+  tmp <- newNameWith "cast"
+  cont' <- addMeta $ AsmZeroExtend to (DataLocal tmp, int32) int64 cont
+  addMeta $ AsmBitcast tmp (from, Value fromType) int32 cont'
+toInt64 (from, fromType@(_ :< ValueIndex "f64")) to cont = do
+  int64 <- getInt64Type
+  addMeta $ AsmBitcast to (from, Value fromType) int64 cont
+toInt64 (from, meta :< ValueIndex _) to cont =
+  toInt64 (from, meta :< ValueIndex "i64") to cont
+toInt64 (from, fromType) to cont = do
+  int64 <- getInt64Type
+  addMeta $ AsmPointerToInt to (from, Value fromType) int64 cont
 
-sizeOfData :: Data -> Int
-sizeOfData (DataLocal _) = 8
-sizeOfData (DataLabel _) = 8
-sizeOfData (DataInt32 _) = 8
-sizeOfData (DataStruct ds) = sum $ map sizeOfData ds
-sizeOfData DataClosure {} = 16
+asmCastFromInt64 :: [(Identifier, Identifier)] -> Asm -> WithEnv Asm
+asmCastFromInt64 [] asm = return asm
+asmCastFromInt64 ((from, to):rest) asm = do
+  asm' <- asmCastFromInt64 rest asm
+  Value targetType <- lookupValueTypeEnv' to
+  fromInt64 from (to, targetType) asm'
+
+fromInt64 :: Identifier -> (Identifier, PreValue) -> Asm -> WithEnv Asm
+fromInt64 from (to, toType@(_ :< ValueIndex i)) cont
+  | i `elem` intTypeList = do
+    fromType <- lookupValueTypeEnv' from
+    addMeta $ AsmTrunc to (DataLocal from, fromType) (Value toType) cont
+fromInt64 from (to, toType@(_ :< ValueIndex "f32")) cont = do
+  int32 <- getInt32Type
+  tmp <- newNameWith "cast"
+  cont' <- addMeta $ AsmBitcast to (DataLocal tmp, int32) (Value toType) cont
+  fromType <- lookupValueTypeEnv' from
+  addMeta $ AsmTrunc tmp (DataLocal from, fromType) int32 cont'
+fromInt64 from (to, toType@(_ :< ValueIndex "f64")) cont = do
+  fromType <- lookupValueTypeEnv' from
+  addMeta $ AsmBitcast to (DataLocal from, fromType) (Value toType) cont
+fromInt64 from (to, toType) cont = do
+  fromType <- lookupValueTypeEnv' from
+  addMeta $ AsmIntToPointer to (DataLocal from, fromType) (Value toType) cont
+
+getInt32Type :: WithEnv Value
+getInt32Type = do
+  meta <- newNameWith "meta"
+  return $ Value $ meta :< ValueIndex "i32"
+
+getInt64Type :: WithEnv Value
+getInt64Type = do
+  meta <- newNameWith "meta"
+  return $ Value $ meta :< ValueIndex "i64"
+
+intTypeList :: [Identifier]
+intTypeList = ["i8", "i16", "i32", "i64"]
