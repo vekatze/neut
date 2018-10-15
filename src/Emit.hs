b2f1320bf1a98661e29b1ae4a1f43cabde7ec299
u2zv1wx <u2zv1wx@protonmail.ch>
2018-10-15 15:59:25 +0900

    reimplement Emit

--- src/Emit.hs
+++ src/Emit.hs
@@ -3,110 +3,171 @@ module Emit
   , emitGlobalLabel
   ) where
 
-import           Control.Monad
-import           Control.Monad.Except
-import           Control.Monad.Identity
-import           Control.Monad.State
-import           Control.Monad.Trans.Except
-import           Data.IORef
+import Prelude hiding (showList)
 
-import           Data
+import Control.Monad
+import Control.Monad.Except
+import Control.Monad.Identity
+import Control.Monad.State
+import Control.Monad.Trans.Except
+import Data.IORef
 
-import           Control.Comonad.Cofree
+import Data
 
-import qualified Text.Show.Pretty           as Pr
+import Control.Comonad.Cofree
 
-import           Data.List
+import qualified Text.Show.Pretty as Pr
 
-import           Debug.Trace
+import Data.List
+
+import Debug.Trace
 
 emit :: WithEnv ()
 emit = do
   env <- get
-  forM_ (asmEnv env) $ uncurry emit'
+  forM_ (asmEnv env) $ uncurry emitDefinition
 
-emit' name asm = do
+emitDefinition :: Identifier -> ([Identifier], Asm) -> WithEnv ()
+emitDefinition name (args, asm) = do
+  let name' = DataLabel name
+  ts <- mapM lookupValueTypeEnv' args
+  let args' = map (\(a, t) -> (DataLocal a, t)) $ zip args ts
+  liftIO $ putStrLn $ "define i64 " ++ showData name' ++ showArgs args' ++ "{"
+  emitAsm asm
+  liftIO $ putStrLn "}"
+
+emitBlock :: Identifier -> Asm -> WithEnv ()
+emitBlock name asm = do
   emitLabel name
-  modify (\e -> e {sizeEnv = []})
-  computeSizeList asm
-  asm' <- insExtendShrink name asm
-  asm'' <- insSaveRestore asm'
-  emitAsm asm''
+  emitAsm asm
 
 emitAsm :: Asm -> WithEnv ()
-emitAsm (_ :< AsmReturn _) = emitOp $ unwords ["ret"]
-emitAsm (_ :< AsmLet x (AsmDataReg y) cont) = do
-  x' <- showReg x
-  y' <- showReg y
-  emitOp $ unwords ["movq", y' ++ ",", x']
+emitAsm (_ :< AsmReturn (d, t)) =
+  emitOp $ unwords ["ret", showType t, showData d]
+emitAsm (_ :< AsmGetElementPtr x (base, t) i cont) = do
+  emitOp $
+    unwords
+      [ x
+      , "= getelementptr"
+      , showTypeNoPtr t ++ ","
+      , showType t
+      , showData base ++ ","
+      , showIndex [0, i]
+      ]
   emitAsm cont
-emitAsm (_ :< AsmLet x (AsmDataLabel label) cont) = do
-  x' <- showReg x
-  emitOp $ unwords ["leaq", label ++ "(%rip),", x']
+emitAsm (_ :< AsmCall x (f, _) args cont) = do
+  tx <- lookupValueTypeEnv' x
+  emitOp $
+    unwords
+      [ showData (DataLocal x)
+      , "="
+      , showType tx
+      , "call"
+      , showData f ++ "(" ++ showArgs args ++ ")"
+      ]
   emitAsm cont
-emitAsm (_ :< AsmLet x (AsmDataImmediate i) cont) = do
-  x' <- showReg x
-  emitOp $ unwords ["movq", show i ++ ",", x']
+emitAsm (_ :< AsmCallTail (f, t) args) = do
+  tmp <- newNameWith "tmp"
+  let tmpType = getCodType t
+  emitOp $
+    unwords
+      [ showData (DataLocal tmp)
+      , "="
+      , showType tmpType
+      , "tail call"
+      , showData f ++ "(" ++ showArgs args ++ ")"
+      ]
+  emitOp $ unwords ["ret", showType tmpType, showData (DataLocal tmp)]
+emitAsm (_ :< AsmBitcast x (d, fromType) toType cont) = do
+  emitOp $
+    unwords
+      [ showData (DataLocal x)
+      , "="
+      , "bitcast"
+      , showType fromType
+      , showData d
+      , "to"
+      , showType toType
+      ]
   emitAsm cont
-emitAsm (_ :< AsmExtractValue dest base offset cont) = do
-  base' <- showReg base
-  dest' <- showReg dest
-  emitOp $ unwords ["movq", showRegWithOffset offset base' ++ ",", dest']
+emitAsm (_ :< AsmZeroExtend x (d, fromType) toType cont) = do
+  emitOp $
+    unwords
+      [ showData (DataLocal x)
+      , "="
+      , "zext"
+      , showType fromType
+      , showData d
+      , "to"
+      , showType toType
+      ]
   emitAsm cont
-emitAsm (_ :< AsmInsertValue val base offset cont) = do
-  val' <- showAsmData val
-  base' <- showReg base
-  emitOp $ unwords ["movq", val' ++ ",", showRegWithOffset offset base']
+emitAsm (_ :< AsmTrunc x (d, fromType) toType cont) = do
+  emitOp $
+    unwords
+      [ showData (DataLocal x)
+      , "="
+      , "trunc"
+      , showType fromType
+      , showData d
+      , "to"
+      , showType toType
+      ]
   emitAsm cont
-emitAsm (_ :< AsmCall _ (AsmDataReg x) _ cont) = do
-  x' <- showReg x
-  emitOp $ unwords ["call", "*" ++ x']
+emitAsm (_ :< AsmIntToPointer x (d, fromType) toType cont) = do
+  emitOp $
+    unwords
+      [ showData (DataLocal x)
+      , "="
+      , "inttoptr"
+      , showType fromType
+      , showData d
+      , "to"
+      , showType toType
+      ]
   emitAsm cont
-emitAsm (_ :< AsmCall _ (AsmDataLabel label) _ cont) = do
-  emitOp $ unwords ["call", label]
+emitAsm (_ :< AsmPointerToInt x (d, fromType) toType cont) = do
+  emitOp $
+    unwords
+      [ showData (DataLocal x)
+      , "="
+      , "ptrtoint"
+      , showType fromType
+      , showData d
+      , "to"
+      , showType toType
+      ]
   emitAsm cont
-emitAsm (_ :< AsmCall _ (AsmDataImmediate _) _ _) =
-  liftIO $ putStrLn "emitAsm.AsmCall: immediate?"
-emitAsm (_ :< AsmCompare x y cont) = do
-  x' <- showReg x
-  y' <- showReg y
-  emitOp $ unwords ["cmp", x' ++ ",", y']
+emitAsm (_ :< AsmSwitch (d, _) defaultBranch branchList) = do
+  defaultLabel <- newNameWith "default"
+  labelList <- constructLabelList branchList
+  emitOp $
+    unwords
+      [ "switch"
+      , "i32"
+      , showData d ++ ","
+      , "label"
+      , defaultLabel
+      , showBranchList $ zip (map fst branchList) labelList
+      ]
+  let asmList = map snd branchList
+  forM_ ((defaultLabel, defaultBranch) : zip labelList asmList) $
+    uncurry emitBlock
+emitAsm (_ :< AsmFree (d, t) cont) = do
+  tmp <- newNameWith "tmp"
+  emitOp $
+    unwords
+      [ showData (DataLocal tmp)
+      , "="
+      , "bitcast"
+      , showType t
+      , showData d
+      , "to"
+      , "i8*"
+      ]
+  emitOp $
+    unwords ["call", "void", "@free(i8* " ++ showData (DataLocal tmp) ++ ")"]
   emitAsm cont
-emitAsm (_ :< AsmJumpIfZero (label, body) cont) = do
-  emitOp $ unwords ["jz", label]
-  emitAsm cont
-  emit' label body
-emitAsm (_ :< AsmJump (label, body)) = do
-  emitOp $ unwords ["jmp", label]
-  emit' label body
-emitAsm (meta :< AsmPush x cont) = do
-  rsp <- getRSP
-  offset <- computeOffset x
-  emitAsm $ meta :< AsmInsertValue (AsmDataReg x) rsp offset cont
-emitAsm (meta :< AsmPop dest cont) = do
-  rsp <- getRSP
-  offset <- computeOffset dest
-  emitAsm $ meta :< AsmExtractValue dest rsp offset cont
-emitAsm (_ :< AsmAddInt64 arg dest cont) = do
-  arg' <- showAsmData arg
-  dest' <- showReg dest
-  emitOp $ unwords ["addq", arg' ++ ",", dest']
-  emitAsm cont
-emitAsm (_ :< AsmSubInt64 arg dest cont) = do
-  arg' <- showAsmData arg
-  dest' <- showReg dest
-  emitOp $ unwords ["subq", arg' ++ ",", dest']
-  emitAsm cont
-
-showAsmData :: AsmData -> WithEnv String
-showAsmData (AsmDataReg x)       = showReg x
-showAsmData (AsmDataLabel x)     = return x
-showAsmData (AsmDataImmediate i) = return $ "$" ++ show i
-
-showReg :: Identifier -> WithEnv String
-showReg x = do
-  x' <- toRegName x
-  return $ "%" ++ x'
 
 emitOp :: String -> WithEnv ()
 emitOp s = liftIO $ putStrLn $ "  " ++ s
@@ -114,159 +175,69 @@ emitOp s = liftIO $ putStrLn $ "  " ++ s
 emitGlobalLabel :: Identifier -> WithEnv ()
 emitGlobalLabel label = liftIO $ putStrLn $ "  " ++ ".globl " ++ label
 
-showRegWithOffset :: Int -> Identifier -> Identifier
-showRegWithOffset offset x = show offset ++ "(" ++ x ++ ")"
-
 emitLabel :: String -> WithEnv ()
 emitLabel s = liftIO $ putStrLn $ s ++ ":"
 
-insSaveRestore :: Asm -> WithEnv Asm
-insSaveRestore (meta :< AsmReturn x) = return $ meta :< AsmReturn x
-insSaveRestore (meta :< AsmLet x y cont) = do
-  cont' <- insSaveRestore cont
-  return $ meta :< AsmLet x y cont'
-insSaveRestore (meta :< AsmInsertValue val offset base cont) = do
-  cont' <- insSaveRestore cont
-  return $ meta :< AsmInsertValue val offset base cont'
-insSaveRestore (meta :< AsmExtractValue offset base dest cont) = do
-  cont' <- insSaveRestore cont
-  return $ meta :< AsmExtractValue offset base dest cont'
-insSaveRestore (meta :< AsmCall x fun args cont) = do
-  let lvs = asmMetaLive meta
-  cont' <- insSaveRestore cont
-  cont'' <- insRestore lvs cont'
-  insSave lvs $ meta :< AsmCall x fun args cont''
-insSaveRestore (meta :< AsmCompare x y cont) = do
-  cont' <- insSaveRestore cont
-  return $ meta :< AsmCompare x y cont'
-insSaveRestore (meta :< AsmJumpIfZero label cont) = do
-  cont' <- insSaveRestore cont
-  return $ meta :< AsmJumpIfZero label cont'
-insSaveRestore (meta :< AsmJump label) = return $ meta :< AsmJump label
-insSaveRestore (meta :< AsmPush x cont) = do
-  cont' <- insSaveRestore cont
-  return $ meta :< AsmPush x cont'
-insSaveRestore (meta :< AsmPop x cont) = do
-  cont' <- insSaveRestore cont
-  return $ meta :< AsmPop x cont'
-insSaveRestore (meta :< AsmAddInt64 arg dest cont) = do
-  cont' <- insSaveRestore cont
-  return $ meta :< AsmAddInt64 arg dest cont'
-insSaveRestore (meta :< AsmSubInt64 arg dest cont) = do
-  cont' <- insSaveRestore cont
-  return $ meta :< AsmSubInt64 arg dest cont'
+constructLabelList :: [(Int, Asm)] -> WithEnv [String]
+constructLabelList [] = return []
+constructLabelList ((i, _):rest) = do
+  label <- newNameWith $ "case." ++ show i
+  labelList <- constructLabelList rest
+  return $ label : labelList
 
-insSave :: [Identifier] -> Asm -> WithEnv Asm
-insSave [] asm = return asm
-insSave (x:xs) asm = do
-  tmp <- insSave xs asm
-  addMeta $ AsmPush x tmp
+showBranchList :: [(Int, String)] -> String
+showBranchList xs = "[" ++ showList (uncurry showBranch) xs ++ "]"
 
-insRestore :: [Identifier] -> Asm -> WithEnv Asm
-insRestore [] asm = return asm
-insRestore (x:xs) asm = do
-  tmp <- insRestore xs asm
-  addMeta $ AsmPop x tmp
+showBranch :: Int -> String -> String
+showBranch i label =
+  "i32 " ++ show i ++ ", label " ++ showData (DataLocal label)
 
-computeSizeList :: Asm -> WithEnv ()
-computeSizeList (_ :< AsmReturn _) = return ()
-computeSizeList (_ :< AsmLet _ _ cont) = computeSizeList cont
-computeSizeList (_ :< AsmInsertValue _ _ _ cont) = computeSizeList cont
-computeSizeList (_ :< AsmExtractValue _ _ _ cont) = computeSizeList cont
-computeSizeList (_ :< AsmCall _ _ _ cont) = computeSizeList cont
-computeSizeList (_ :< AsmPush x cont) = do
-  computeSizeList cont
-  size <- sizeOf x
-  insSizeEnv x size
-computeSizeList (_ :< AsmCompare _ _ cont) = computeSizeList cont
-computeSizeList (_ :< AsmJumpIfZero _ cont) = computeSizeList cont
-computeSizeList (_ :< AsmJump _) = return ()
-computeSizeList (_ :< AsmPop _ cont) = computeSizeList cont
-computeSizeList (_ :< AsmAddInt64 _ _ cont) = computeSizeList cont
-computeSizeList (_ :< AsmSubInt64 _ _ cont) = computeSizeList cont
+showData :: Data -> String
+showData (DataLocal x) = "%" ++ x
+showData (DataLabel x) = "@" ++ x
+showData (DataInt32 i) = show i
+showData (DataStruct dts) = do
+  let ds = map fst dts
+  "{" ++ showList showData ds ++ "}*"
 
-computeOffset :: Identifier -> WithEnv Int
-computeOffset x = do
-  env <- get
-  let prefixList = takeWhile (\(y, _) -> x /= y) $ sizeEnv env
-  return $ sum $ map snd prefixList
+showIndex :: [Int] -> String
+showIndex [] = ""
+showIndex [i] = "i32 " ++ show i
+showIndex (i:is) = "i32 " ++ show i ++ ", " ++ showIndex is
 
--- extend/shrink the stack pointer before/after executing function body
-insExtendShrink :: Identifier -> Asm -> WithEnv Asm
-insExtendShrink name asm = do
-  asm' <- extendStack name asm
-  insShrink name asm'
+showArg :: DataPlus -> String
+showArg (d, t) = showType t ++ " " ++ showData d
 
-insShrink :: Identifier -> Asm -> WithEnv Asm
-insShrink name (meta :< AsmReturn x) = shrinkStack name $ meta :< AsmReturn x
-insShrink name (meta :< AsmLet x y cont) = do
-  cont' <- insShrink name cont
-  return $ meta :< AsmLet x y cont'
-insShrink name (meta :< AsmInsertValue val offset base cont) = do
-  cont' <- insShrink name cont
-  return $ meta :< AsmInsertValue val offset base cont'
-insShrink name (meta :< AsmExtractValue offset base dest cont) = do
-  cont' <- insShrink name cont
-  return $ meta :< AsmExtractValue offset base dest cont'
-insShrink name (meta :< AsmCall x fun args cont) = do
-  cont' <- insShrink name cont
-  return $ meta :< AsmCall x fun args cont'
-insShrink name (meta :< AsmCompare x y cont) = do
-  cont' <- insShrink name cont
-  return $ meta :< AsmCompare x y cont'
-insShrink name (meta :< AsmJumpIfZero label cont) = do
-  cont' <- insShrink name cont
-  return $ meta :< AsmJumpIfZero label cont'
-insShrink _ (meta :< AsmJump label) = return $ meta :< AsmJump label
-insShrink name (meta :< AsmPush x cont) = do
-  cont' <- insShrink name cont
-  return $ meta :< AsmPush x cont'
-insShrink name (meta :< AsmPop x cont) = do
-  cont' <- insShrink name cont
-  return $ meta :< AsmPop x cont'
-insShrink name (meta :< AsmAddInt64 arg dest cont) = do
-  cont' <- insShrink name cont
-  return $ meta :< AsmAddInt64 arg dest cont'
-insShrink name (meta :< AsmSubInt64 arg dest cont) = do
-  cont' <- insShrink name cont
-  return $ meta :< AsmSubInt64 arg dest cont'
+showArgs :: [DataPlus] -> String
+showArgs ds = "(" ++ showList showArg ds ++ ")"
 
-extendStack :: Identifier -> Asm -> WithEnv Asm
-extendStack name asm = do
-  size <- alignedSize name
-  rsp <- getRSP
-  addMeta $ AsmSubInt64 (AsmDataImmediate size) rsp asm
+showType :: Value -> String
+showType (Value (_ :< ValueVar _)) = "any*"
+showType (Value (_ :< ValueConst _)) = undefined
+showType (Value (_ :< ValuePi _ _)) = "i8*"
+showType (Value (_ :< ValueSigma xts t)) = do
+  let ts = map Value $ map snd xts ++ [t]
+  "{" ++ showList showType ts ++ "}*"
+showType (Value (_ :< ValueIndex i))
+  | i `elem` intTypeList = i
+showType (Value (_ :< ValueIndex "f32")) = "float"
+showType (Value (_ :< ValueIndex "f64")) = "double"
+showType (Value (_ :< ValueUp t)) = showType $ Value t
+showType (Value (_ :< ValueUniv)) = "i8"
+showType v = error $ "Emit.showType: " ++ show v ++ " is not a type"
 
-shrinkStack :: Identifier -> Asm -> WithEnv Asm
-shrinkStack name asm = do
-  size <- alignedSize name
-  rsp <- getRSP
-  addMeta $ AsmAddInt64 (AsmDataImmediate size) rsp asm
+getCodType :: Value -> Value
+getCodType (Value (_ :< ValuePi _ t)) = getCodType $ Value t
+getCodType t = t
 
-alignedSize :: Identifier -> WithEnv Int
-alignedSize name = do
-  env <- get
-  let x = sum $ map snd $ sizeEnv env
-  b <- isClause name
-  if b
-    then return $ alignedSize16 x
-    else return $ alignedSize8 x
+showTypeNoPtr :: Value -> String
+showTypeNoPtr (Value (_ :< ValuePi _ _)) = "i8"
+showTypeNoPtr (Value (_ :< ValueSigma xts t)) = do
+  let ts = map Value $ map snd xts ++ [t]
+  "{" ++ showList showType ts ++ "}"
+showTypeNoPtr v = error $ "Emit.showTypeNoPtr:\n" ++ Pr.ppShow v
 
--- find the least y such that y >= x && y mod 16 == 8
-alignedSize8 :: Int -> Int
-alignedSize8 x =
-  if x `mod` 16 == 8
-    then x
-    else alignedSize8 (x + 1)
-
-alignedSize16 :: Int -> Int
-alignedSize16 x =
-  if x `mod` 16 == 0
-    then x
-    else alignedSize16 (x + 1)
-
-isClause :: Identifier -> WithEnv Bool
-isClause name = do
-  env <- get
-  let labelList = join $ map snd $ indexEnv env
-  return $ name `elem` labelList
+showList :: (a -> String) -> [a] -> String
+showList _ [] = ""
+showList f [a] = f a
+showList f (a:as) = f a ++ ", " ++ showList f as
