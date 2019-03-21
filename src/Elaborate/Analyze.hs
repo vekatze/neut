ea7fdf5eea33a23500c1b93b4930bf29189ead59
u2zv1wx <u2zv1wx@protonmail.ch>
2019-03-21 18:09:58 +0900

    cleanup

--- src/Elaborate/Analyze.hs
+++ src/Elaborate/Analyze.hs
@@ -71,8 +71,8 @@ simp (c@(_, e1, e2, _):cs) = do
 simp' :: [PreConstraint] -> WithEnv [PreConstraint]
 simp' [] = return []
 simp' ((ctx, e1, e2, t):cs)
-  | Just (f, es1) <- headMeta1 [] e1
-  , Just (g, es2) <- headMeta1 [] e2
+  | Just (f, es1) <- headMeta' [] e1
+  , Just (g, es2) <- headMeta' [] e2
   , f == g
   , length es1 == length es2 = do
     sub <- gets substitution
@@ -142,9 +142,8 @@ simp'' ((ctx, i :< NeutSigma ((x, tx):xts), j :< NeutSigma ((y, ty):yts), univ):
     (ctx, tx, ty, univ) : (ctx ++ [x], i :< NeutSigma xts, sig', univ) : cs'
 simp'' ((ctx, _ :< NeutSigmaIntro es1, _ :< NeutSigmaIntro es2, _ :< NeutSigma xts):cs)
   | length es1 == length es2 = do
-    let (_, ts) = unzip xts
-    let sub = zip (map fst xts) es1
-    let ts' = map (subst sub) ts
+    let (xs, ts) = unzip xts
+    let ts' = map (subst $ zip xs es1) ts
     newCs <-
       forM (zip (zip es1 es2) ts') $ \((e1, e2), t') -> return (ctx, e1, e2, t')
     simp $ newCs ++ cs
@@ -243,3 +242,173 @@ updateQueue' sub q =
     Just (Enriched (ctx, e1, e2, t) _) -> do
       analyze [(ctx, subst sub e1, subst sub e2, t)]
       updateQueue' sub $ Q.deleteMin q
+
+isEq :: Neut -> Neut -> WithEnv Bool
+isEq (_ :< NeutVar x1) (_ :< NeutVar x2) = return $ x1 == x2
+isEq (_ :< NeutPi (x1, t11) t12) (_ :< NeutPi (x2, t21) t22) = do
+  vx <- toVar' x1
+  b1 <- isEq t11 t21
+  b2 <- isEq t12 $ subst [(x2, vx)] t22
+  return $ b1 && b2
+isEq (_ :< NeutPiIntro (x1, t1) e1) (_ :< NeutPiIntro (x2, t2) e2) = do
+  vx <- toVar' x1
+  b1 <- isEq t1 t2
+  b2 <- isEq e1 $ subst [(x2, vx)] e2
+  return $ b1 && b2
+isEq (_ :< NeutPiElim e11 e12) (_ :< NeutPiElim e21 e22) = do
+  b1 <- isEq e11 e21
+  b2 <- isEq e12 e22
+  return $ b1 && b2
+isEq (_ :< NeutSigma xts) (_ :< NeutSigma yts) = isEqSigma xts yts
+isEq (_ :< NeutSigmaIntro es1) (_ :< NeutSigmaIntro es2)
+  | length es1 == length es2 = do
+    bs <- zipWithM isEq es1 es2
+    return $ and bs
+isEq (_ :< NeutSigmaElim e11 xs1 e12) (_ :< NeutSigmaElim e21 xs2 e22)
+  | length xs1 == length xs2 = do
+    metaList <- mapM (const $ newNameWith "meta") xs1
+    let vs = map (\(meta, x) -> meta :< NeutVar x) $ zip metaList xs1
+    let sub = zip xs2 vs
+    let e22' = subst sub e22
+    b1 <- isEq e11 e21
+    b2 <- isEq e12 e22'
+    return $ b1 && b2
+isEq (_ :< NeutIndex l1) (_ :< NeutIndex l2) = return $ l1 == l2
+isEq (_ :< NeutIndexIntro i1) (_ :< NeutIndexIntro i2) = return $ i1 == i2
+isEq (_ :< NeutIndexElim e1 bs1) (_ :< NeutIndexElim e2 bs2) = do
+  b1 <- isEq e1 e2
+  b2 <- isEqBranch bs1 bs2
+  return $ b1 && b2
+isEq (_ :< NeutUniv l1) (_ :< NeutUniv l2) = return $ l1 == l2
+isEq (_ :< NeutConst t1) (_ :< NeutConst t2) = return $ t1 == t2
+isEq (_ :< NeutMu x1 e1) (_ :< NeutMu x2 e2) = do
+  vx <- toVar' x1
+  isEq e1 $ subst [(x2, vx)] e2
+isEq (_ :< NeutHole x1) (_ :< NeutHole x2) = return $ x1 == x2
+isEq _ _ = return False
+
+isEqSigma :: [(Identifier, Neut)] -> [(Identifier, Neut)] -> WithEnv Bool
+isEqSigma [] [] = return True
+isEqSigma ((x, tx):xts) ((y, ty):yts) = do
+  vx <- toVar' x
+  b1 <- isEq tx ty
+  let (ys, ts) = unzip yts
+  let ts' = map (subst [(y, vx)]) ts
+  let yts' = zip ys ts'
+  b2 <- isEqSigma xts yts'
+  return $ b1 && b2
+isEqSigma _ _ = return False
+
+isEqBranch :: [(IndexOrVar, Neut)] -> [(IndexOrVar, Neut)] -> WithEnv Bool
+isEqBranch [] [] = return True
+isEqBranch ((Right x1, e1):es1) ((Right x2, e2):es2) = do
+  vx <- toVar' x1
+  b1 <- isEq e1 $ subst [(x2, vx)] e2
+  b2 <- isEqBranch es1 es2
+  return $ b1 && b2
+isEqBranch ((Left (IndexLabel x1), e1):es1) ((Left (IndexLabel x2), e2):es2)
+  | x1 == x2 = do
+    b1 <- isEq e1 e2
+    b2 <- isEqBranch es1 es2
+    return $ b1 && b2
+isEqBranch ((Left (IndexInteger i1), e1):es1) ((Left (IndexInteger i2), e2):es2)
+  | i1 == i2 = do
+    b1 <- isEq e1 e2
+    b2 <- isEqBranch es1 es2
+    return $ b1 && b2
+isEqBranch ((Left (IndexFloat i1), e1):es1) ((Left (IndexFloat i2), e2):es2)
+  | i1 == i2 = do
+    b1 <- isEq e1 e2
+    b2 <- isEqBranch es1 es2
+    return $ b1 && b2
+isEqBranch ((Left IndexDefault, e1):es1) ((Left IndexDefault, e2):es2) = do
+  b1 <- isEq e1 e2
+  b2 <- isEqBranch es1 es2
+  return $ b1 && b2
+isEqBranch _ _ = return False
+
+affineCheck :: [Identifier] -> [Identifier] -> Bool
+affineCheck xs = affineCheck' xs xs
+
+affineCheck' :: [Identifier] -> [Identifier] -> [Identifier] -> Bool
+affineCheck' _ [] _ = True
+affineCheck' xs (y:ys) fvs =
+  if y `notElem` fvs
+    then affineCheck' xs ys fvs
+    else null (isLinear y xs) && affineCheck' xs ys fvs
+
+projectionList :: Neut -> [Neut] -> WithEnv [Neut]
+projectionList e ts = do
+  xiList <- forM ts $ \t -> newNameOfType t
+  metaList <- mapM (const newName) xiList
+  let varList = map (\(m, x) -> m :< NeutVar x) $ zip metaList xiList
+  forM varList $ \v -> do
+    meta <- newName
+    return $ meta :< NeutSigmaElim e xiList v
+
+sConstraint :: Subst -> [PreConstraint] -> WithEnv [PreConstraint]
+sConstraint s ctcs = do
+  let (ctxList, cs, typeList) = split ctcs
+  let (ts1, ts2) = unzip cs
+  let ts1' = map (subst s) ts1
+  let ts2' = map (subst s) ts2
+  let typeList' = map (subst s) typeList
+  return $ unsplit ctxList (zip ts1' ts2') typeList'
+
+split :: [PreConstraint] -> ([[Identifier]], [(Neut, Neut)], [Neut])
+split [] = ([], [], [])
+split ((ctx, e1, e2, t):rest) = do
+  let (ctxList, cs, typeList) = split rest
+  (ctx : ctxList, (e1, e2) : cs, t : typeList)
+
+unsplit :: [[Identifier]] -> [(Neut, Neut)] -> [Neut] -> [PreConstraint]
+unsplit [] [] [] = []
+unsplit (ctx:ctxList) ((e1, e2):cs) (t:typeList) =
+  (ctx, e1, e2, t) : unsplit ctxList cs typeList
+unsplit _ _ _ = error "Infer.unsplit: invalid arguments"
+
+hasMeta :: Neut -> Bool
+hasMeta (_ :< NeutVar _) = False
+hasMeta (_ :< NeutPi (_, tdom) tcod) = hasMeta tdom || hasMeta tcod
+hasMeta (_ :< NeutPiIntro _ e) = hasMeta e
+hasMeta (_ :< NeutPiElim e1 e2) = hasMeta e1 || hasMeta e2
+hasMeta (_ :< NeutSigma xts) = do
+  let (_, ts) = unzip xts
+  any hasMeta ts
+hasMeta (_ :< NeutSigmaIntro es) = any hasMeta es
+hasMeta (_ :< NeutSigmaElim e1 _ e2) = hasMeta e1 || hasMeta e2
+hasMeta (_ :< NeutIndex _) = False
+hasMeta (_ :< NeutIndexIntro _) = False
+hasMeta (_ :< NeutIndexElim e branchList) = do
+  let (_, es) = unzip branchList
+  any hasMeta (e : es)
+hasMeta (_ :< NeutUniv _) = False
+hasMeta (_ :< NeutConst _) = False
+hasMeta (_ :< NeutMu _ e) = hasMeta e
+hasMeta (_ :< NeutHole _) = True
+
+depth :: Identifier -> WithEnv Int
+depth x = do
+  sub <- gets substitution
+  case lookup x sub of
+    Nothing -> return 0
+    Just e -> do
+      let vs = var e
+      ds <- mapM depth vs
+      return $ 1 + maximum ds
+
+headMeta :: [Identifier] -> Neut -> Maybe (Identifier, [Identifier])
+headMeta args (_ :< NeutPiElim e1 (_ :< NeutVar x)) = headMeta (x : args) e1
+headMeta args (_ :< NeutHole x) = Just (x, args)
+headMeta _ _ = Nothing
+
+headMeta' :: [Neut] -> Neut -> Maybe (Identifier, [Neut])
+headMeta' args (_ :< NeutPiElim e1 e2) = headMeta' (e2 : args) e1
+headMeta' args (_ :< NeutHole x) = Just (x, args)
+headMeta' _ _ = Nothing
+
+headMeta'' :: Neut -> Maybe Identifier
+headMeta'' (_ :< NeutVar x) = Just x
+headMeta'' (_ :< NeutPiElim e1 _) = headMeta'' e1
+headMeta'' (_ :< NeutIndexElim e _) = headMeta'' e
+headMeta'' _ = Nothing
