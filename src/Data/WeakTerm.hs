4cb8fe87136aeb8a8135276586679521940cceed
vekatze <vekatze@icloud.com>
2019-07-18 09:31:00 +0900

    txs -> xts

--- src/Data/WeakTerm.hs
+++ src/Data/WeakTerm.hs
@@ -75,11 +75,11 @@ varAndHole (_ :< WeakTermEpsilonElim (x, t) e branchList) = do
       let (xs, hs) = varAndHole body
       return (filter (/= x) xs, hs)
   pairwiseConcat (xhs1 : xhs2 : xhss)
-varAndHole (_ :< WeakTermPi txs) = varAndHoleBindings txs []
-varAndHole (_ :< WeakTermPiIntro txs e) = varAndHoleBindings txs [e]
+varAndHole (_ :< WeakTermPi xts) = varAndHoleBindings xts []
+varAndHole (_ :< WeakTermPiIntro xts e) = varAndHoleBindings xts [e]
 varAndHole (_ :< WeakTermPiElim e es) =
   pairwiseConcat $ varAndHole e : map varAndHole es
-varAndHole (_ :< WeakTermSigma txs) = varAndHoleBindings txs []
+varAndHole (_ :< WeakTermSigma xts) = varAndHoleBindings xts []
 varAndHole (_ :< WeakTermSigmaIntro es) = pairwiseConcat $ map varAndHole es
 varAndHole (_ :< WeakTermSigmaElim us e1 e2) =
   pairwiseConcat [varAndHole e1, varAndHoleBindings us [e2]]
@@ -97,9 +97,9 @@ varAndHole (_ :< WeakTermHole x) = ([], [x])
 varAndHoleBindings ::
      [IdentifierPlus] -> [WeakTerm] -> ([Identifier], [Identifier])
 varAndHoleBindings [] es = pairwiseConcat $ map varAndHole es
-varAndHoleBindings ((x, t):txs) es = do
+varAndHoleBindings ((x, t):xts) es = do
   let (xs1, hs1) = varAndHole t
-  let (xs2, hs2) = varAndHoleBindings txs es
+  let (xs2, hs2) = varAndHoleBindings xts es
   (xs1 ++ filter (/= x) xs2, hs1 ++ hs2)
 
 pairwiseConcat :: [([a], [b])] -> ([a], [b])
@@ -121,26 +121,26 @@ substWeakTerm sub (j :< WeakTermEpsilonElim (x, t) e branchList) = do
   let sub' = filter (\(k, _) -> k /= x) sub
   let es' = map (substWeakTerm sub') es
   j :< WeakTermEpsilonElim (x, t') e' (zip caseList es')
-substWeakTerm sub (j :< WeakTermPi txs) = do
-  let txs' = substWeakTermBindings sub txs
-  j :< WeakTermPi txs'
-substWeakTerm sub (j :< WeakTermPiIntro txs body) = do
-  let (txs', body') = substWeakTermBindingsWithBody sub txs body
-  j :< WeakTermPiIntro txs' body'
+substWeakTerm sub (j :< WeakTermPi xts) = do
+  let xts' = substWeakTermBindings sub xts
+  j :< WeakTermPi xts'
+substWeakTerm sub (j :< WeakTermPiIntro xts body) = do
+  let (xts', body') = substWeakTermBindingsWithBody sub xts body
+  j :< WeakTermPiIntro xts' body'
 substWeakTerm sub (j :< WeakTermPiElim e es) = do
   let e' = substWeakTerm sub e
   let es' = map (substWeakTerm sub) es
   j :< WeakTermPiElim e' es'
-substWeakTerm sub (j :< WeakTermSigma txs) = do
-  let txs' = substWeakTermBindings sub txs
-  j :< WeakTermSigma txs'
+substWeakTerm sub (j :< WeakTermSigma xts) = do
+  let xts' = substWeakTermBindings sub xts
+  j :< WeakTermSigma xts'
 substWeakTerm sub (j :< WeakTermSigmaIntro es) = do
   let es' = map (substWeakTerm sub) es
   j :< WeakTermSigmaIntro es'
-substWeakTerm sub (j :< WeakTermSigmaElim txs e1 e2) = do
+substWeakTerm sub (j :< WeakTermSigmaElim xts e1 e2) = do
   let e1' = substWeakTerm sub e1
-  let (txs', e2') = substWeakTermBindingsWithBody sub txs e2
-  j :< WeakTermSigmaElim txs' e1' e2'
+  let (xts', e2') = substWeakTermBindingsWithBody sub xts e2
+  j :< WeakTermSigmaElim xts' e1' e2'
 substWeakTerm sub (j :< WeakTermTau t) = j :< WeakTermTau (substWeakTerm sub t)
 substWeakTerm sub (j :< WeakTermTauIntro e) =
   j :< WeakTermTauIntro (substWeakTerm sub e)
@@ -164,10 +164,10 @@ substWeakTerm sub (j :< WeakTermHole s) =
 
 substWeakTermBindings :: SubstWeakTerm -> [IdentifierPlus] -> [IdentifierPlus]
 substWeakTermBindings _ [] = []
-substWeakTermBindings sub ((x, t):txs) = do
+substWeakTermBindings sub ((x, t):xts) = do
   let sub' = filter (\(k, _) -> k /= x) sub
-  let txs' = substWeakTermBindings sub' txs
-  (x, substWeakTerm sub t) : txs'
+  let xts' = substWeakTermBindings sub' xts
+  (x, substWeakTerm sub t) : xts'
 
 substWeakTermBindingsWithBody ::
      SubstWeakTerm
@@ -175,10 +175,10 @@ substWeakTermBindingsWithBody ::
   -> WeakTerm
   -> ([IdentifierPlus], WeakTerm)
 substWeakTermBindingsWithBody sub [] e = ([], substWeakTerm sub e)
-substWeakTermBindingsWithBody sub ((x, t):txs) e = do
+substWeakTermBindingsWithBody sub ((x, t):xts) e = do
   let sub' = filter (\(k, _) -> k /= x) sub
-  let (txs', e') = substWeakTermBindingsWithBody sub' txs e
-  ((x, substWeakTerm sub t) : txs', e')
+  let (xts', e') = substWeakTermBindingsWithBody sub' xts e
+  ((x, substWeakTerm sub t) : xts', e')
 
 isReducible :: WeakTerm -> Bool
 isReducible (_ :< WeakTermUniv _) = False
@@ -191,16 +191,16 @@ isReducible (_ :< WeakTermEpsilonElim _ (_ :< WeakTermEpsilonIntro l) branchList
 isReducible (_ :< WeakTermEpsilonElim (_, _) e _) = isReducible e
 isReducible (_ :< WeakTermPi _) = False
 isReducible (_ :< WeakTermPiIntro _ _) = False
-isReducible (_ :< WeakTermPiElim (_ :< WeakTermPiIntro txs _) es)
-  | length txs == length es = True
+isReducible (_ :< WeakTermPiElim (_ :< WeakTermPiIntro xts _) es)
+  | length xts == length es = True
 isReducible (_ :< WeakTermPiElim (_ :< WeakTermMu _ _) _) = True -- CBV recursion
 isReducible (_ :< WeakTermPiElim (_ :< WeakTermConst c) [_ :< WeakTermEpsilonIntro (LiteralInteger _), _ :< WeakTermEpsilonIntro (LiteralInteger _)]) -- constant application
   | c `elem` intArithConstantList = True
 isReducible (_ :< WeakTermPiElim e es) = isReducible e || any isReducible es
 isReducible (_ :< WeakTermSigma _) = False
 isReducible (_ :< WeakTermSigmaIntro es) = any isReducible es
-isReducible (_ :< WeakTermSigmaElim txs (_ :< WeakTermSigmaIntro es) _)
-  | length txs == length es = True
+isReducible (_ :< WeakTermSigmaElim xts (_ :< WeakTermSigmaIntro es) _)
+  | length xts == length es = True
 isReducible (_ :< WeakTermSigmaElim _ e1 _) = isReducible e1
 isReducible (_ :< WeakTermTau _) = False
 isReducible (_ :< WeakTermTauIntro e) = isReducible e
