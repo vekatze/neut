f4a7b7a4deff90ba88a30492bbc39ec46b9ab5a8
vekatze <vekatze@icloud.com>
2019-07-18 10:25:20 +0900

    update simp

--- src/Elaborate/Analyze.hs
+++ src/Elaborate/Analyze.hs
@@ -29,19 +29,17 @@ simp ((e1, e2):cs)
     me1' <- liftIO $ timeout 5000000 $ return $ reduceWeakTerm e1 -- 5 sec
     case me1' of
       Just e1' -> simp $ (e1', e2) : cs
-      Nothing -> do
-        let e1' = toDTerm e1
-        let e2' = toDTerm e2
-        throwError $ "cannot simplify [TIMEOUT]:\n" ++ Pr.ppShow (e1', e2')
+      Nothing ->
+        throwError $ "cannot simplify [TIMEOUT]:\n" ++ Pr.ppShow (e1, e2)
 simp ((e1, e2):cs)
   | isReducible e2 = simp $ (e2, e1) : cs
 simp ((e1, e2):cs)
-  | _ :< WeakTermPiElim s1 (_ :< WeakTermUpsilon f) es1 <- e1
-  , _ :< WeakTermPiElim s2 (_ :< WeakTermUpsilon g) es2 <- e2
+  | _ :< WeakTermPiElim (_ :< WeakTermUpsilon f) es1 <- e1
+  , _ :< WeakTermPiElim (_ :< WeakTermUpsilon g) es2 <- e2
   , f == g
-  , length es1 == length es2 = simp $ (s1, s2) : zip es1 es2 ++ cs
+  , length es1 == length es2 = simp $ zip es1 es2 ++ cs
 simp ((_ :< WeakTermUniv i, _ :< WeakTermUniv j):cs) = do
-  insUnivConstraintEnv i j
+  simpLevel i j
   simp cs
 simp ((_ :< WeakTermUpsilon x1, _ :< WeakTermUpsilon x2):cs)
   | x1 == x2 = simp cs
@@ -49,26 +47,26 @@ simp ((_ :< WeakTermEpsilon l1, _ :< WeakTermEpsilon l2):cs)
   | l1 == l2 = simp cs
 simp ((_ :< WeakTermEpsilonIntro l1, _ :< WeakTermEpsilonIntro l2):cs)
   | l1 == l2 = simp cs
-simp ((_ :< WeakTermPi s1 txs1, _ :< WeakTermPi s2 txs2):cs)
-  | length txs1 == length txs2 = simpPiOrSigma s1 txs1 s2 txs2 cs
-simp ((_ :< WeakTermPiIntro s1 txs1 body1, _ :< WeakTermPiIntro s2 txs2 body2):cs) = do
+simp ((_ :< WeakTermPi xts1, _ :< WeakTermPi xts2):cs)
+  | length xts1 == length xts2 = simpPiOrSigma xts1 xts2 cs
+simp ((_ :< WeakTermPiIntro xts1 body1, _ :< WeakTermPiIntro xts2 body2):cs) = do
   h1 <- newNameWith "hole"
   h2 <- newNameWith "hole"
-  simpPiOrSigma s1 (txs1 ++ [(body1, h1)]) s2 (txs2 ++ [(body2, h2)]) cs
-simp ((_ :< WeakTermPiIntro s txs body1, e2):cs) = do
-  let (_, xs) = unzip txs
+  simpPiOrSigma (xts1 ++ [(h1, body1)]) (xts2 ++ [(h2, body2)]) cs
+simp ((_ :< WeakTermPiIntro xts body1, e2):cs) = do
+  let (xs, _) = unzip xts
   vs <- mapM toVar' xs
   appMeta <- newNameWith "meta"
-  simp $ (body1, appMeta :< WeakTermPiElim s e2 vs) : cs
+  simp $ (body1, appMeta :< WeakTermPiElim e2 vs) : cs
 simp ((e1, e2@(_ :< WeakTermPiIntro {})):cs) = simp $ (e2, e1) : cs
-simp ((_ :< WeakTermSigma s1 txs1, _ :< WeakTermSigma s2 txs2):cs)
-  | length txs1 == length txs2 = simpPiOrSigma s1 txs1 s2 txs2 cs
-simp ((_ :< WeakTermSigmaIntro s1 es1, _ :< WeakTermSigmaIntro s2 es2):cs)
-  | length es1 == length es2 = simp $ (s1, s2) : zip es1 es2 ++ cs
-simp ((_ :< WeakTermSigmaElim s txs e1 e2, e):cs) = do
-  hs <- mapM (const newHole) txs
-  sigmaIntro <- wrapType $ WeakTermSigmaIntro s hs
-  let e2' = substWeakTerm (zip (map snd txs) hs) e2
+simp ((_ :< WeakTermSigma xts1, _ :< WeakTermSigma xts2):cs)
+  | length xts1 == length xts2 = simpPiOrSigma xts1 xts2 cs
+simp ((_ :< WeakTermSigmaIntro es1, _ :< WeakTermSigmaIntro es2):cs)
+  | length es1 == length es2 = simp $ zip es1 es2 ++ cs
+simp ((_ :< WeakTermSigmaElim xts e1 e2, e):cs) = do
+  hs <- mapM (const newHole) xts
+  sigmaIntro <- wrapType $ WeakTermSigmaIntro hs
+  let e2' = substWeakTerm (zip (map fst xts) hs) e2
   simp $ (e1, sigmaIntro) : (e2', e) : cs
 simp ((e1, e2@(_ :< WeakTermSigmaElim {})):cs) = simp $ (e2, e1) : cs
 simp ((_ :< WeakTermConst x, _ :< WeakTermConst y):cs)
@@ -81,27 +79,27 @@ simp ((e1, e2):cs) = do
       cs' <- simp cs
       return $ Enriched (e1, e2) [m] (ConstraintImmediate m e2) : cs'
     (_, Just (StuckHole _, _)) -> simp $ (e2, e1) : cs
-    (Just (StuckPiElimStrict s1 m1 exs1, _), _)
+    (Just (StuckPiElimStrict m1 exs1, _), _)
       | (es1, xs1) <- unzip exs1
       , isSolvable e2 m1 xs1 -> do
         cs' <- simp cs
-        return $ Enriched (e1, e2) [m1] (ConstraintPattern s1 m1 es1 e2) : cs'
-    (_, Just (StuckPiElimStrict _ m2 exs2, _))
+        return $ Enriched (e1, e2) [m1] (ConstraintPattern m1 es1 e2) : cs'
+    (_, Just (StuckPiElimStrict m2 exs2, _))
       | isSolvable e1 m2 (map snd exs2) -> simp $ (e2, e1) : cs
-    (Just (StuckPiElimStrict s1 m1 exs1, _), _) -> do
+    (Just (StuckPiElimStrict m1 exs1, _), _) -> do
       cs' <- simp cs
       return $
-        Enriched (e1, e2) [m1] (ConstraintQuasiPattern s1 m1 (map fst exs1) e2) :
+        Enriched (e1, e2) [m1] (ConstraintQuasiPattern m1 (map fst exs1) e2) :
         cs'
     (_, Just (StuckPiElimStrict {}, _)) -> simp $ (e2, e1) : cs
-    (Just (StuckPiElim s1 m1 es1, _), Nothing) -> do
+    (Just (StuckPiElim m1 es1, _), Nothing) -> do
       cs' <- simp cs
-      let c = Enriched (e1, e2) [m1] $ ConstraintFlexRigid s1 m1 es1 e2
+      let c = Enriched (e1, e2) [m1] $ ConstraintFlexRigid m1 es1 e2
       return $ c : cs'
     (Nothing, Just (StuckPiElim {}, _)) -> simp $ (e2, e1) : cs
-    (Just (StuckPiElim s1 m1 es1, _), _) -> do
+    (Just (StuckPiElim m1 es1, _), _) -> do
       cs' <- simp cs
-      let c = Enriched (e1, e2) [m1] $ ConstraintFlexFlex s1 m1 es1 e2
+      let c = Enriched (e1, e2) [m1] $ ConstraintFlexFlex m1 es1 e2
       return $ c : cs'
     (_, Just (StuckPiElim {}, _)) -> simp $ (e2, e1) : cs
     (Just (StuckOther, m1), _) -> do
@@ -109,48 +107,53 @@ simp ((e1, e2):cs) = do
       let c = Enriched (e1, e2) [m1] ConstraintOther
       return $ c : cs'
     (_, Just (StuckOther, _)) -> simp $ (e2, e1) : cs
-    _ -> throwError $ "cannot simplify:\n" ++ Pr.ppShow (toDTerm e1, toDTerm e2)
+    _ -> throwError $ "cannot simplify:\n" ++ Pr.ppShow (e1, e2)
+
+simpLevel :: WeakLevel -> WeakLevel -> WithEnv ()
+simpLevel (WeakLevelInt i1) (WeakLevelInt i2)
+  | i1 == i2 = return ()
+simpLevel WeakLevelInfinity WeakLevelInfinity = return ()
+simpLevel (WeakLevelHole _) _ = return () -- FIXME: update level environment
+simpLevel l (WeakLevelHole h) = simpLevel (WeakLevelHole h) l
+simpLevel l1 l2 =
+  throwError $ "LevelError: cannot simplify: " ++ show l1 ++ " with " ++ show l2
 
 simpPiOrSigma ::
-     WeakTerm
-  -> [(WeakTerm, Identifier)]
-  -> WeakTerm
-  -> [(WeakTerm, Identifier)]
+     [(Identifier, WeakTerm)]
+  -> [(Identifier, WeakTerm)]
   -> [(WeakTerm, WeakTerm)]
   -> WithEnv [EnrichedConstraint]
-simpPiOrSigma s1 txs1 s2 txs2 cs = do
-  let (ts1, xs1) = unzip txs1
+simpPiOrSigma xts1 xts2 cs = do
+  let (xs1, ts1) = unzip xts1
   vs1 <- mapM toVar' xs1
-  let (ts2, xs2) = unzip txs2
+  let (xs2, ts2) = unzip xts2
   let ts2' = map (substWeakTerm (zip xs2 vs1)) ts2
-  simp $ (s1, s2) : zip ts1 ts2' ++ cs
+  simp $ zip ts1 ts2' ++ cs
 
 data Stuck
   = StuckHole Identifier
-  | StuckPiElim WeakSortal
-                Identifier
+  | StuckPiElim Identifier
                 [WeakTerm]
-  | StuckPiElimStrict WeakSortal
-                      Identifier
+  | StuckPiElimStrict Identifier
                       [(WeakTerm, Identifier)]
   | StuckOther
 
 asStuckedTerm :: WeakTerm -> Maybe (Stuck, Identifier)
 asStuckedTerm (_ :< WeakTermHole m) = Just (StuckHole m, m)
-asStuckedTerm (_ :< WeakTermPiElim s (_ :< WeakTermHole x) es) =
+asStuckedTerm (_ :< WeakTermPiElim (_ :< WeakTermHole x) es) =
   case mapM interpretAsUpsilon es of
-    Nothing -> Just (StuckPiElim s x es, x)
-    Just xs -> Just (StuckPiElimStrict s x (zip es xs), x)
+    Nothing -> Just (StuckPiElim x es, x)
+    Just xs -> Just (StuckPiElimStrict x (zip es xs), x)
 asStuckedTerm (_ :< WeakTermEpsilonElim _ e _)
   | Just m <- obtainStuckReason e = Just (StuckOther, m)
 asStuckedTerm _ = Nothing
 
 obtainStuckReason :: WeakTerm -> Maybe Identifier
-obtainStuckReason (_ :< WeakTermHole x)             = Just x
-obtainStuckReason (_ :< WeakTermPiElim _ e _)       = obtainStuckReason e
-obtainStuckReason (_ :< WeakTermSigmaElim _ _ e1 _) = obtainStuckReason e1
-obtainStuckReason (_ :< WeakTermEpsilonElim _ e _)  = obtainStuckReason e
-obtainStuckReason _                                 = Nothing
+obtainStuckReason (_ :< WeakTermHole x)            = Just x
+obtainStuckReason (_ :< WeakTermPiElim e _)        = obtainStuckReason e
+obtainStuckReason (_ :< WeakTermSigmaElim _ e1 _)  = obtainStuckReason e1
+obtainStuckReason (_ :< WeakTermEpsilonElim _ e _) = obtainStuckReason e
+obtainStuckReason _                                = Nothing
 
 isSolvable :: WeakTerm -> Identifier -> [Identifier] -> Bool
 isSolvable e x xs = do
@@ -178,11 +181,11 @@ isLinear x xs =
     then []
     else [x]
 
-bindFormalArgs :: WeakSortal -> [Identifier] -> WeakTerm -> WithEnv WeakTerm
-bindFormalArgs s xs e = do
+bindFormalArgs :: [Identifier] -> WeakTerm -> WithEnv WeakTerm
+bindFormalArgs xs e = do
   ts <- mapM (const newHole) xs
   meta <- newNameWith "meta"
-  return $ meta :< WeakTermPiIntro s (zip ts xs) e
+  return $ meta :< WeakTermPiIntro (zip xs ts) e
 
 interpretAsUpsilon :: WeakTerm -> Maybe Identifier
 interpretAsUpsilon (_ :< WeakTermUpsilon x) = Just x
