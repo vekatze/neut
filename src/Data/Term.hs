7048e2fbd92bcf15050a6877f5cb44b690157e47
vekatze <vekatze@icloud.com>
2019-07-18 20:28:26 +0900

    Level

--- src/Data/Term.hs
+++ src/Data/Term.hs
@@ -4,60 +4,71 @@ import           Data.Maybe (fromMaybe)
 
 import           Data.Basic
 
+data Level
+  = LevelInt Term -- Int (EpsilonIntro)
+  | LevelInfinity
+  deriving (Show)
+
 data Term
-  = TermVar Identifier
-  | TermConst Identifier
-  | TermPiIntro Identifier
+  = TermUpsilon Identifier
+  | TermEpsilonIntro Literal
+                     LowType
+  | TermEpsilonElim Identifier
+                    Term
+                    [(Case, Term)]
+  | TermPiIntro Level
+                [Identifier]
                 Term
   | TermPiElim Term
-               Term
+               [Term]
   | TermConstElim Identifier
                   [Term]
-  | TermSigmaIntro [Term]
+  | TermSigmaIntro Level
+                   [Term]
   | TermSigmaElim [Identifier]
                   Term
                   Term
-  | TermIndexIntro Index
-                   LowType
-  | TermIndexElim Term
-                  [(Index, Term)]
+  | TermTauIntro Term
+  | TermTauElim Term
+  | TermConst Identifier
   deriving (Show)
 
 type SubstTerm = [(Identifier, Term)]
 
 substTerm :: SubstTerm -> Term -> Term
-substTerm sub (TermVar s) = fromMaybe (TermVar s) (lookup s sub)
-substTerm _ (TermConst x) = TermConst x
-substTerm sub (TermPiIntro s body) = do
-  let sub' = filter (\(x, _) -> x /= s) sub
+substTerm sub (TermUpsilon s) = fromMaybe (TermUpsilon s) (lookup s sub)
+substTerm _ (TermEpsilonIntro l meta) = TermEpsilonIntro l meta
+substTerm sub (TermEpsilonElim x e branchList) = do
+  let e' = substTerm sub e
+  let (labelList, es) = unzip branchList
+  let sub' = filter (\(k, _) -> k /= x) sub
+  let es' = map (substTerm sub') es
+  TermEpsilonElim x e' $ zip labelList es'
+substTerm sub (TermPiIntro s xs body) = do
+  let s' = substLevel sub s
+  let sub' = filter (\(k, _) -> k `notElem` xs) sub
   let body' = substTerm sub' body
-  TermPiIntro s body'
-substTerm sub (TermPiElim e1 e2) = do
-  let e1' = substTerm sub e1
-  let e2' = substTerm sub e2
-  TermPiElim e1' e2'
+  TermPiIntro s' xs body'
+substTerm sub (TermPiElim e es) = do
+  let e' = substTerm sub e
+  let es' = map (substTerm sub) es
+  TermPiElim e' es'
 substTerm sub (TermConstElim x es) = do
   let es' = map (substTerm sub) es
   TermConstElim x es'
-substTerm sub (TermSigmaIntro es) = TermSigmaIntro (map (substTerm sub) es)
+substTerm sub (TermSigmaIntro s es) = do
+  let s' = substLevel sub s
+  let es' = map (substTerm sub) es
+  TermSigmaIntro s' es'
 substTerm sub (TermSigmaElim xs e1 e2) = do
   let e1' = substTerm sub e1
-  let sub' = filter (\(x, _) -> x `notElem` xs) sub
+  let sub' = filter (\(k, _) -> k `notElem` xs) sub
   let e2' = substTerm sub' e2
   TermSigmaElim xs e1' e2'
-substTerm _ (TermIndexIntro l meta) = TermIndexIntro l meta
-substTerm sub (TermIndexElim e branchList) = do
-  let e' = substTerm sub e
-  let (labelList, es) = unzip branchList
-  let es' = map (substTerm sub) es
-  TermIndexElim e' $ zip labelList es'
+substTerm sub (TermTauIntro e) = TermTauIntro $ substTerm sub e
+substTerm sub (TermTauElim e) = TermTauElim $ substTerm sub e
+substTerm _ (TermConst x) = TermConst x
 
-toTermPiElimSeq :: Term -> (Term, [Term])
-toTermPiElimSeq (TermPiElim e1 e2) = do
-  let (fun, xs) = toTermPiElimSeq e1
-  (fun, xs ++ [e2])
-toTermPiElimSeq c = (c, [])
-
-fromTermPiElimSeq :: (Term, [Term]) -> Term
-fromTermPiElimSeq (term, [])   = term
-fromTermPiElimSeq (term, v:xs) = fromTermPiElimSeq (TermPiElim term v, xs)
+substLevel :: SubstTerm -> Level -> Level
+substLevel sub (LevelInt e) = LevelInt $ substTerm sub e
+substLevel _ LevelInfinity  = LevelInfinity
