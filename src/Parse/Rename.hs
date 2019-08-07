c9d7fb27d2f86cc2c423a888d7c65cec2cdd73c7
vekatze <vekatze@icloud.com>
2019-08-07 09:11:46 +0900

    rename

--- src/Parse/Rename.hs
+++ src/Parse/Rename.hs
@@ -42,54 +42,51 @@ rename (m :< WeakTermEpsilonElim (x, t) e caseList) = do
     x' <- newIdentifierWith x
     caseList' <- renameCaseList caseList
     return $ m :< WeakTermEpsilonElim (x', t') e' caseList'
-rename (m :< WeakTermPi xts) = do
+rename (m :< WeakTermPi i xts) = do
   xts' <- renameBindings xts
-  return $ m :< WeakTermPi xts'
-rename (m :< WeakTermPiIntro xts e) = do
+  return $ m :< WeakTermPi i xts'
+rename (m :< WeakTermPiIntro i xts e) = do
   (xts', e') <- renameBindingsWithBody xts e
-  return $ m :< WeakTermPiIntro xts' e'
-rename (m :< WeakTermPiElim e es) = do
+  return $ m :< WeakTermPiIntro i xts' e'
+rename (m :< WeakTermPiElim i e es) = do
   e' <- rename e
   es' <- mapM rename es
-  return $ m :< WeakTermPiElim e' es'
-rename (m :< WeakTermSigma xts) = do
+  return $ m :< WeakTermPiElim i e' es'
+rename (m :< WeakTermSigma i xts) = do
   xts' <- renameBindings xts
-  return $ m :< WeakTermSigma xts'
-rename (m :< WeakTermSigmaIntro es) = do
+  return $ m :< WeakTermSigma i xts'
+rename (m :< WeakTermSigmaIntro i es) = do
   es' <- mapM rename es
-  return $ m :< WeakTermSigmaIntro es'
-rename (m :< WeakTermSigmaElim xts e1 e2) = do
+  return $ m :< WeakTermSigmaIntro i es'
+rename (m :< WeakTermSigmaElim i xts e1 e2) = do
   e1' <- rename e1
   (xts', e2') <- renameBindingsWithBody xts e2
-  return $ m :< WeakTermSigmaElim xts' e1' e2'
-rename (m :< WeakTermTau t) = do
+  return $ m :< WeakTermSigmaElim i xts' e1' e2'
+rename (m :< WeakTermTau i t) = do
   t' <- rename t
-  return $ m :< WeakTermTau t'
-rename (m :< WeakTermTauIntro e) = do
+  return $ m :< WeakTermTau i t'
+rename (m :< WeakTermTauIntro i e) = do
   e' <- rename e
-  return $ m :< WeakTermTauIntro e'
-rename (m :< WeakTermTauElim e) = do
+  return $ m :< WeakTermTauIntro i e'
+rename (m :< WeakTermTauElim i e) = do
   e' <- rename e
-  return $ m :< WeakTermTauElim e'
+  return $ m :< WeakTermTauElim i e'
 rename (m :< WeakTermTheta t) = do
   t' <- rename t
   return $ m :< WeakTermTheta t'
 rename (m :< WeakTermThetaIntro e) = do
   e' <- rename e
   return $ m :< WeakTermThetaIntro e'
-rename (m :< WeakTermThetaElim e) = do
+rename (m :< WeakTermThetaElim e i) = do
   e' <- rename e
-  return $ m :< WeakTermThetaElim e'
+  return $ m :< WeakTermThetaElim e' i
 rename (m :< WeakTermMu xt e) =
   local $ do
     xt' <- newIdentifierPlusWith xt
     e' <- rename e
     return $ m :< WeakTermMu xt' e'
-rename (m :< WeakTermIota e i) = do
-  e' <- rename e
-  return $ m :< WeakTermIota e' i
 rename (m :< WeakTermConst x) = return $ m :< WeakTermConst x
-rename (m :< WeakTermHole x) = return $ m :< WeakTermHole x
+rename (m :< WeakTermHole h) = return $ m :< WeakTermHole h
 
 renameBindings :: [IdentifierPlus] -> WithEnv [IdentifierPlus]
 renameBindings [] = return []
