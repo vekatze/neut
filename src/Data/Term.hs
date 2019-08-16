a86fe3ee131fd49c380c3bc985b4d54a195822c5
vekatze <vekatze@icloud.com>
2019-08-16 09:42:26 +0900

    update

--- src/Data/Term.hs
+++ src/Data/Term.hs
@@ -1,143 +1,171 @@
-{-# LANGUAGE DeriveFunctor      #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE TemplateHaskell    #-}
-
 module Data.Term where
 
-import           Control.Comonad.Cofree
-import           Control.Monad          (forM)
-import           Data.Maybe             (fromMaybe)
-import           Text.Show.Deriving
+import           Control.Monad (forM)
+import           Data.Maybe    (fromMaybe)
 
 import           Data.Basic
 
-data TermF a
+data Term
   = TermTau
   | TermTheta Identifier
   | TermUpsilon Identifier
   | TermEpsilon Identifier
   | TermEpsilonIntro Literal
-  | TermEpsilonElim (Identifier, a)
-                    a
-                    [(Case, a)]
-  | TermPi [(Identifier, a)]
-  | TermPiIntro [(Identifier, a)]
-                a
-  | TermPiElim a
-               [a]
-  | TermSigma [(Identifier, a)]
-  | TermSigmaIntro [a]
-  | TermSigmaElim [(Identifier, a)]
-                  a
-                  a
-  | TermMu (Identifier, a)
-           a
-
-type Term = Cofree TermF Meta
+  | TermEpsilonElim (Identifier, TermPlus)
+                    TermPlus
+                    [(Case, TermPlus)]
+  | TermPi [(Identifier, TermPlus)]
+  | TermPiIntro [(Identifier, TermPlus)]
+                TermPlus
+  | TermPiElim TermPlus
+               [TermPlus]
+  | TermSigma [(Identifier, TermPlus)]
+  | TermSigmaIntro [TermPlus]
+  | TermSigmaElim [(Identifier, TermPlus)]
+                  TermPlus
+                  TermPlus
+  | TermMu (Identifier, TermPlus)
+           TermPlus
+  deriving (Show)
 
 data Meta
   = MetaTerminal (Maybe (Int, Int))
-  | MetaNonTerminal Term
+  | MetaNonTerminal TermPlus
                     (Maybe (Int, Int))
+  deriving (Show)
 
-$(deriveShow1 ''TermF)
+type TermPlus = (Meta, Term)
 
-deriving instance Show Meta
+type SubstTerm = [(Identifier, TermPlus)]
 
-type SubstTerm = [(Identifier, Term)]
+type Hole = Identifier
 
-type IdentifierPlus = (Identifier, Term)
+type IdentifierPlus = (Identifier, TermPlus)
 
-varTerm :: Term -> [Identifier]
-varTerm (_ :< TermTau) = []
-varTerm (_ :< TermTheta _) = []
-varTerm (_ :< TermUpsilon x) = [x]
-varTerm (_ :< TermEpsilon _) = []
-varTerm (_ :< TermEpsilonIntro _) = []
-varTerm (_ :< TermEpsilonElim (x, t) e branchList) = do
-  let xs1 = varTerm t
-  let xs2 = varTerm e
-  xss <-
+varTermPlus :: TermPlus -> ([Identifier], [Hole])
+varTermPlus (_, TermTau) = ([], [])
+varTermPlus (_, TermTheta _) = ([], [])
+varTermPlus (_, TermUpsilon x) = ([x], [])
+varTermPlus (_, TermEpsilon _) = ([], [])
+varTermPlus (_, TermEpsilonIntro _) = ([], [])
+varTermPlus (_, TermEpsilonElim (x, t) e branchList) = do
+  let xhs1 = varTermPlus t
+  let xhs2 = varTermPlus e
+  xhss <-
     forM branchList $ \(_, body) -> do
-      let xs = varTerm body
-      return $ filter (/= x) xs
-  xs1 ++ xs2 ++ concat xss
-varTerm (_ :< TermPi xts) = varTermBindings xts []
-varTerm (_ :< TermPiIntro xts e) = varTermBindings xts [e]
-varTerm (_ :< TermPiElim e es) = varTerm e ++ concatMap varTerm es
-varTerm (_ :< TermSigma xts) = varTermBindings xts []
-varTerm (_ :< TermSigmaIntro es) = concatMap varTerm es
-varTerm (_ :< TermSigmaElim us e1 e2) = varTerm e1 ++ varTermBindings us [e2]
-varTerm (_ :< TermMu ut e) = varTermBindings [ut] [e]
+      let (xs, hs) = varTermPlus body
+      return (filter (/= x) xs, hs)
+  pairwiseConcat (xhs1 : xhs2 : xhss)
+varTermPlus (_, TermPi xts) = varTermPlusBindings xts []
+varTermPlus (_, TermPiIntro xts e) = varTermPlusBindings xts [e]
+varTermPlus (_, TermPiElim e es) =
+  pairwiseConcat $ varTermPlus e : map varTermPlus es
+varTermPlus (_, TermSigma xts) = varTermPlusBindings xts []
+varTermPlus (_, TermSigmaIntro es) = pairwiseConcat $ map varTermPlus es
+varTermPlus (_, TermSigmaElim us e1 e2) =
+  pairwiseConcat [varTermPlus e1, varTermPlusBindings us [e2]]
+varTermPlus (_, TermMu ut e) = varTermPlusBindings [ut] [e]
 
-varTermBindings :: [IdentifierPlus] -> [Term] -> [Identifier]
-varTermBindings [] es = concatMap varTerm es
-varTermBindings ((x, t):xts) es = do
-  let xs1 = varTerm t
-  let xs2 = varTermBindings xts es
-  xs1 ++ filter (/= x) xs2
+varTermPlusBindings ::
+     [IdentifierPlus] -> [TermPlus] -> ([Identifier], [Identifier])
+varTermPlusBindings [] es = pairwiseConcat $ map varTermPlus es
+varTermPlusBindings ((x, t):xts) es = do
+  let (xs1, hs1) = varTermPlus t
+  let (xs2, hs2) = varTermPlusBindings xts es
+  (xs1 ++ filter (/= x) xs2, hs1 ++ hs2)
 
-substTerm :: SubstTerm -> Term -> Term
-substTerm _ (m :< TermTau) = m :< TermTau
-substTerm _ (m :< TermTheta t) = m :< TermTheta t
-substTerm sub (m :< TermUpsilon x) =
-  fromMaybe (m :< TermUpsilon x) (lookup x sub)
-substTerm _ (m :< TermEpsilon x) = m :< TermEpsilon x
-substTerm _ (m :< TermEpsilonIntro l) = m :< TermEpsilonIntro l
-substTerm sub (m :< TermEpsilonElim (x, t) e branchList) = do
-  let t' = substTerm sub t
-  let e' = substTerm sub e
+pairwiseConcat :: [([a], [b])] -> ([a], [b])
+pairwiseConcat [] = ([], [])
+pairwiseConcat ((xs, ys):rest) = do
+  let (xs', ys') = pairwiseConcat rest
+  (xs ++ xs', ys ++ ys')
+
+substTermPlus :: SubstTerm -> TermPlus -> TermPlus
+substTermPlus _ (m, TermTau) = (m, TermTau)
+substTermPlus _ (m, TermTheta t) = (m, TermTheta t)
+substTermPlus sub (m, TermUpsilon x) =
+  fromMaybe (m, TermUpsilon x) (lookup x sub)
+substTermPlus _ (m, TermEpsilon x) = (m, TermEpsilon x)
+substTermPlus _ (m, TermEpsilonIntro l) = (m, TermEpsilonIntro l)
+substTermPlus sub (m, TermEpsilonElim (x, t) e branchList) = do
+  let t' = substTermPlus sub t
+  let e' = substTermPlus sub e
   let (caseList, es) = unzip branchList
   let sub' = filter (\(k, _) -> k /= x) sub
-  let es' = map (substTerm sub') es
-  m :< TermEpsilonElim (x, t') e' (zip caseList es')
-substTerm sub (m :< TermPi xts) = do
-  let xts' = substTermBindings sub xts
-  m :< TermPi xts'
-substTerm sub (m :< TermPiIntro xts body) = do
-  let (xts', body') = substTermBindingsWithBody sub xts body
-  m :< TermPiIntro xts' body'
-substTerm sub (m :< TermPiElim e es) = do
-  let e' = substTerm sub e
-  let es' = map (substTerm sub) es
-  m :< TermPiElim e' es'
-substTerm sub (m :< TermSigma xts) = do
-  let xts' = substTermBindings sub xts
-  m :< TermSigma xts'
-substTerm sub (m :< TermSigmaIntro es) = do
-  let es' = map (substTerm sub) es
-  m :< TermSigmaIntro es'
-substTerm sub (m :< TermSigmaElim xts e1 e2) = do
-  let e1' = substTerm sub e1
-  let (xts', e2') = substTermBindingsWithBody sub xts e2
-  m :< TermSigmaElim xts' e1' e2'
-substTerm sub (m :< TermMu (x, t) e) = do
-  let t' = substTerm sub t
-  let e' = substTerm (filter (\(k, _) -> k /= x) sub) e
-  m :< TermMu (x, t') e'
+  let es' = map (substTermPlus sub') es
+  (m, TermEpsilonElim (x, t') e' (zip caseList es'))
+substTermPlus sub (m, TermPi xts) = do
+  let xts' = substTermPlusBindings sub xts
+  (m, TermPi xts')
+substTermPlus sub (m, TermPiIntro xts body) = do
+  let (xts', body') = substTermPlusBindingsWithBody sub xts body
+  (m, TermPiIntro xts' body')
+substTermPlus sub (m, TermPiElim e es) = do
+  let e' = substTermPlus sub e
+  let es' = map (substTermPlus sub) es
+  (m, TermPiElim e' es')
+substTermPlus sub (m, TermSigma xts) = do
+  let xts' = substTermPlusBindings sub xts
+  (m, TermSigma xts')
+substTermPlus sub (m, TermSigmaIntro es) = do
+  let es' = map (substTermPlus sub) es
+  (m, TermSigmaIntro es')
+substTermPlus sub (m, TermSigmaElim xts e1 e2) = do
+  let e1' = substTermPlus sub e1
+  let (xts', e2') = substTermPlusBindingsWithBody sub xts e2
+  (m, TermSigmaElim xts' e1' e2')
+substTermPlus sub (m, TermMu (x, t) e) = do
+  let t' = substTermPlus sub t
+  let e' = substTermPlus (filter (\(k, _) -> k /= x) sub) e
+  (m, TermMu (x, t') e')
 
-substTermBindings :: SubstTerm -> [IdentifierPlus] -> [IdentifierPlus]
-substTermBindings _ [] = []
-substTermBindings sub ((x, t):xts) = do
+substTermPlusBindings :: SubstTerm -> [IdentifierPlus] -> [IdentifierPlus]
+substTermPlusBindings _ [] = []
+substTermPlusBindings sub ((x, t):xts) = do
   let sub' = filter (\(k, _) -> k /= x) sub
-  let xts' = substTermBindings sub' xts
-  (x, substTerm sub t) : xts'
+  let xts' = substTermPlusBindings sub' xts
+  (x, substTermPlus sub t) : xts'
 
-substTermBindingsWithBody ::
-     SubstTerm -> [IdentifierPlus] -> Term -> ([IdentifierPlus], Term)
-substTermBindingsWithBody sub [] e = ([], substTerm sub e)
-substTermBindingsWithBody sub ((x, t):xts) e = do
+substTermPlusBindingsWithBody ::
+     SubstTerm -> [IdentifierPlus] -> TermPlus -> ([IdentifierPlus], TermPlus)
+substTermPlusBindingsWithBody sub [] e = ([], substTermPlus sub e)
+substTermPlusBindingsWithBody sub ((x, t):xts) e = do
   let sub' = filter (\(k, _) -> k /= x) sub
-  let (xts', e') = substTermBindingsWithBody sub' xts e
-  ((x, substTerm sub t) : xts', e')
+  let (xts', e') = substTermPlusBindingsWithBody sub' xts e
+  ((x, substTermPlus sub t) : xts', e')
 
-isValue :: Term -> Bool
-isValue (_ :< TermTau)            = True
-isValue (_ :< TermUpsilon _)      = True
-isValue (_ :< TermEpsilon _)      = True
-isValue (_ :< TermEpsilonIntro _) = True
-isValue (_ :< TermPi {})          = True
-isValue (_ :< TermPiIntro {})     = True
-isValue (_ :< TermSigma {})       = True
-isValue (_ :< TermSigmaIntro es)  = all isValue es
-isValue _                         = False
+isReducible :: TermPlus -> Bool
+isReducible (_, TermTau) = False
+isReducible (_, TermTheta _) = False
+isReducible (_, TermUpsilon _) = False
+isReducible (_, TermEpsilon _) = False
+isReducible (_, TermEpsilonIntro _) = False
+isReducible (_, TermEpsilonElim _ (_, TermEpsilonIntro l) branchList) = do
+  let (caseList, _) = unzip branchList
+  CaseLiteral l `elem` caseList || CaseDefault `elem` caseList
+isReducible (_, TermEpsilonElim (_, _) e _) = isReducible e
+isReducible (_, TermPi _) = False
+isReducible (_, TermPiIntro {}) = False
+isReducible (_, TermPiElim (_, TermPiIntro xts _) es)
+  | length xts == length es = True
+isReducible (_, TermPiElim (_, TermMu _ _) _) = True -- CBV recursion
+isReducible (_, TermPiElim (_, TermTheta c) [(_, TermEpsilonIntro (LiteralInteger _)), (_, TermEpsilonIntro (LiteralInteger _))]) -- constant application
+  | c `elem` intArithConstantList = True
+isReducible (_, TermPiElim e es) = isReducible e || any isReducible es
+isReducible (_, TermSigma _) = False
+isReducible (_, TermSigmaIntro es) = any isReducible es
+isReducible (_, TermSigmaElim xts (_, TermSigmaIntro es) _)
+  | length xts == length es = True
+isReducible (_, TermSigmaElim _ e1 _) = isReducible e1
+isReducible (_, TermMu _ _) = False
+
+isValue :: TermPlus -> Bool
+isValue (_, TermTau)            = True
+isValue (_, TermUpsilon _)      = True
+isValue (_, TermEpsilon _)      = True
+isValue (_, TermEpsilonIntro _) = True
+isValue (_, TermPi {})          = True
+isValue (_, TermPiIntro {})     = True
+isValue (_, TermSigma {})       = True
+isValue (_, TermSigmaIntro es)  = all isValue es
+isValue _                       = False
