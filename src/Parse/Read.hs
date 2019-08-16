fbc256dfd20904feb7ccad751e3426a036b0bd9c
vekatze <vekatze@icloud.com>
2019-08-16 09:07:09 +0900

    stop using cofree

--- src/Parse/Read.hs
+++ src/Parse/Read.hs
@@ -2,7 +2,7 @@ module Parse.Read
   ( strToTree
   ) where
 
-import           Control.Comonad.Cofree
+-- import           Control.Comonad.Cofree
 import           Control.Monad.State
 import           Control.Monad.Trans.Except
 import           Text.Parsec                hiding (count)
@@ -12,34 +12,34 @@ import           Data.Tree
 
 type Parser a = ParsecT String () (StateT Env (ExceptT String IO)) a
 
-strToTree :: String -> WithEnv [Tree]
+strToTree :: String -> WithEnv [TreePlus]
 strToTree input = do
   t <- runParserT (skip >> parseSExpList) () "read" input
   case t of
     Left err -> lift $ throwE (show err)
     Right p  -> return p
 
-parseSExpList :: Parser [Tree]
+parseSExpList :: Parser [TreePlus]
 parseSExpList = sepEndBy parseStr skip
 
 symbol :: Parser String
 symbol = many1 (noneOf "()[] \n;")
 
-parseStr :: Parser Tree
+parseStr :: Parser TreePlus
 parseStr = parseNode <|> parseAtom
 
-parseAtom :: Parser Tree
+parseAtom :: Parser TreePlus
 parseAtom = do
   s <- symbol
   _ <- skip
-  return $ emptyTreeMeta :< TreeAtom s
+  return (emptyTreeMeta, TreeAtom s)
 
-parseNode :: Parser Tree
+parseNode :: Parser TreePlus
 parseNode = do
   _ <- char '(' >> skip
   itemList <- many parseStr
   _ <- skip >> char ')' >> skip
-  return $ emptyTreeMeta :< TreeNode itemList
+  return (emptyTreeMeta, TreeNode itemList)
 
 skip :: Parser ()
 skip = spaces >> (comment <|> spaces)
