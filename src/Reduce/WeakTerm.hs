97130564d4e7502b0f6b210c82aca65f8377dd1b
vekatze <vekatze@icloud.com>
2019-08-16 09:28:27 +0900

    reduce

--- src/Reduce/WeakTerm.hs
+++ src/Reduce/WeakTerm.hs
@@ -1,40 +1,38 @@
-module Reduce.WeakTerm
-  ( reduceWeakTerm
+module Reduce.WeakTermPlus
+  ( reduceWeakTermPlus
   ) where
-
-import           Control.Comonad.Cofree
 
 import           Data.Basic
 import           Data.Env
 import           Data.WeakTerm
 
-reduceWeakTerm :: WeakTerm -> WithEnv WeakTerm
-reduceWeakTerm (m :< WeakTermEpsilonElim (x, t) e branchList) = do
-  e' <- reduceWeakTerm e
+reduceWeakTermPlus :: WeakTermPlus -> WithEnv WeakTermPlus
+reduceWeakTermPlus (m, WeakTermEpsilonElim (x, t) e branchList) = do
+  e' <- reduceWeakTermPlus e
   case e' of
-    _ :< WeakTermEpsilonIntro l ->
+    (_, WeakTermEpsilonIntro l) ->
       case lookup (CaseLiteral l) branchList of
-        Just body -> reduceWeakTerm $ substWeakTerm [(x, e')] body
+        Just body -> reduceWeakTermPlus $ substWeakTermPlus [(x, e')] body
         Nothing ->
           case lookup CaseDefault branchList of
-            Just body -> reduceWeakTerm $ substWeakTerm [(x, e')] body
-            Nothing -> return $ m :< WeakTermEpsilonElim (x, t) e' branchList
-    _ -> return $ m :< WeakTermEpsilonElim (x, t) e' branchList
-reduceWeakTerm (m :< WeakTermPiElim e es) = do
-  e' <- reduceWeakTerm e
-  es' <- mapM reduceWeakTerm es
+            Just body -> reduceWeakTermPlus $ substWeakTermPlus [(x, e')] body
+            Nothing -> return (m, WeakTermEpsilonElim (x, t) e' branchList)
+    _ -> return (m, WeakTermEpsilonElim (x, t) e' branchList)
+reduceWeakTermPlus (m, WeakTermPiElim e es) = do
+  e' <- reduceWeakTermPlus e
+  es' <- mapM reduceWeakTermPlus es
   case e' of
-    _ :< WeakTermPiIntro xts body
+    (_, WeakTermPiIntro xts body)
       | length xts == length es'
       , all isValue es' -> do
         let xs = map fst xts
-        reduceWeakTerm $ substWeakTerm (zip xs es') body
-    self@(_ :< WeakTermMu (x, _) body)
+        reduceWeakTermPlus $ substWeakTermPlus (zip xs es') body
+    self@(_, WeakTermMu (x, _) body)
       | all isValue es' -> do
-        let self' = substWeakTerm [(x, self)] body
-        reduceWeakTerm (m :< WeakTermPiElim self' es')
-    _ :< WeakTermTheta constant
-      | [_ :< WeakTermEpsilonIntro (LiteralInteger x), _ :< WeakTermEpsilonIntro (LiteralInteger y)] <-
+        let self' = substWeakTermPlus [(x, self)] body
+        reduceWeakTermPlus (m, WeakTermPiElim self' es')
+    (_, WeakTermTheta constant)
+      | [(_, WeakTermEpsilonIntro (LiteralInteger x)), (_, WeakTermEpsilonIntro (LiteralInteger y))] <-
          es' -> do
         let b1 = constant `elem` intAddConstantList
         let b2 = constant `elem` intSubConstantList
@@ -42,25 +40,25 @@ reduceWeakTerm (m :< WeakTermPiElim e es) = do
         let b4 = constant `elem` intDivConstantList
         case (b1, b2, b3, b4) of
           (True, _, _, _) ->
-            return $ m :< WeakTermEpsilonIntro (LiteralInteger (x + y))
+            return (m, WeakTermEpsilonIntro (LiteralInteger (x + y)))
           (_, True, _, _) ->
-            return $ m :< WeakTermEpsilonIntro (LiteralInteger (x - y))
+            return (m, WeakTermEpsilonIntro (LiteralInteger (x - y)))
           (_, _, True, _) ->
-            return $ m :< WeakTermEpsilonIntro (LiteralInteger (x * y))
+            return (m, WeakTermEpsilonIntro (LiteralInteger (x * y)))
           (_, _, _, True) ->
-            return $ m :< WeakTermEpsilonIntro (LiteralInteger (x `div` y))
-          _ -> return $ m :< WeakTermPiElim e' es'
-    _ -> return $ m :< WeakTermPiElim e' es'
-reduceWeakTerm (m :< WeakTermSigmaIntro es) = do
-  es' <- mapM reduceWeakTerm es
-  return $ m :< WeakTermSigmaIntro es'
-reduceWeakTerm (m :< WeakTermSigmaElim xts e1 e2) = do
-  e1' <- reduceWeakTerm e1
+            return (m, WeakTermEpsilonIntro (LiteralInteger (x `div` y)))
+          _ -> return (m, WeakTermPiElim e' es')
+    _ -> return (m, WeakTermPiElim e' es')
+reduceWeakTermPlus (m, WeakTermSigmaIntro es) = do
+  es' <- mapM reduceWeakTermPlus es
+  return (m, WeakTermSigmaIntro es')
+reduceWeakTermPlus (m, WeakTermSigmaElim xts e1 e2) = do
+  e1' <- reduceWeakTermPlus e1
   case e1' of
-    _ :< WeakTermSigmaIntro es
+    (_, WeakTermSigmaIntro es)
       | length es == length xts
       , all isValue es -> do
         let xs = map fst xts
-        reduceWeakTerm $ substWeakTerm (zip xs es) e2
-    _ -> return $ m :< WeakTermSigmaElim xts e1' e2
-reduceWeakTerm t = return t
+        reduceWeakTermPlus $ substWeakTermPlus (zip xs es) e2
+    _ -> return (m, WeakTermSigmaElim xts e1' e2)
+reduceWeakTermPlus t = return t
