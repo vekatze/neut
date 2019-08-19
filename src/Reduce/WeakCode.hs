03441d26aa30f53d53a3ea14ef2686f03995298b
u2zv1wx <u2zv1wx@protonmail.ch>
2019-08-19 15:04:07 +0900

    update

--- src/Reduce/WeakCode.hs
+++ src/Reduce/WeakCode.hs
@@ -1,71 +1,71 @@
-module Reduce.Polarized
-  ( reduceNeg
+module Reduce.WeakCode
+  ( reduceWeakCodePlus
   ) where
-
-import           Control.Monad.State
-import           Control.Monad.Trans.Except
 
 import           Data.Basic
 import           Data.Env
-import           Data.Polarized
+import           Data.WeakCode
 
-reduceNeg :: Neg -> WithEnv Neg
-reduceNeg (NegPiElimDownElim v vs) =
+reduceWeakCodePlus :: WeakCodePlus -> WithEnv WeakCodePlus
+reduceWeakCodePlus (m, WeakCodeEpsilonElim (x, t) v branchList) =
   case v of
-    PosConst x -> do
-      penv <- gets polEnv
-      case lookup x penv of
-        Just (args, body)
-          | length args == length vs -> reduceNeg $ substNeg (zip args vs) body
-        _ -> return $ NegPiElimDownElim v vs
-    _ -> return $ NegPiElimDownElim v vs
-reduceNeg (NegSigmaElim xs v body) =
-  case v of
-    PosSigmaIntro vs
-      | length xs == length vs -> reduceNeg $ substNeg (zip xs vs) body
-    _ -> return $ NegSigmaElim xs v body
-reduceNeg (NegEpsilonElim v branchList) =
-  case v of
-    PosEpsilonIntro l _ ->
+    (_, WeakDataEpsilonIntro l) ->
       case lookup (CaseLiteral l) branchList of
-        Just body -> reduceNeg body
+        Just body -> reduceWeakCodePlus $ substWeakCodePlus [(x, v)] body
         Nothing ->
           case lookup CaseDefault branchList of
-            Just body -> reduceNeg body
-            Nothing ->
-              lift $
-              throwE $
-              "the index " ++ show l ++ " is not included in branchList"
-    _ -> return $ NegEpsilonElim v branchList
-reduceNeg (NegUpElim x e1 e2) = do
-  e1' <- reduceNeg e1
+            Just body -> reduceWeakCodePlus $ substWeakCodePlus [(x, v)] body
+            Nothing   -> return (m, WeakCodeEpsilonElim (x, t) v branchList)
+    _ -> return (m, WeakCodeEpsilonElim (x, t) v branchList)
+reduceWeakCodePlus (m, WeakCodePiElim e vs) = do
+  e' <- reduceWeakCodePlus e
+  case e' of
+    (_, WeakCodePiIntro xts body)
+      | length xts == length vs -> do
+        let xs = map fst xts
+        reduceWeakCodePlus $ substWeakCodePlus (zip xs vs) body
+    self@(m', WeakCodeMu (x, t) body) -> do
+      let meta = WeakDataMetaNonTerminal t (obtainLocation m')
+      let x' = (meta, WeakDataDownIntro self)
+      let self' = substWeakCodePlus [(x, x')] body
+      reduceWeakCodePlus (m, WeakCodePiElim self' vs)
+    (_, WeakCodeDownElim (_, WeakDataTheta constant))
+      | [(m1, WeakDataEpsilonIntro (LiteralInteger x)), (_, WeakDataEpsilonIntro (LiteralInteger y))] <-
+         vs -> do
+        let b1 = constant `elem` intAddConstantList
+        let b2 = constant `elem` intSubConstantList
+        let b3 = constant `elem` intMulConstantList
+        let b4 = constant `elem` intDivConstantList
+        let up d = return (m, WeakCodeUpIntro d)
+        case (b1, b2, b3, b4) of
+          (True, _, _, _) ->
+            up (m1, WeakDataEpsilonIntro (LiteralInteger (x + y)))
+          (_, True, _, _) ->
+            up (m1, WeakDataEpsilonIntro (LiteralInteger (x - y)))
+          (_, _, True, _) ->
+            up (m1, WeakDataEpsilonIntro (LiteralInteger (x * y)))
+          (_, _, _, True) ->
+            up (m1, WeakDataEpsilonIntro (LiteralInteger (x `div` y)))
+          _ -> return (m, WeakCodePiElim e' vs)
+    _ -> return (m, WeakCodePiElim e' vs)
+reduceWeakCodePlus (m, WeakCodeSigmaElim xts v e) =
+  case v of
+    (_, WeakDataSigmaIntro es)
+      | length es == length xts -> do
+        let xs = map fst xts
+        reduceWeakCodePlus $ substWeakCodePlus (zip xs es) e
+    _ -> return (m, WeakCodeSigmaElim xts v e)
+reduceWeakCodePlus (m, WeakCodeUpElim (x, t) e1 e2) = do
+  e1' <- reduceWeakCodePlus e1
   case e1' of
-    NegUpIntro v -> reduceNeg $ substNeg [(x, v)] e2
-    _            -> return $ NegUpElim x e1' e2
-reduceNeg (NegConstElim (ConstantLabel x) vs) = do
-  penv <- gets polEnv
-  case lookup x penv of
-    Just (args, body)
-      | length args == length vs -> reduceNeg $ substNeg (zip args vs) body
-    _ -> return $ NegConstElim (ConstantLabel x) vs
-reduceNeg (NegConstElim c vs) = do
-  let xs = takeIntegerList vs
-  let t = LowTypeSignedInt 64 -- for now
-  case (c, xs) of
-    (ConstantArith _ ArithAdd, Just [x, y]) ->
-      return $ NegUpIntro (PosEpsilonIntro (LiteralInteger (x + y)) t)
-    (ConstantArith _ ArithSub, Just [x, y]) ->
-      return $ NegUpIntro (PosEpsilonIntro (LiteralInteger (x - y)) t)
-    (ConstantArith _ ArithMul, Just [x, y]) ->
-      return $ NegUpIntro (PosEpsilonIntro (LiteralInteger (x * y)) t)
-    (ConstantArith _ ArithDiv, Just [x, y]) ->
-      return $ NegUpIntro (PosEpsilonIntro (LiteralInteger (x `div` y)) t)
-    _ -> return $ NegConstElim c vs
-reduceNeg e = return e
+    (_, WeakCodeUpIntro v) -> reduceWeakCodePlus $ substWeakCodePlus [(x, v)] e2
+    _ -> return (m, WeakCodeUpElim (x, t) e1' e2)
+reduceWeakCodePlus (m, WeakCodeDownElim v) =
+  case v of
+    (_, WeakDataDownIntro e) -> reduceWeakCodePlus e
+    _                        -> return (m, WeakCodeDownElim v)
+reduceWeakCodePlus t = return t
 
-takeIntegerList :: [Pos] -> Maybe [Int]
-takeIntegerList [] = Just []
-takeIntegerList (PosEpsilonIntro (LiteralInteger i) _:rest) = do
-  is <- takeIntegerList rest
-  return (i : is)
-takeIntegerList _ = Nothing
+obtainLocation :: WeakCodeMeta -> Maybe (Int, Int)
+obtainLocation (WeakCodeMetaTerminal ml)      = ml
+obtainLocation (WeakCodeMetaNonTerminal _ ml) = ml
