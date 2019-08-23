14adcd5650db3e62ad884023a87c7059a6fb9ac3
u2zv1wx <u2zv1wx@protonmail.ch>
2019-08-23 17:59:40 +0900

    WeakCode ~> Code, WeakData ~> Data

---
+++ src/Data/Code.hs
@@ -0,0 +1,275 @@
+module Data.Code where
+
+import           Data.Maybe (fromMaybe)
+
+import           Data.Basic
+
+data Data
+  = DataTau
+  | DataTheta Identifier -- global variable
+  | DataUpsilon Identifier
+  | DataEpsilon Identifier
+  | DataEpsilonIntro Literal
+  | DataDownIntroPiIntro [IdentifierPlus]
+                         CodePlus
+  | DataSigma [IdentifierPlus]
+              DataPlus
+  | DataSigmaIntro [DataPlus]
+  | DataDown CodePlus
+  deriving (Show)
+
+data Code
+  = CodeTau
+  | CodeTheta Theta
+  | CodeEpsilonElim IdentifierPlus
+                    DataPlus
+                    [(Case, CodePlus)]
+  | CodePi [IdentifierPlus]
+           CodePlus
+  | CodePiElimDownElim DataPlus
+                       [DataPlus]
+  | CodeSigmaElim [IdentifierPlus]
+                  DataPlus
+                  CodePlus
+  | CodeUp DataPlus
+  | CodeUpIntro DataPlus
+  | CodeUpElim IdentifierPlus
+               CodePlus
+               CodePlus
+  | CodeMu IdentifierPlus
+           CodePlus
+  deriving (Show)
+
+data Theta
+  = ThetaArith Arith
+               DataPlus
+               DataPlus
+  | ThetaPrint DataPlus
+  deriving (Show)
+
+type IdentifierPlus = (Identifier, DataPlus)
+
+data DataMeta
+  = DataMetaTerminal (Maybe (Int, Int))
+  | DataMetaNonTerminal DataPlus
+                        (Maybe (Int, Int))
+  deriving (Show)
+
+data CodeMeta
+  = CodeMetaTerminal (Maybe (Int, Int))
+  | CodeMetaNonTerminal CodePlus
+                        (Maybe (Int, Int))
+  deriving (Show)
+
+-- FIXME: (Data, DataMeta)としたほうがe : Aに揃って読みやすいかもしれない。
+type DataPlus = (DataMeta, Data)
+
+type CodePlus = (CodeMeta, Code)
+
+obtainInfoCodeMeta :: CodeMeta -> (CodePlus, Maybe (Int, Int))
+obtainInfoCodeMeta (CodeMetaTerminal ml) = ((CodeMetaTerminal ml, CodeTau), ml)
+obtainInfoCodeMeta (CodeMetaNonTerminal t ml) = (t, ml)
+
+obtainInfoDataMeta :: DataMeta -> (DataPlus, Maybe (Int, Int))
+obtainInfoDataMeta (DataMetaTerminal ml) = ((DataMetaTerminal ml, DataTau), ml)
+obtainInfoDataMeta (DataMetaNonTerminal t ml) = (t, ml)
+
+varDataPlus :: DataPlus -> [IdentifierPlus]
+varDataPlus (_, DataTau) = []
+varDataPlus (_, DataTheta _) = []
+varDataPlus (m, DataUpsilon x) = [(x, fst $ obtainInfoDataMeta m)]
+varDataPlus (_, DataEpsilon _) = []
+varDataPlus (_, DataEpsilonIntro _) = []
+varDataPlus (_, DataDownIntroPiIntro xps e) =
+  filterPlus (`notElem` map fst xps) $
+  concatMap (varDataPlus . snd) xps ++ varCodePlus e
+varDataPlus (_, DataSigma xps p) = varDataPlusPiOrSigma xps (varDataPlus p)
+varDataPlus (_, DataSigmaIntro vs) = concatMap varDataPlus vs
+varDataPlus (_, DataDown n) = varCodePlus n
+
+varDataPlusPiOrSigma :: [IdentifierPlus] -> [IdentifierPlus] -> [IdentifierPlus]
+varDataPlusPiOrSigma [] xs = xs
+varDataPlusPiOrSigma ((x, p):xps) xs =
+  varDataPlus p ++ filterPlus (/= x) (varDataPlusPiOrSigma xps xs)
+
+varCodePlus :: CodePlus -> [IdentifierPlus]
+varCodePlus (_, CodeTau) = []
+varCodePlus (_, CodeTheta e) = varTheta e
+varCodePlus (_, CodeEpsilonElim (x, _) v branchList) = do
+  let (_, es) = unzip branchList
+  varDataPlus v ++ filterPlus (/= x) (concatMap varCodePlus es)
+varCodePlus (_, CodePi xps n) = varDataPlusPiOrSigma xps (varCodePlus n)
+varCodePlus (_, CodePiElimDownElim v vs) =
+  varDataPlus v ++ concatMap varDataPlus vs
+varCodePlus (_, CodeSigmaElim xps v e) =
+  varDataPlus v ++ filterPlus (`notElem` map fst xps) (varCodePlus e)
+varCodePlus (_, CodeUp p) = varDataPlus p
+varCodePlus (_, CodeUpIntro v) = varDataPlus v
+varCodePlus (_, CodeUpElim (x, _) e1 e2) =
+  varCodePlus e1 ++ filterPlus (/= x) (varCodePlus e2)
+varCodePlus (_, CodeMu (x, _) e) = filterPlus (/= x) $ varCodePlus e
+
+varDataPlusPi :: [IdentifierPlus] -> DataPlus -> [IdentifierPlus]
+varDataPlusPi [] n = varDataPlus n
+varDataPlusPi ((x, p):xps) n =
+  varDataPlus p ++ filterPlus (/= x) (varDataPlusPi xps n)
+
+varTheta :: Theta -> [IdentifierPlus]
+varTheta = undefined
+
+filterPlus :: (Identifier -> Bool) -> [IdentifierPlus] -> [IdentifierPlus]
+filterPlus = undefined
+
+type SubstDataPlus = [IdentifierPlus]
+
+substDataPlus :: SubstDataPlus -> DataPlus -> DataPlus
+substDataPlus sub (m, DataTau) = do
+  let m' = substDataMeta sub m
+  (m', DataTau)
+substDataPlus sub (m, DataTheta x) = do
+  let m' = substDataMeta sub m
+  (m', DataTheta x)
+substDataPlus sub (m, DataUpsilon s) = do
+  let m' = substDataMeta sub m
+  fromMaybe (m', DataUpsilon s) (lookup s sub)
+substDataPlus sub (m, DataEpsilon k) = do
+  let m' = substDataMeta sub m
+  (m', DataEpsilon k)
+substDataPlus sub (m, DataEpsilonIntro l) = do
+  let m' = substDataMeta sub m
+  (m', DataEpsilonIntro l)
+substDataPlus sub (m, DataDownIntroPiIntro xps e) = do
+  let (xps', e') = substCodePlusPi sub xps e
+  let m' = substDataMeta sub m
+  (m', DataDownIntroPiIntro xps' e')
+substDataPlus sub (m, DataSigma xps p) = do
+  let (xps', p') = substDataPlusSigma sub xps p
+  let m' = substDataMeta sub m
+  (m', DataSigma xps' p')
+substDataPlus sub (m, DataSigmaIntro vs) = do
+  let vs' = map (substDataPlus sub) vs
+  let m' = substDataMeta sub m
+  (m', DataSigmaIntro vs')
+substDataPlus sub (m, DataDown n) = do
+  let n' = substCodePlus sub n
+  let m' = substDataMeta sub m
+  (m', DataDown n')
+
+substDataMeta :: SubstDataPlus -> DataMeta -> DataMeta
+substDataMeta _ (DataMetaTerminal ml) = DataMetaTerminal ml
+substDataMeta sub (DataMetaNonTerminal p ml) =
+  DataMetaNonTerminal (substDataPlus sub p) ml
+
+substCodeMeta :: SubstDataPlus -> CodeMeta -> CodeMeta
+substCodeMeta _ (CodeMetaTerminal ml) = CodeMetaTerminal ml
+substCodeMeta sub (CodeMetaNonTerminal p ml) =
+  CodeMetaNonTerminal (substCodePlus sub p) ml
+
+substCodePlus :: SubstDataPlus -> CodePlus -> CodePlus
+substCodePlus sub (m, CodeTau) = do
+  let m' = substCodeMeta sub m
+  (m', CodeTau)
+substCodePlus sub (m, CodeTheta theta) = do
+  let m' = substCodeMeta sub m
+  let theta' = substTheta sub theta
+  (m', CodeTheta theta')
+substCodePlus sub (m, CodeEpsilonElim (x, p) v branchList) = do
+  let p' = substDataPlus sub p
+  let v' = substDataPlus sub v
+  let (cs, es) = unzip branchList
+  let es' = map (substCodePlus (filter (\(y, _) -> y /= x) sub)) es
+  let branchList' = zip cs es'
+  let m' = substCodeMeta sub m
+  (m', CodeEpsilonElim (x, p') v' branchList')
+substCodePlus sub (m, CodePi xps n) = do
+  let (xps', n') = substDataPlusPi sub xps n
+  let m' = substCodeMeta sub m
+  (m', CodePi xps' n')
+substCodePlus sub (m, CodePiElimDownElim v vs) = do
+  let v' = substDataPlus sub v
+  let vs' = map (substDataPlus sub) vs
+  let m' = substCodeMeta sub m
+  (m', CodePiElimDownElim v' vs')
+substCodePlus sub (m, CodeSigmaElim xps v e) = do
+  let v' = substDataPlus sub v
+  let (xps', e') = substDataPlusSigmaElim sub xps e
+  let m' = substCodeMeta sub m
+  (m', CodeSigmaElim xps' v' e')
+substCodePlus sub (m, CodeUp p) = do
+  let p' = substDataPlus sub p
+  let m' = substCodeMeta sub m
+  (m', CodeUp p')
+substCodePlus sub (m, CodeUpIntro v) = do
+  let v' = substDataPlus sub v
+  let m' = substCodeMeta sub m
+  (m', CodeUpIntro v')
+substCodePlus sub (m, CodeUpElim (x, p) e1 e2) = do
+  let p' = substDataPlus sub p
+  let e1' = substCodePlus sub e1
+  let e2' = substCodePlus (filter (\(y, _) -> y /= x) sub) e2
+  let m' = substCodeMeta sub m
+  (m', CodeUpElim (x, p') e1' e2')
+substCodePlus sub (m, CodeMu (x, p) e) = do
+  let p' = substDataPlus sub p
+  let e' = substCodePlus (filter (\(y, _) -> y /= x) sub) e
+  let m' = substCodeMeta sub m
+  (m', CodeMu (x, p') e')
+
+substTheta :: SubstDataPlus -> Theta -> Theta
+substTheta sub (ThetaArith a v1 v2) = do
+  let v1' = substDataPlus sub v1
+  let v2' = substDataPlus sub v2
+  ThetaArith a v1' v2'
+substTheta sub (ThetaPrint v) = ThetaPrint $ substDataPlus sub v
+
+substDataPlusPiOrSigma :: SubstDataPlus -> [IdentifierPlus] -> [IdentifierPlus]
+substDataPlusPiOrSigma _ [] = []
+substDataPlusPiOrSigma sub ((x, p):xps) = do
+  let xps' = substDataPlusPiOrSigma (filter (\(y, _) -> y /= x) sub) xps
+  let p' = substDataPlus sub p
+  (x, p') : xps'
+
+substDataPlusPi ::
+     SubstDataPlus
+  -> [IdentifierPlus]
+  -> CodePlus
+  -> ([IdentifierPlus], CodePlus)
+substDataPlusPi sub [] n = ([], substCodePlus sub n)
+substDataPlusPi sub ((x, p):xps) n = do
+  let (xps', n') = substDataPlusPi (filter (\(y, _) -> y /= x) sub) xps n
+  ((x, substDataPlus sub p) : xps', n')
+
+substDataPlusSigma ::
+     SubstDataPlus
+  -> [IdentifierPlus]
+  -> DataPlus
+  -> ([IdentifierPlus], DataPlus)
+substDataPlusSigma sub [] q = ([], substDataPlus sub q)
+substDataPlusSigma sub ((x, p):xps) q = do
+  let (xps', q') = substDataPlusSigma (filter (\(y, _) -> y /= x) sub) xps q
+  ((x, substDataPlus sub p) : xps', q')
+
+substCodePlusPi ::
+     SubstDataPlus
+  -> [IdentifierPlus]
+  -> CodePlus
+  -> ([IdentifierPlus], CodePlus)
+substCodePlusPi sub [] n = ([], substCodePlus sub n)
+substCodePlusPi sub ((x, p):xps) n = do
+  let (xps', n') = substCodePlusPi (filter (\(y, _) -> y /= x) sub) xps n
+  let p' = substDataPlus sub p
+  ((x, p') : xps', n')
+
+substDataPlusSigmaElim ::
+     SubstDataPlus
+  -> [IdentifierPlus]
+  -> CodePlus
+  -> ([IdentifierPlus], CodePlus)
+substDataPlusSigmaElim sub [] e = do
+  let e' = substCodePlus sub e
+  ([], e')
+substDataPlusSigmaElim sub ((x, p):xps) e = do
+  let sub' = filter (\(y, _) -> y /= x) sub
+  let (xps', e') = substDataPlusSigmaElim sub' xps e
+  let p' = substDataPlus sub p
+  ((x, p') : xps', e')
--- src/Polarize.hs
+++ src/Polarize.hs
@@ -13,66 +13,57 @@ import           Control.Monad.Except
 import           Prelude              hiding (pi)
 
 import           Data.Basic
+import           Data.Code
 import           Data.Env
 import           Data.Term
-import           Data.WeakCode
-import           Reduce.WeakCode
+import           Reduce.Code
 
-polarize :: TermPlus -> WithEnv WeakCodePlus
+polarize :: TermPlus -> WithEnv CodePlus
 polarize (m, TermTau) = do
   (u, ml) <- polarizeMeta m
-  return (negMeta (up u ml) ml, WeakCodeUpIntro (posMeta u ml, WeakDataTau))
+  return (negMeta (up u ml) ml, CodeUpIntro (posMeta u ml, DataTau))
 polarize (m, TermTheta x) = polarizeTheta m x
 polarize (m, TermUpsilon x) = do
   (u, ml) <- polarizeMeta m
-  return
-    (negMeta (up u ml) ml, WeakCodeUpIntro (posMeta u ml, WeakDataUpsilon x))
+  return (negMeta (up u ml) ml, CodeUpIntro (posMeta u ml, DataUpsilon x))
 polarize (m, TermEpsilon x) = do
   (u, ml) <- polarizeMeta m
-  return
-    (negMeta (up u ml) ml, WeakCodeUpIntro (posMeta u ml, WeakDataEpsilon x))
+  return (negMeta (up u ml) ml, CodeUpIntro (posMeta u ml, DataEpsilon x))
 polarize (m, TermEpsilonIntro l) = do
   (u, ml) <- polarizeMeta m
-  return
-    ( negMeta (up u ml) ml
-    , WeakCodeUpIntro (posMeta u ml, WeakDataEpsilonIntro l))
+  return (negMeta (up u ml) ml, CodeUpIntro (posMeta u ml, DataEpsilonIntro l))
 polarize (m, TermEpsilonElim (x, t) e bs) = do
   (u, ml) <- polarizeMeta m
   let (cs, es) = unzip bs
   es' <- mapM polarize es
   (y, ye) <- polarize' e
   (z, zt) <- polarize' t
-  bindLet
-    [ye, zt]
-    (negMeta (up u ml) ml, WeakCodeEpsilonElim (x, z) y (zip cs es'))
+  bindLet [ye, zt] (negMeta (up u ml) ml, CodeEpsilonElim (x, z) y (zip cs es'))
 polarize (m, TermPi xts t) = do
   (u, ml) <- polarizeMeta m
-  p <- polarize t >>= reduceWeakCodePlus >>= extract
+  p <- polarize t >>= reduceCodePlus >>= extract
   (xs', xts', xs) <- polarizePlus xts
-  let negMetaMeta = WeakCodeMetaTerminal ml
-  let z' = (negMetaMeta, WeakCodeUp p)
+  let negMetaMeta = CodeMetaTerminal ml
+  let z' = (negMetaMeta, CodeUp p)
   bindLet
     xts'
     ( negMeta (up u ml) ml
-    , WeakCodeUpIntro
-        (posMeta u ml, WeakDataDown (negMetaMeta, WeakCodePi (zip xs xs') z')))
+    , CodeUpIntro (posMeta u ml, DataDown (negMetaMeta, CodePi (zip xs xs') z')))
 polarize (m, TermPiIntro xts e) = makeClosure m xts e
 polarize (m, TermPiElim e es) = callClosure m e es
 polarize (m, TermSigma xts t) = do
   (u, ml) <- polarizeMeta m
-  p <- polarize t >>= reduceWeakCodePlus >>= extract
+  p <- polarize t >>= reduceCodePlus >>= extract
   (ys', yts', xs) <- polarizePlus xts
   bindLet
     yts'
-    ( negMeta (up u ml) ml
-    , WeakCodeUpIntro (posMeta u ml, WeakDataSigma (zip xs ys') p))
+    (negMeta (up u ml) ml, CodeUpIntro (posMeta u ml, DataSigma (zip xs ys') p))
 polarize (m, TermSigmaIntro es) = do
   (u, ml) <- polarizeMeta m
   (xs, xes) <- unzip <$> mapM polarize' es
   bindLet
     xes
-    ( negMeta (up u ml) ml
-    , WeakCodeUpIntro (posMeta u ml, WeakDataSigmaIntro xs))
+    (negMeta (up u ml) ml, CodeUpIntro (posMeta u ml, DataSigmaIntro xs))
 polarize (m, TermSigmaElim xts e1 e2) = do
   (u, ml) <- polarizeMeta m
   (z', ze1') <- polarize' e1
@@ -80,71 +71,68 @@ polarize (m, TermSigmaElim xts e1 e2) = do
   e2' <- polarize e2
   bindLet
     (ze1' : yts')
-    (negMeta (up u ml) ml, WeakCodeSigmaElim (zip xs ys') z' e2')
+    (negMeta (up u ml) ml, CodeSigmaElim (zip xs ys') z' e2')
 polarize (m, TermMu (x, t) e) = do
   e' <- polarize e
-  let fvs = varWeakCodePlus e'
+  let fvs = varCodePlus e'
   lamVar <- newNameWith "cls"
   envName <- newNameWith "env"
   let lamBody =
         ( undefined
-        , WeakCodeSigmaElim
+        , CodeSigmaElim
             fvs
-            (undefined, WeakDataUpsilon envName)
+            (undefined, DataUpsilon envName)
             ( undefined
-            , WeakCodeUpElim
+            , CodeUpElim
                 (x, undefined)
                 ( undefined
-                , WeakCodePiElimDownElim
-                    (undefined, WeakDataUpsilon lamVar)
+                , CodePiElimDownElim
+                    (undefined, DataUpsilon lamVar)
                     (map toVar fvs))
                 e'))
   insPolEnv lamVar ((envName, undefined) : undefined) lamBody
   return
     ( undefined
-    , WeakCodePiElimDownElim (undefined, WeakDataTheta lamVar) (map toVar fvs))
+    , CodePiElimDownElim (undefined, DataTheta lamVar) (map toVar fvs))
   -- mu . e ~> (down-elim f) @ fvs
   -- (u, ml) <- polarizeMeta m
   -- (y', yt') <- polarize' t
   -- (k', kt') <- polarize' e
-  -- inner <- bindLet [kt'] (negMeta (up u ml) ml, WeakCodeDownElim k')
-  -- bindLet [yt'] (negMeta (up u ml) ml, WeakCodeMu (x, y') inner)
+  -- inner <- bindLet [kt'] (negMeta (up u ml) ml, CodeDownElim k')
+  -- bindLet [yt'] (negMeta (up u ml) ml, CodeMu (x, y') inner)
 
-makeClosure ::
-     Meta -> [(Identifier, TermPlus)] -> TermPlus -> WithEnv WeakCodePlus
+makeClosure :: Meta -> [(Identifier, TermPlus)] -> TermPlus -> WithEnv CodePlus
 makeClosure m xts e = do
   (d, ml) <- polarizeMeta m
   pi <- extractFromDown d
   -- (ys', yts', xs) <- polarizePlus xts
   xps <- polarizePlus' xts
   e' <- polarize e
-  let vs = varWeakCodePlus e'
+  let vs = varCodePlus e'
   lamVar <- newNameWith "cls"
   envName <- newNameWith "env"
   let lamBody =
-        ( undefined
-        , WeakCodeSigmaElim vs (undefined, WeakDataUpsilon envName) e')
+        (undefined, CodeSigmaElim vs (undefined, DataUpsilon envName) e')
   -- lamVar == thunk (lam (envName, x1, ..., xn) lamBody)
   insPolEnv lamVar ((envName, undefined) : xps) lamBody
-  let fvEnv = (undefined, WeakDataSigmaIntro $ map toVar vs)
+  let fvEnv = (undefined, DataSigmaIntro $ map toVar vs)
   return
     ( undefined
-    , WeakCodeUpIntro
-        ( undefined
-        , WeakDataSigmaIntro [(undefined, WeakDataTheta lamVar), fvEnv]))
+    , CodeUpIntro
+        (undefined, DataSigmaIntro [(undefined, DataTheta lamVar), fvEnv]))
   -- bindLet
   --   yts'
   --   ( negMeta (up d ml) ml
-  --   , WeakCodeUpIntro
+  --   , CodeUpIntro
   --       ( posMeta d ml
-  --       , WeakDataDownIntro (negMeta pi ml, WeakCodePiIntro (zip xs ys') e')))
-  -- let lamBody = WeakCodeSigmaElim fvs (PosUpsilon envName) e
+  --       , DataDownIntro (negMeta pi ml, CodePiIntro (zip xs ys') e')))
+  -- let lamBody = CodeSigmaElim fvs (PosUpsilon envName) e
   -- -- lamVar == thunk (lam (envName, x1, ..., xn) lamBody)
   -- insPolEnv lamVar (envName : xs) lamBody
   -- let fvEnv = PosSigmaIntro $ map PosUpsilon fvs
-  -- return $ WeakCodeUpIntro $ PosSigmaIntro [PosConst lamVar, fvEnv]
+  -- return $ CodeUpIntro $ PosSigmaIntro [PosConst lamVar, fvEnv]
 
-callClosure :: Meta -> TermPlus -> [TermPlus] -> WithEnv WeakCodePlus
+callClosure :: Meta -> TermPlus -> [TermPlus] -> WithEnv CodePlus
 callClosure m e es = do
   e' <- polarize e
   es' <- mapM polarize es
@@ -156,97 +144,96 @@ callClosure m e es = do
     bindLet
       (zip argVarNameList es')
       ( undefined
-      , WeakCodeSigmaElim
+      , CodeSigmaElim
           [(thunkLamVarName, undefined), (envVarName, undefined)]
-          (undefined, WeakDataUpsilon clsVarName)
+          (undefined, DataUpsilon clsVarName)
           ( undefined
-          , WeakCodePiElimDownElim
-              (undefined, WeakDataUpsilon thunkLamVarName)
-              ((undefined, WeakDataUpsilon envVarName) :
+          , CodePiElimDownElim
+              (undefined, DataUpsilon thunkLamVarName)
+              ((undefined, DataUpsilon envVarName) :
                map undefined argVarNameList)))
-  return (undefined, WeakCodeUpElim (clsVarName, undefined) e' cont)
+  return (undefined, CodeUpElim (clsVarName, undefined) e' cont)
           -- ( undefined
-          -- , WeakCodeSigmaElim
+          -- , CodeSigmaElim
           --     [thunkLamVarName, envVarName]
           --     (PosUpsilon clsVarName) $
-          --   WeakCodePiElimDownElim
+          --   CodePiElimDownElim
           --     (PosUpsilon thunkLamVarName)
           --     (PosUpsilon envVarName : map PosUpsilon argVarNameList))
   -- return $
-  --   WeakCodeUpElim clsVarName e $
+  --   CodeUpElim clsVarName e $
   --   bindLet (zip argVarNameList es) $
-  --   WeakCodeSigmaElim [thunkLamVarName, envVarName] (PosUpsilon clsVarName) $
-  --   WeakCodePiElimDownElim
+  --   CodeSigmaElim [thunkLamVarName, envVarName] (PosUpsilon clsVarName) $
+  --   CodePiElimDownElim
   --     (PosUpsilon thunkLamVarName)
   --     (PosUpsilon envVarName : map PosUpsilon argVarNameList)
 
-toVar :: (Identifier, WeakDataPlus) -> WeakDataPlus
+toVar :: (Identifier, DataPlus) -> DataPlus
 toVar = undefined
 
-polarize' :: TermPlus -> WithEnv (WeakDataPlus, (Identifier, WeakCodePlus))
+polarize' :: TermPlus -> WithEnv (DataPlus, (Identifier, CodePlus))
 polarize' e@(m, _) = do
   e' <- polarize e
   (p, ml) <- polarizeMeta m
   x <- newNameWith "var"
-  return ((posMeta p ml, WeakDataUpsilon x), (x, e'))
+  return ((posMeta p ml, DataUpsilon x), (x, e'))
 
-bindLet :: [(Identifier, WeakCodePlus)] -> WeakCodePlus -> WithEnv WeakCodePlus
+bindLet :: [(Identifier, CodePlus)] -> CodePlus -> WithEnv CodePlus
 bindLet [] cont = return cont
 bindLet ((x, e@(m, _)):xes) cont = do
   e' <- bindLet xes cont
-  let (typeOfCont, ml) = obtainInfoWeakCodeMeta $ fst e'
-  p <- extract $ fst $ obtainInfoWeakCodeMeta m
+  let (typeOfCont, ml) = obtainInfoCodeMeta $ fst e'
+  p <- extract $ fst $ obtainInfoCodeMeta m
   -- kleisli extension (i.e. dependent up-elimination)
-  let ke = (fst typeOfCont, WeakCodeUpElim (x, p) e typeOfCont)
-  return (negMeta ke ml, WeakCodeUpElim (x, p) e e')
+  let ke = (fst typeOfCont, CodeUpElim (x, p) e typeOfCont)
+  return (negMeta ke ml, CodeUpElim (x, p) e e')
 
 obtainInfoMeta :: Meta -> (TermPlus, Maybe (Int, Int))
 obtainInfoMeta (MetaTerminal ml)      = ((MetaTerminal ml, TermTau), ml)
 obtainInfoMeta (MetaNonTerminal t ml) = (t, ml)
 
 polarizePlus ::
-     [(a, TermPlus)]
-  -> WithEnv ([WeakDataPlus], [(Identifier, WeakCodePlus)], [a])
+     [(a, TermPlus)] -> WithEnv ([DataPlus], [(Identifier, CodePlus)], [a])
 polarizePlus xts = do
   let (xs, ts) = unzip xts
   (ys', yts') <- unzip <$> mapM polarize' ts
   return (ys', yts', xs)
 
-polarizePlus' :: [(a, TermPlus)] -> WithEnv [(a, WeakDataPlus)]
+polarizePlus' :: [(a, TermPlus)] -> WithEnv [(a, DataPlus)]
 polarizePlus' xts = do
   let (xs, ts) = unzip xts
-  ps <- mapM (polarize >=> reduceWeakCodePlus >=> extract) ts
+  ps <- mapM (polarize >=> reduceCodePlus >=> extract) ts
   return $ zip xs ps
 
-polarizeMeta :: Meta -> WithEnv (WeakDataPlus, Maybe (Int, Int))
+polarizeMeta :: Meta -> WithEnv (DataPlus, Maybe (Int, Int))
 polarizeMeta m = do
   let (t, ml) = obtainInfoMeta m
-  d <- polarize t >>= reduceWeakCodePlus >>= extract
+  d <- polarize t >>= reduceCodePlus >>= extract
   return (d, ml)
 
-extract :: WeakCodePlus -> WithEnv WeakDataPlus
+extract :: CodePlus -> WithEnv DataPlus
 extract e =
   case e of
-    (_, WeakCodeUpIntro d) -> return d
-    _                      -> throwError "extract"
+    (_, CodeUpIntro d) -> return d
+    _                  -> throwError "extract"
 
-extractFromDown :: WeakDataPlus -> WithEnv WeakCodePlus
+extractFromDown :: DataPlus -> WithEnv CodePlus
 extractFromDown e =
   case e of
-    (_, WeakDataDown d) -> return d
-    _                   -> throwError "extract"
+    (_, DataDown d) -> return d
+    _               -> throwError "extract"
 
-posMeta :: WeakDataPlus -> Maybe (Int, Int) -> WeakDataMeta
-posMeta = WeakDataMetaNonTerminal
+posMeta :: DataPlus -> Maybe (Int, Int) -> DataMeta
+posMeta = DataMetaNonTerminal
 
-negMeta :: WeakCodePlus -> Maybe (Int, Int) -> WeakCodeMeta
-negMeta = WeakCodeMetaNonTerminal
+negMeta :: CodePlus -> Maybe (Int, Int) -> CodeMeta
+negMeta = CodeMetaNonTerminal
 
-up :: WeakDataPlus -> Maybe (Int, Int) -> WeakCodePlus
-up u ml = (WeakCodeMetaTerminal ml, WeakCodeUp u)
+up :: DataPlus -> Maybe (Int, Int) -> CodePlus
+up u ml = (CodeMetaTerminal ml, CodeUp u)
 
 -- expand definitions of constants
-polarizeTheta :: Meta -> Identifier -> WithEnv WeakCodePlus
+polarizeTheta :: Meta -> Identifier -> WithEnv CodePlus
 polarizeTheta m "core.i8.add"    = polarizeThetaArith ArithAdd m
 polarizeTheta m "core.i16.add"   = polarizeThetaArith ArithAdd m
 polarizeTheta m "core.i32.add"   = polarizeThetaArith ArithAdd m
@@ -266,39 +253,38 @@ polarizeTheta m "core.i64.div"   = polarizeThetaArith ArithDiv m
 polarizeTheta m "core.print.i64" = polarizeThetaPrint m
 polarizeTheta _ _                = throwError "polarize.theta"
 
-polarizeThetaArith :: Arith -> Meta -> WithEnv WeakCodePlus
+polarizeThetaArith :: Arith -> Meta -> WithEnv CodePlus
 polarizeThetaArith op m = do
   (upT, ml) <- polarizeMeta m
   case upT of
-    d@(_, WeakDataDown (_, WeakCodePi _ (_, WeakCodeUp int))) -> do
+    d@(_, DataDown (_, CodePi _ (_, CodeUp int))) -> do
       (x, varX) <- varOfType int
       (y, varY) <- varOfType int
       return
         ( negMeta (up d ml) ml
-        , WeakCodeUpIntro
+        , CodeUpIntro
             ( posMeta d ml
-            , WeakDataDownIntroPiIntro
+            , DataDownIntroPiIntro
                 [(x, int), (y, int)]
-                ( negMeta (up int ml) ml
-                , WeakCodeTheta (WeakThetaArith op varX varY))))
+                (negMeta (up int ml) ml, CodeTheta (ThetaArith op varX varY))))
     _ -> throwError "polarize.theta.arith"
 
-polarizeThetaPrint :: Meta -> WithEnv WeakCodePlus
+polarizeThetaPrint :: Meta -> WithEnv CodePlus
 polarizeThetaPrint m = do
   (upT, ml) <- polarizeMeta m
   case upT of
-    d@(_, WeakDataDown (_, WeakCodePi [(_, int)] cod)) -> do
+    d@(_, DataDown (_, CodePi [(_, int)] cod)) -> do
       (x, varX) <- varOfType int
       return
         ( negMeta (up d ml) ml
-        , WeakCodeUpIntro
+        , CodeUpIntro
             ( posMeta d ml
-            , WeakDataDownIntroPiIntro
+            , DataDownIntroPiIntro
                 [(x, int)]
-                (negMeta cod ml, WeakCodeTheta (WeakThetaPrint varX))))
+                (negMeta cod ml, CodeTheta (ThetaPrint varX))))
     _ -> throwError "polarize.theta.print"
 
-varOfType :: WeakDataPlus -> WithEnv (Identifier, WeakDataPlus)
+varOfType :: DataPlus -> WithEnv (Identifier, DataPlus)
 varOfType t = do
   x <- newNameWith "arg"
-  return (x, (posMeta t Nothing, WeakDataUpsilon x))
+  return (x, (posMeta t Nothing, DataUpsilon x))
