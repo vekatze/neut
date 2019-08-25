4841cba558cf738c1311cbb871faade95f76af95
vekatze <vekatze@icloud.com>
2019-08-25 18:07:25 +0900

    update

--- src/Parse.hs
+++ src/Parse.hs
@@ -2,7 +2,6 @@ module Parse
   ( parse
   ) where
 
-import           Control.Comonad.Cofree
 import           Control.Monad.State
 import           Control.Monad.Trans.Except
 import           System.Directory
@@ -20,15 +19,15 @@ import           Parse.Rename
 data Def =
   DefLet WeakMeta -- meta
          IdentifierPlus -- the `(x : t)` in `let (x : t) = e`
-         WeakTerm -- the `e` in `let x = e`
+         WeakTermPlus -- the `e` in `let x = e`
 
-parse :: String -> WithEnv WeakTerm
+parse :: String -> WithEnv WeakTermPlus
 parse s = strToTree s >>= parse' >>= concatDefList
 
 -- Parse the head element of the input list.
-parse' :: [Tree] -> WithEnv [Def]
+parse' :: [TreePlus] -> WithEnv [Def]
 parse' [] = return []
-parse' ((_ :< TreeNode [_ :< TreeAtom "notation", from, to]):as) =
+parse' ((_, TreeNode [(_, TreeAtom "notation"), from, to]):as) =
   if not $ isSaneNotation from
     then lift $
          throwE
@@ -36,14 +35,14 @@ parse' ((_ :< TreeNode [_ :< TreeAtom "notation", from, to]):as) =
     else do
       modify (\e -> e {notationEnv = (from, to) : notationEnv e})
       parse' as
-parse' ((_ :< TreeNode [_ :< TreeAtom "reserve", _ :< TreeAtom s]):as) = do
+parse' ((_, TreeNode [(_, TreeAtom "reserve"), (_, TreeAtom s)]):as) = do
   modify (\e -> e {reservedEnv = s : reservedEnv e})
   parse' as
-parse' ((_ :< TreeNode ((_ :< TreeAtom "sortal"):(_ :< TreeAtom name):ts)):as) = do
+parse' ((_, TreeNode ((_, TreeAtom "sortal"):(_, TreeAtom name):ts)):as) = do
   indexList <- mapM extractIdentifier ts
   insEpsilonEnv name indexList
   parse' as
-parse' ((_ :< TreeNode [_ :< TreeAtom "include", _ :< TreeAtom pathString]):as) =
+parse' ((_, TreeNode [(_, TreeAtom "include"), (_, TreeAtom pathString)]):as) =
   case readMaybe pathString :: Maybe String of
     Nothing -> lift $ throwE "the argument of `include` must be a string"
     Just path -> do
@@ -60,16 +59,16 @@ parse' ((_ :< TreeNode [_ :< TreeAtom "include", _ :< TreeAtom pathString]):as) 
           modify (\e -> e {currentDir = dirPath})
           defList <- parse' as
           return $ includedDefList ++ defList
-parse' ((_ :< TreeNode ((_ :< TreeAtom "statement"):as1)):as2) = do
+parse' ((_, TreeNode ((_, TreeAtom "statement"):as1)):as2) = do
   defList1 <- parse' as1
   defList2 <- parse' as2
   return $ defList1 ++ defList2
-parse' ((_ :< TreeNode [_ :< TreeAtom "extern", _ :< TreeAtom name]):as)
+parse' ((_, TreeNode [(_, TreeAtom "extern"), (_, TreeAtom name)]):as)
   -- Declare external constants.
  = do
   modify (\e -> e {constantEnv = name : constantEnv e})
   parse' as
-parse' ((_ :< TreeNode [_ :< TreeAtom "let", xt, e]):as) = do
+parse' ((_, TreeNode [(_, TreeAtom "let"), xt, e]):as) = do
   e' <- macroExpand e >>= interpret >>= rename
   (x, t) <- macroExpand xt >>= interpretIdentifierPlus
   t' <- rename t
@@ -84,31 +83,30 @@ parse' (a:as)
   if isSpecialForm e
     then parse' $ e : as
     else do
-      e'@(meta :< _) <- interpret e >>= rename
+      e'@(meta, _) <- interpret e >>= rename
       name <- newNameWith "hole"
       u <- wrap WeakTermTau
       t <- newHoleOfType u
       defList <- parse' as
       return $ DefLet meta (name, t) e' : defList
 
-isSpecialForm :: Tree -> Bool
-isSpecialForm (_ :< TreeNode [_ :< TreeAtom "notation", _, _]) = True
-isSpecialForm (_ :< TreeNode [_ :< TreeAtom "reserve", _ :< TreeAtom _]) = True
-isSpecialForm (_ :< TreeNode ((_ :< TreeAtom "sortal"):(_ :< TreeAtom _):_)) =
-  True
-isSpecialForm (_ :< TreeNode [_ :< TreeAtom "include", _ :< TreeAtom _]) = True
-isSpecialForm (_ :< TreeNode [_ :< TreeAtom "extern", _ :< TreeAtom _]) = True
-isSpecialForm (_ :< TreeNode ((_ :< TreeAtom "statement"):_)) = True
-isSpecialForm (_ :< TreeNode [_ :< TreeAtom "let", _, _]) = True
-isSpecialForm _ = False
+isSpecialForm :: TreePlus -> Bool
+isSpecialForm (_, TreeNode [(_, TreeAtom "notation"), _, _])           = True
+isSpecialForm (_, TreeNode [(_, TreeAtom "reserve"), (_, TreeAtom _)]) = True
+isSpecialForm (_, TreeNode ((_, TreeAtom "sortal"):(_, TreeAtom _):_)) = True
+isSpecialForm (_, TreeNode [(_, TreeAtom "include"), (_, TreeAtom _)]) = True
+isSpecialForm (_, TreeNode [(_, TreeAtom "extern"), (_, TreeAtom _)])  = True
+isSpecialForm (_, TreeNode ((_, TreeAtom "statement"):_))              = True
+isSpecialForm (_, TreeNode [(_, TreeAtom "let"), _, _])                = True
+isSpecialForm _                                                        = False
 
 -- Represent the list of Defs in the target language, using `let`.
 -- (Note that `let x := e1 in e2` can be represented as `(lam x e2) e1`.)
-concatDefList :: [Def] -> WithEnv WeakTerm
+concatDefList :: [Def] -> WithEnv WeakTermPlus
 concatDefList [] = do
   m <- newMeta
-  return $ m :< WeakTermSigmaIntro []
+  return (m, WeakTermSigmaIntro [])
 concatDefList (DefLet meta tu e:es) = do
   cont <- concatDefList es
   m <- newMeta
-  return $ meta :< WeakTermPiElim (m :< WeakTermPiIntro [tu] cont) [e]
+  return (meta, WeakTermPiElim (m, WeakTermPiIntro [tu] cont) [e])
