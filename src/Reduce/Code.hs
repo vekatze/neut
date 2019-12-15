d841a4a8f90cbd5334c0dfdae80ff2be609572ed
vekatze <vekatze@icloud.com>
2019-12-15 11:31:41 +0900

    remove inlineCodePlus

--- src/Reduce/Code.hs
+++ src/Reduce/Code.hs
@@ -1,6 +1,5 @@
 module Reduce.Code
   ( reduceCodePlus
-  , inlineCodePlus
   ) where
 
 import Control.Monad.State
@@ -51,48 +50,3 @@ reduceCodePlus (m, CodeSigmaElim xs v e) =
       | length es == length xs -> reduceCodePlus $ substCodePlus (zip xs es) e
     _ -> return (m, CodeSigmaElim xs v e)
 reduceCodePlus t = return t
-
-inlineCodePlus :: CodePlus -> WithEnv CodePlus
-inlineCodePlus (m, CodeTheta theta) =
-  case theta of
-    ThetaArith ArithAdd t (m1, DataEpsilonIntro (LiteralInteger i1) _) (_, DataEpsilonIntro (LiteralInteger i2) _) ->
-      return
-        (m, CodeUpIntro (m1, DataEpsilonIntro (LiteralInteger $ i1 + i2) t))
-    ThetaArith ArithSub t (m1, DataEpsilonIntro (LiteralInteger i1) _) (_, DataEpsilonIntro (LiteralInteger i2) _) ->
-      return
-        (m, CodeUpIntro (m1, DataEpsilonIntro (LiteralInteger $ i1 - i2) t))
-    ThetaArith ArithMul t (m1, DataEpsilonIntro (LiteralInteger i1) _) (_, DataEpsilonIntro (LiteralInteger i2) _) ->
-      return
-        (m, CodeUpIntro (m1, DataEpsilonIntro (LiteralInteger $ i1 * i2) t))
-    ThetaArith ArithDiv t (m1, DataEpsilonIntro (LiteralInteger i1) _) (_, DataEpsilonIntro (LiteralInteger i2) _) ->
-      return
-        (m, CodeUpIntro (m1, DataEpsilonIntro (LiteralInteger $ i1 `div` i2) t))
-    ThetaPrint (_, DataEpsilonIntro (LiteralInteger i) _) -> do
-      liftIO $ putStr $ show i
-      return (m, CodeUpIntro (Nothing, DataSigmaIntro []))
-    _ -> return (m, CodeTheta theta)
-inlineCodePlus (m, CodeEpsilonElim x v branchList) =
-  case v of
-    (_, DataEpsilonIntro l _) ->
-      case lookup (CaseLiteral l) branchList of
-        Just body -> inlineCodePlus $ substCodePlus [(x, v)] body
-        Nothing ->
-          case lookup CaseDefault branchList of
-            Just body -> inlineCodePlus $ substCodePlus [(x, v)] body
-            Nothing -> do
-              let (cs, es) = unzip branchList
-              es' <- mapM inlineCodePlus es
-              return (m, CodeEpsilonElim x v (zip cs es'))
-    _ -> return (m, CodeEpsilonElim x v branchList)
-inlineCodePlus (m, CodePiElimDownElim v ds)
-  -- FIXME: reduce theta when the theta is exponent
-  -- i.e. reduce `A` recursively in `exponent-i A e`
- = return (m, CodePiElimDownElim v ds)
-inlineCodePlus (m, CodeSigmaElim xs v e) =
-  case v of
-    (_, DataSigmaIntro es)
-      | length es == length xs -> inlineCodePlus $ substCodePlus (zip xs es) e
-    _ -> do
-      e' <- inlineCodePlus e
-      return (m, CodeSigmaElim xs v e')
-inlineCodePlus t = return t
