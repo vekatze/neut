3855f43803c396a96e50fa0e72e34f0cd8481380
vekatze <vekatze@icloud.com>
2019-12-23 09:20:42 +0900

    substTermPlus: apply subst to Meta too

--- src/Data/Term.hs
+++ src/Data/Term.hs
@@ -99,50 +99,49 @@ getClosedVarChainBindings ((x, t):xts) es = do
   let xs2 = getClosedVarChainBindings xts es
   xs1 ++ filter (\(y, _, _) -> y /= x) xs2
 
--- FIXME: このときmetaについてもsubstすべき
 substTermPlus :: SubstTerm -> TermPlus -> TermPlus
-substTermPlus _ (m, TermTau) = (m, TermTau)
-substTermPlus _ (m, TermTheta t) = (m, TermTheta t)
+substTermPlus sub (m, TermTau) = (substMeta sub m, TermTau)
+substTermPlus sub (m, TermTheta t) = (substMeta sub m, TermTheta t)
 substTermPlus sub (m, TermUpsilon x) =
-  fromMaybe (m, TermUpsilon x) (lookup x sub)
+  fromMaybe (substMeta sub m, TermUpsilon x) (lookup x sub)
 substTermPlus sub (m, TermPi xts) = do
   let xts' = substTermPlusBindings sub xts
-  (m, TermPi xts')
+  (substMeta sub m, TermPi xts')
 substTermPlus sub (m, TermPiIntro xts body) = do
   let (xts', body') = substTermPlusBindingsWithBody sub xts body
-  (m, TermPiIntro xts' body')
+  (substMeta sub m, TermPiIntro xts' body')
 substTermPlus sub (m, TermPiElim e es) = do
   let e' = substTermPlus sub e
   let es' = map (substTermPlus sub) es
-  (m, TermPiElim e' es')
+  (substMeta sub m, TermPiElim e' es')
 substTermPlus sub (m, TermMu (x, t) e) = do
   let t' = substTermPlus sub t
   let e' = substTermPlus (filter (\(k, _) -> k /= x) sub) e
-  (m, TermMu (x, t') e')
-substTermPlus _ (m, TermIntS size x) = (m, TermIntS size x)
-substTermPlus _ (m, TermIntU size x) = (m, TermIntU size x)
-substTermPlus _ (m, TermFloat16 x) = (m, TermFloat16 x)
-substTermPlus _ (m, TermFloat32 x) = (m, TermFloat32 x)
-substTermPlus _ (m, TermFloat64 x) = (m, TermFloat64 x)
-substTermPlus _ (m, TermEnum x) = (m, TermEnum x)
-substTermPlus _ (m, TermEnumIntro l) = (m, TermEnumIntro l)
+  (substMeta sub m, TermMu (x, t') e')
+substTermPlus sub (m, TermIntS size x) = (substMeta sub m, TermIntS size x)
+substTermPlus sub (m, TermIntU size x) = (substMeta sub m, TermIntU size x)
+substTermPlus sub (m, TermFloat16 x) = (substMeta sub m, TermFloat16 x)
+substTermPlus sub (m, TermFloat32 x) = (substMeta sub m, TermFloat32 x)
+substTermPlus sub (m, TermFloat64 x) = (substMeta sub m, TermFloat64 x)
+substTermPlus sub (m, TermEnum x) = (substMeta sub m, TermEnum x)
+substTermPlus sub (m, TermEnumIntro l) = (substMeta sub m, TermEnumIntro l)
 substTermPlus sub (m, TermEnumElim e branchList) = do
   let e' = substTermPlus sub e
   let (caseList, es) = unzip branchList
   let es' = map (substTermPlus sub) es
-  (m, TermEnumElim e' (zip caseList es'))
+  (substMeta sub m, TermEnumElim e' (zip caseList es'))
 substTermPlus sub (m, TermArray k dom cod) = do
   let dom' = substTermPlus sub dom
   let cod' = substTermPlus sub cod
-  (m, TermArray k dom' cod')
+  (substMeta sub m, TermArray k dom' cod')
 substTermPlus sub (m, TermArrayIntro k les) = do
   let (ls, es) = unzip les
   let es' = map (substTermPlus sub) es
-  (m, TermArrayIntro k (zip ls es'))
+  (substMeta sub m, TermArrayIntro k (zip ls es'))
 substTermPlus sub (m, TermArrayElim k e1 e2) = do
   let e1' = substTermPlus sub e1
   let e2' = substTermPlus sub e2
-  (m, TermArrayElim k e1' e2')
+  (substMeta sub m, TermArrayElim k e1' e2')
 
 substTermPlusBindings :: SubstTerm -> [IdentifierPlus] -> [IdentifierPlus]
 substTermPlusBindings _ [] = []
@@ -158,6 +157,10 @@ substTermPlusBindingsWithBody sub ((x, t):xts) e = do
   let sub' = filter (\(k, _) -> k /= x) sub
   let (xts', e') = substTermPlusBindingsWithBody sub' xts e
   ((x, substTermPlus sub t) : xts', e')
+
+substMeta :: SubstTerm -> Meta -> Meta
+substMeta _ (MetaTerminal ml) = MetaTerminal ml
+substMeta sub (MetaNonTerminal t ml) = MetaNonTerminal (substTermPlus sub t) ml
 
 isValue :: TermPlus -> Bool
 isValue (_, TermTau) = True
