cdbf1ce9f89dafac8b0734f66d9b5a55c99a1296
vekatze <vekatze@icloud.com>
2020-01-07 09:38:06 +0900

    remove comment on distinguish

--- src/Clarify/Linearize.hs
+++ src/Clarify/Linearize.hs
@@ -161,9 +161,6 @@ distinguishData z (ml, DataSigmaIntro ds) = do
   return (concat vss, (ml, DataSigmaIntro ds'))
 distinguishData _ d = return ([], d)
 
--- distinguishのときにtype annotationはいじらなくていいんだろうか？大丈夫そう？
--- 大丈夫そう。だって、annotationのほうは実際のコードには反映されていないから。
--- ここのannotationはあくまでlinearizeをおこなうときに必要な情報を保持しているだけで、実際のコードとは関係ない。
 distinguishCode :: Identifier -> CodePlus -> WithEnv ([Identifier], CodePlus)
 distinguishCode z (ml, CodeTheta theta) = do
   (vs, theta') <- distinguishTheta z theta
