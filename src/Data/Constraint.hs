b4178862f6429fd8e7b1c1c64e0bed92887f711a
u2zv1wx <u2zv1wx@protonmail.ch>
2020-01-14 10:37:36 +0900

    resolve patterns without inserting them into queue

--- src/Data/Constraint.hs
+++ src/Data/Constraint.hs
@@ -10,7 +10,6 @@ type IterInfo = (String, [IdentifierPlus], WeakTermPlus, WeakTermPlus)
 
 data Constraint
   = ConstraintAnalyzable
-  | ConstraintPattern Hole [[WeakTermPlus]] WeakTermPlus
   | ConstraintDelta IterInfo [(Meta, [WeakTermPlus])] [(Meta, [WeakTermPlus])]
   | ConstraintQuasiPattern Hole [[WeakTermPlus]] WeakTermPlus
   | ConstraintFlexRigid Hole [[WeakTermPlus]] WeakTermPlus
@@ -19,11 +18,10 @@ data Constraint
 
 constraintToInt :: Constraint -> Int
 constraintToInt ConstraintAnalyzable = 0
-constraintToInt ConstraintPattern {} = 1
-constraintToInt ConstraintDelta {} = 2
-constraintToInt ConstraintQuasiPattern {} = 3
-constraintToInt ConstraintFlexRigid {} = 4
-constraintToInt ConstraintOther = 5
+constraintToInt ConstraintDelta {} = 1
+constraintToInt ConstraintQuasiPattern {} = 2
+constraintToInt ConstraintFlexRigid {} = 3
+constraintToInt ConstraintOther = 4
 
 instance Eq Constraint where
   c1 == c2 = constraintToInt c1 == constraintToInt c2
