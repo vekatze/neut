cc4c552ecd05cfa0908bd19ad4bc2f7b22cebe2d
vekatze <vekatze@icloud.com>
2020-02-10 12:59:02 +0900

    update

--- src/Elaborate/Synthesize.hs
+++ src/Elaborate/Synthesize.hs
@@ -212,11 +212,12 @@ setupPosInfo ((Enriched (e1, e2) _ _):cs) = do
 
 showErrors :: [PosInfo] -> [(PosInfo, PreConstraint)] -> WithEnv [IO ()]
 showErrors _ [] = return []
-showErrors ps ((pos, (e1, e2)):pcs) = do
-  e1' <- invRename e1
-  e2' <- invRename e2
-  -- let e1' = e1
-  -- let e2' = e2
+showErrors ps ((pos, (e1, e2)):pcs)
+  -- e1' <- invRename e1
+  -- e2' <- invRename e2
+ = do
+  let e1' = e1
+  let e2' = e2
   showErrors' pos ps e1' e2' pcs
 
 showErrors' ::
