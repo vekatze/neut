81b669dc721b7ccb15421ecd962d61d2de4bcb82
vekatze <vekatze@icloud.com>
2020-02-16 13:49:15 +0900

    substLLVM

--- src/Data/LLVM.hs
+++ src/Data/LLVM.hs
@@ -49,3 +49,78 @@ data AllocSize
   = AllocSizeExact Integer
   | AllocSizePtrList Integer
   deriving (Show)
+
+type SubstLLVM = [(Identifier, LLVMData)]
+
+substLLVMData :: SubstLLVM -> LLVMData -> LLVMData
+substLLVMData sub (LLVMDataLocal x) =
+  case lookup x sub of
+    Just d -> d
+    Nothing -> LLVMDataLocal x
+substLLVMData _ d = d
+
+substLLVM :: SubstLLVM -> LLVM -> LLVM
+substLLVM sub (LLVMReturn d) = LLVMReturn $ substLLVMData sub d
+substLLVM sub (LLVMLet x op cont) = do
+  let op' = substLLVMOp sub op
+  let sub' = filter (\(y, _) -> y /= x) sub
+  let cont' = substLLVM sub' cont
+  LLVMLet x op' cont'
+substLLVM sub (LLVMCont op cont) = do
+  let op' = substLLVMOp sub op
+  let cont' = substLLVM sub cont
+  LLVMCont op' cont'
+substLLVM sub (LLVMSwitch (d, t) defaultBranch les) = do
+  let (ls, es) = unzip les
+  let d' = substLLVMData sub d
+  let defaultBranch' = substLLVM sub defaultBranch
+  let es' = map (substLLVM sub) es
+  LLVMSwitch (d', t) defaultBranch' (zip ls es')
+substLLVM sub (LLVMCall d ds) = do
+  let d' = substLLVMData sub d
+  let ds' = map (substLLVMData sub) ds
+  LLVMCall d' ds'
+substLLVM _ LLVMUnreachable = LLVMUnreachable
+
+substLLVMOp :: SubstLLVM -> LLVMOp -> LLVMOp
+substLLVMOp sub (LLVMOpCall d ds) = do
+  let d' = substLLVMData sub d
+  let ds' = map (substLLVMData sub) ds
+  LLVMOpCall d' ds'
+substLLVMOp sub (LLVMOpGetElementPtr (d, t) dts) = do
+  let d' = substLLVMData sub d
+  let (ds, ts) = unzip dts
+  let ds' = map (substLLVMData sub) ds
+  LLVMOpGetElementPtr (d', t) (zip ds' ts)
+substLLVMOp sub (LLVMOpBitcast d t1 t2) = do
+  let d' = substLLVMData sub d
+  LLVMOpBitcast d' t1 t2
+substLLVMOp sub (LLVMOpIntToPointer d t1 t2) = do
+  let d' = substLLVMData sub d
+  LLVMOpIntToPointer d' t1 t2
+substLLVMOp sub (LLVMOpPointerToInt d t1 t2) = do
+  let d' = substLLVMData sub d
+  LLVMOpPointerToInt d' t1 t2
+substLLVMOp sub (LLVMOpLoad d t) = do
+  let d' = substLLVMData sub d
+  LLVMOpLoad d' t
+substLLVMOp sub (LLVMOpStore t d1 d2) = do
+  let d1' = substLLVMData sub d1
+  let d2' = substLLVMData sub d2
+  LLVMOpStore t d1' d2'
+substLLVMOp sub (LLVMOpAlloc d) = do
+  let d' = substLLVMData sub d
+  LLVMOpAlloc d'
+substLLVMOp sub (LLVMOpFree d) = do
+  let d' = substLLVMData sub d
+  LLVMOpFree d'
+substLLVMOp sub (LLVMOpUnaryOp op d) = do
+  let d' = substLLVMData sub d
+  LLVMOpUnaryOp op d'
+substLLVMOp sub (LLVMOpBinaryOp op d1 d2) = do
+  let d1' = substLLVMData sub d1
+  let d2' = substLLVMData sub d2
+  LLVMOpBinaryOp op d1' d2'
+substLLVMOp sub (LLVMOpSysCall i ds) = do
+  let ds' = map (substLLVMData sub) ds
+  LLVMOpSysCall i ds'
