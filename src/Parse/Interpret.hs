bfaeee9c0cd25de93147ad870ee582a07f6bda19
u2zv1wx <u2zv1wx@protonmail.ch>
2020-03-21 13:49:23 +0900

    update

--- src/Parse/Interpret.hs
+++ src/Parse/Interpret.hs
@@ -5,9 +5,7 @@ module Parse.Interpret
   , interpretIdentifierPlus
   , interpretIter
   , interpretEnumItem
-  , interpretEnumValueMaybe
   , adjustPhase
-  , readEnumValueIntU
   , raiseSyntaxError
   , toIdentPlus
   ) where
@@ -24,7 +22,6 @@ import Text.Read (readMaybe)
 import qualified Data.HashMap.Strict as Map
 import qualified Data.Text as T
 
--- import qualified Text.Show.Pretty as Pr
 import Data.Basic
 import Data.Env
 import Data.Tree
@@ -617,8 +614,6 @@ raiseSyntaxError m form =
 --   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 encodeChar :: Char -> [Word8]
 encodeChar c = (map fromIntegral . go . ord) c
-  -- let result = (map fromIntegral . go . ord) c
-  -- assertP "encodeChar" result (decode result == [c])
   where
     go oc
       | oc <= 0x7f = [oc]
@@ -664,45 +659,3 @@ encodeChar c = (map fromIntegral . go . ord) c
 --   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 encode :: String -> [Word8]
 encode input = concatMap encodeChar input
-  -- let result = concatMap encodeChar input
-  -- assertP "encode" result (decode result == input)
--- adopted from https://hackage.haskell.org/package/utf8-string-1.0.1.1/docs/src/Codec-Binary-UTF8-String.html
--- replacement_character :: Char
--- replacement_character = '\xfffd'
--- adopted from https://hackage.haskell.org/package/utf8-string-1.0.1.1/docs/src/Codec-Binary-UTF8-String.html
--- this function is used only for assertion.
--- decode :: [Word8] -> String
--- decode [] = ""
--- decode (c:cs)
---   | c < 0x80 = chr (fromEnum c) : decode cs
---   | c < 0xc0 = replacement_character : decode cs
---   | c < 0xe0 = multi1
---   | c < 0xf0 = multi_byte 2 0xf 0x800
---   | c < 0xf8 = multi_byte 3 0x7 0x10000
---   | c < 0xfc = multi_byte 4 0x3 0x200000
---   | c < 0xfe = multi_byte 5 0x1 0x4000000
---   | otherwise = replacement_character : decode cs
---   where
---     multi1 =
---       case cs of
---         c1:ds
---           | c1 .&. 0xc0 == 0x80 ->
---             let d =
---                   ((fromEnum c .&. 0x1f) `shiftL` 6) .|. fromEnum (c1 .&. 0x3f)
---              in if d >= 0x000080
---                   then toEnum d : decode ds
---                   else replacement_character : decode ds
---         _ -> replacement_character : decode cs
---     multi_byte :: Int -> Word8 -> Int -> [Char]
---     multi_byte i mask overlong = aux i cs (fromEnum (c .&. mask))
---       where
---         aux 0 rs acc
---           | overlong <= acc &&
---               acc <= 0x10ffff &&
---               (acc < 0xd800 || 0xdfff < acc) && (acc < 0xfffe || 0xffff < acc) =
---             chr acc : decode rs
---           | otherwise = replacement_character : decode rs
---         aux n (r:rs) acc
---           | r .&. 0xc0 == 0x80 =
---             aux (n - 1) rs $ shiftL acc 6 .|. fromEnum (r .&. 0x3f)
---         aux _ rs _ = replacement_character : decode rs
