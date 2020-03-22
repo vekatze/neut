ea44e56566c6e96dec8dd540f08973d4587d02e7
u2zv1wx <u2zv1wx@protonmail.ch>
2020-03-22 15:14:50 +0900

    update

--- main/Main.hs
+++ main/Main.hs
@@ -8,10 +8,13 @@ import Control.Monad.State
 import Options.Applicative
 import Path
 import Path.IO
+import System.Directory (getDirectoryContents)
 import System.Exit
 import System.Process
 import Text.Read (readMaybe)
 
+import qualified Codec.Archive.Tar as Tar
+import qualified Codec.Compression.GZip as GZip
 import qualified Data.ByteString.Lazy as L
 
 import Clarify
@@ -33,6 +36,10 @@ type CheckOptEndOfEntry = String
 
 type CheckOptColorize = Bool
 
+type ArchiveOptInputPath = String
+
+type ArchiveOptOutputPath = String
+
 type Line = Int
 
 type Column = Int
@@ -50,6 +57,7 @@ instance Read OutputKind where
 data Command
   = Build BuildOptInputPath (Maybe BuildOptOutputPath) OutputKind
   | Check CheckOptInputPath CheckOptColorize (Maybe CheckOptEndOfEntry)
+  | Archive ArchiveOptInputPath (Maybe ArchiveOptOutputPath)
   | Complete FilePath Line Column
 
 main :: IO ()
@@ -64,6 +72,11 @@ parseOpt =
    command
      "check"
      (info (helper <*> parseCheckOpt) (progDesc "check specified file")) <>
+   command
+     "archive"
+     (info
+        (helper <*> parseArchiveOpt)
+        (progDesc "create archive from given path")) <>
    command
      "complete"
      (info (helper <*> parseCompleteOpt) (progDesc "show completion info")))
@@ -118,6 +131,22 @@ parseCheckOpt = do
           ]
   Check <$> inputPathOpt <*> colorizeOpt <*> footerOpt
 
+parseArchiveOpt :: Parser Command
+parseArchiveOpt = do
+  let inputPathOpt =
+        argument str $
+        mconcat [metavar "INPUT", help "The path of input directory"]
+  let outputPathOpt =
+        optional $
+        strOption $
+        mconcat
+          [ long "output"
+          , short 'o'
+          , metavar "OUTPUT"
+          , help "The path of output"
+          ]
+  Archive <$> inputPathOpt <*> outputPathOpt
+
 parseCompleteOpt :: Parser Command
 parseCompleteOpt = do
   let inputPathOpt =
@@ -150,6 +179,12 @@ run (Check inputPathStr colorizeFlag mEndOfEntry) = do
         Nothing ->
           seqIO "" (map (outputLog colorizeFlag) err) >>
           exitWith (ExitFailure 1)
+run (Archive inputPathStr mOutputPathStr) = do
+  inputPath <- resolveDir' inputPathStr
+  mOutputPath <- mapM resolveFile' mOutputPathStr
+  outputPath <- toFilePath <$> constructOutputArchivePath inputPath mOutputPath
+  contents <- getDirectoryContents $ toFilePath inputPath
+  archive outputPath (toFilePath inputPath) contents
 run (Complete inputPathStr l c) = do
   inputPath <- resolveFile' inputPathStr
   resultOrErr <- evalWithEnv (complete inputPath l c) initialEnv
@@ -166,6 +201,14 @@ constructOutputPath basename Nothing OutputKindObject = do
   dir <- getCurrentDir
   return $ dir </> basename
 constructOutputPath _ (Just path) _ = return path
+
+constructOutputArchivePath ::
+     Path Abs Dir -> Maybe (Path Abs File) -> IO (Path Abs File)
+constructOutputArchivePath inputPath Nothing = do
+  let baseName = fromRelDir $ dirname inputPath
+  outputPath <- resolveFile' baseName
+  outputPath <.> "tar.gz"
+constructOutputArchivePath _ (Just path) = return path
 
 writeResult :: L.ByteString -> Path Abs File -> OutputKind -> IO ()
 writeResult result outputPath OutputKindLLVM = do
@@ -189,3 +232,8 @@ check inputPath = parse inputPath >>= elaborate >> return ()
 seqIO :: String -> [IO ()] -> IO ()
 seqIO _ [] = return ()
 seqIO eoe (a:as) = a >> putStr eoe >> seqIO eoe as
+
+archive :: FilePath -> FilePath -> [FilePath] -> IO ()
+archive tarPath base dir = do
+  es <- Tar.pack base dir
+  L.writeFile tarPath $ GZip.compress $ Tar.write es
