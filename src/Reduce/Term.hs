8181b53f37f0acdeeafffd727f9eb3d60575049a
vekatze <vekatze@icloud.com>
2020-04-07 10:10:44 +0900

    add Reduce.normalize

--- src/Reduce/Term.hs
+++ src/Reduce/Term.hs
@@ -2,10 +2,16 @@
 
 module Reduce.Term
   ( reduceTermPlus
-  , reduceTermIdentPlus
+  , reduceIdentPlus
+  , normalize
   ) where
 
+import Control.Monad.State
+
+import qualified Data.IntMap.Strict as IntMap
+
 import Data.Basic
+import Data.Env
 import Data.Term
 
 reduceTermPlus :: TermPlus -> TermPlus
@@ -25,9 +31,9 @@ reduceTermPlus (m, TermPiIntroNoReduce xts e) = do
   let e' = reduceTermPlus e
   (m, TermPiIntroNoReduce (zip3 ms xs ts') e')
 reduceTermPlus (m, TermPiIntroPlus ind (name, is, args1, args2) xts e) = do
-  let args1' = map reduceTermIdentPlus args1
-  let args2' = map reduceTermIdentPlus args2
-  let xts' = map reduceTermIdentPlus xts
+  let args1' = map reduceIdentPlus args1
+  let args2' = map reduceIdentPlus args2
+  let xts' = map reduceIdentPlus xts
   let e' = reduceTermPlus e
   (m, TermPiIntroPlus ind (name, is, args1', args2') xts' e')
 reduceTermPlus (m, TermPiElim e es) = do
@@ -113,8 +119,8 @@ reduceTermPlus (m, TermCase indName e cxtes) = do
   (m, TermCase indName e' cxtes'')
 reduceTermPlus t = t
 
-reduceTermIdentPlus :: IdentifierPlus -> IdentifierPlus
-reduceTermIdentPlus (m, x, t) = (m, x, reduceTermPlus t)
+reduceIdentPlus :: IdentifierPlus -> IdentifierPlus
+reduceIdentPlus (m, x, t) = (m, x, reduceTermPlus t)
 
 isValue :: TermPlus -> Bool
 isValue (_, TermTau) = True
@@ -145,3 +151,123 @@ isValueConst (I (x, _))
   | x == "os:stdout" = True
   | x == "os:stderr" = True
   | otherwise = False
+
+normalize :: TermPlus -> WithEnv TermPlus
+normalize (m, TermTau) = return (m, TermTau)
+normalize (m, TermUpsilon x) = return (m, TermUpsilon x)
+normalize (m, TermPi mName xts cod) = do
+  let (ms, xs, ts) = unzip3 xts
+  ts' <- mapM normalize ts
+  cod' <- normalize cod
+  return (m, TermPi mName (zip3 ms xs ts') cod')
+normalize (m, TermPiIntro xts e) = do
+  let (ms, xs, ts) = unzip3 xts
+  ts' <- mapM normalize ts
+  e' <- normalize e
+  return (m, TermPiIntro (zip3 ms xs ts') e')
+normalize (m, TermPiIntroNoReduce xts e) = do
+  let (ms, xs, ts) = unzip3 xts
+  ts' <- mapM normalize ts
+  e' <- normalize e
+  return (m, TermPiIntroNoReduce (zip3 ms xs ts') e')
+normalize (m, TermPiIntroPlus ind (name, is, args1, args2) xts e) = do
+  args1' <- mapM normalizeIdentPlus args1
+  args2' <- mapM normalizeIdentPlus args2
+  xts' <- mapM normalizeIdentPlus xts
+  e' <- normalize e
+  return (m, TermPiIntroPlus ind (name, is, args1', args2') xts' e')
+normalize (m, TermPiElim e es) = do
+  e' <- normalize e
+  es' <- mapM normalize es
+  case e' of
+    (_, TermPiIntro xts body) -> do
+      let xs = map (\(_, x, _) -> asInt x) xts
+      normalize $ substTermPlus (zip xs es') body
+    (_, TermPiIntroNoReduce xts body) -> do
+      let xs = map (\(_, x, _) -> asInt x) xts
+      normalize $ substTermPlus (zip xs es') body
+    (_, TermPiIntroPlus _ _ xts body) -> do
+      let xs = map (\(_, x, _) -> asInt x) xts
+      normalize $ substTermPlus (zip xs es') body
+    iter@(_, TermIter (_, self, _) xts body) -> do
+      let xs = map (\(_, x, _) -> asInt x) xts
+      normalize $ substTermPlus ((asInt self, iter) : zip xs es') body
+    _ -> return (m, TermPiElim e' es')
+normalize (m, TermIter (mx, x, t) xts e) = do
+  t' <- normalize t
+  let (ms, xs, ts) = unzip3 xts
+  ts' <- mapM normalize ts
+  e' <- normalize e
+  return (m, TermIter (mx, x, t') (zip3 ms xs ts') e')
+normalize (m, TermConst x) = do
+  cenv <- gets cacheEnv
+  case IntMap.lookup (asInt x) cenv of
+    Just (Left e) -> normalize e
+    _ -> return (m, TermConst x)
+normalize (m, TermFloat16 x) = return (m, TermFloat16 x)
+normalize (m, TermFloat32 x) = return (m, TermFloat32 x)
+normalize (m, TermFloat64 x) = return (m, TermFloat64 x)
+normalize (m, TermEnum enumType) = return (m, TermEnum enumType)
+normalize (m, TermEnumIntro enumValue) = return (m, TermEnumIntro enumValue)
+normalize (m, TermEnumElim (e, t) les) = do
+  t' <- normalize t
+  e' <- normalize e
+  let (ls, es) = unzip les
+  let les'' = zip (map snd ls) es
+  case e' of
+    (_, TermEnumIntro l) ->
+      case lookup (CaseValue l) les'' of
+        Just body -> normalize body
+        Nothing ->
+          case lookup CaseDefault les'' of
+            Just body -> normalize body
+            Nothing -> do
+              es' <- mapM normalize es
+              let les' = zip ls es'
+              return (m, TermEnumElim (e', t') les')
+    _ -> do
+      es' <- mapM normalize es
+      let les' = zip ls es'
+      return (m, TermEnumElim (e', t') les')
+normalize (m, TermArray dom k) = do
+  dom' <- normalize dom
+  return (m, TermArray dom' k)
+normalize (m, TermArrayIntro k es) = do
+  es' <- mapM normalize es
+  return (m, TermArrayIntro k es')
+normalize (m, TermArrayElim k xts e1 e2) = do
+  e1' <- normalize e1
+  case e1 of
+    (_, TermArrayIntro k' es)
+      | length es == length xts
+      , k == k' -> do
+        let xs = map (\(_, x, _) -> asInt x) xts
+        normalize $ substTermPlus (zip xs es) e2
+    _ -> return (m, TermArrayElim k xts e1' e2)
+normalize (m, TermStruct ks) = return (m, TermStruct ks)
+normalize (m, TermStructIntro eks) = do
+  let (es, ks) = unzip eks
+  es' <- mapM normalize es
+  return (m, TermStructIntro $ zip es' ks)
+normalize (m, TermStructElim xks e1 e2) = do
+  e1' <- normalize e1
+  case e1' of
+    (_, TermStructIntro eks)
+      | (_, xs, ks1) <- unzip3 xks
+      , (es, ks2) <- unzip eks
+      , ks1 == ks2 -> normalize $ substTermPlus (zip (map asInt xs) es) e2
+    _ -> return (m, TermStructElim xks e1' e2)
+normalize (m, TermCase indName e cxtes) = do
+  e' <- normalize e
+  cxtes'' <-
+    flip mapM cxtes $ \((c, xts), body) -> do
+      let (ms, xs, ts) = unzip3 xts
+      ts' <- mapM normalize ts
+      body' <- normalize body
+      return ((c, zip3 ms xs ts'), body')
+  return (m, TermCase indName e' cxtes'')
+
+normalizeIdentPlus :: IdentifierPlus -> WithEnv IdentifierPlus
+normalizeIdentPlus (m, x, t) = do
+  t' <- normalize t
+  return (m, x, t')
