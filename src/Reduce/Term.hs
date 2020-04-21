48b604cb5fec7abfef97a897021d8b8f6052b5c2
vekatze <vekatze@icloud.com>
2020-04-21 14:16:19 +0900

    update

--- src/Reduce/Term.hs
+++ src/Reduce/Term.hs
@@ -1,6 +1,5 @@
 module Reduce.Term
   ( reduceTermPlus,
-    reduceIdentPlus,
     normalize,
   )
 where
@@ -14,117 +13,123 @@ import Data.Term
 import qualified Data.Text as T
 
 reduceTermPlus :: TermPlus -> TermPlus
-reduceTermPlus (m, TermPi mName xts cod) = do
-  let (ms, xs, ts) = unzip3 xts
-  let ts' = map reduceTermPlus ts
-  let cod' = reduceTermPlus cod
-  (m, TermPi mName (zip3 ms xs ts') cod')
-reduceTermPlus (m, TermPiIntro info xts e) = do
-  let info' = fmap2 (map reduceIdentPlus) info
-  let (ms, xs, ts) = unzip3 xts
-  let ts' = map reduceTermPlus ts
-  let e' = reduceTermPlus e
-  (m, TermPiIntro info' (zip3 ms xs ts') e')
-reduceTermPlus (m, TermPiElim e es) = do
-  let e' = reduceTermPlus e
-  let es' = map reduceTermPlus es
-  let app = TermPiElim e' es'
-  let valueCond = and $ map isValue es
-  case e' of
-    (_, TermPiIntro Nothing xts body) -- fixme: reduceできるだけreduceするようにする (partial evaluation)
-      | length xts == length es',
-        valueCond -> do
-        let xs = map (\(_, x, _) -> asInt x) xts
-        let sub = IntMap.fromList $ zip xs es'
-        reduceTermPlus $ substTermPlus sub body
-    _ -> (m, app)
-reduceTermPlus (m, TermIter (mx, x, t) xts e)
-  | x `notElem` varTermPlus e = reduceTermPlus (m, termPiIntro xts e)
-  | otherwise = do
-    let t' = reduceTermPlus t
-    let (ms, xs, ts) = unzip3 xts
-    let ts' = map reduceTermPlus ts
-    let e' = reduceTermPlus e
-    (m, TermIter (mx, x, t') (zip3 ms xs ts') e')
-reduceTermPlus (m, TermEnumElim (e, t) les) = do
-  let t' = reduceTermPlus t
-  let e' = reduceTermPlus e
-  let (ls, es) = unzip les
-  let les'' = zip (map snd ls) es
-  case e' of
-    (_, TermEnumIntro l) ->
-      case lookup (CaseValue l) les'' of
-        Just body -> reduceTermPlus body
-        Nothing ->
-          case lookup CaseDefault les'' of
-            Just body -> reduceTermPlus body
-            Nothing -> do
-              let es' = map reduceTermPlus es
-              let les' = zip ls es'
-              (m, TermEnumElim (e', t') les')
-    _ -> do
+reduceTermPlus term =
+  case term of
+    (m, TermPi mName xts cod) -> do
+      let (ms, xs, ts) = unzip3 xts
+      let ts' = map reduceTermPlus ts
+      let cod' = reduceTermPlus cod
+      (m, TermPi mName (zip3 ms xs ts') cod')
+    (m, TermPiIntro info xts e) -> do
+      let info' = fmap2 (map reduceIdentPlus) info
+      let (ms, xs, ts) = unzip3 xts
+      let ts' = map reduceTermPlus ts
+      let e' = reduceTermPlus e
+      (m, TermPiIntro info' (zip3 ms xs ts') e')
+    (m, TermPiElim e es) -> do
+      let e' = reduceTermPlus e
       let es' = map reduceTermPlus es
-      let les' = zip ls es'
-      (m, TermEnumElim (e', t') les')
-reduceTermPlus (m, TermArray dom k) = do
-  let dom' = reduceTermPlus dom
-  (m, TermArray dom' k)
-reduceTermPlus (m, TermArrayIntro k es) = do
-  let es' = map reduceTermPlus es
-  (m, TermArrayIntro k es')
-reduceTermPlus (m, TermArrayElim k xts e1 e2) = do
-  let e1' = reduceTermPlus e1
-  case e1 of
-    (_, TermArrayIntro k' es)
-      | length es == length xts,
-        k == k' -> do
-        let xs = map (\(_, x, _) -> asInt x) xts
-        let sub = IntMap.fromList $ zip xs es
-        reduceTermPlus $ substTermPlus sub e2
-    _ -> (m, TermArrayElim k xts e1' e2)
-reduceTermPlus (m, TermStructIntro eks) = do
-  let (es, ks) = unzip eks
-  let es' = map reduceTermPlus es
-  (m, TermStructIntro $ zip es' ks)
-reduceTermPlus (m, TermStructElim xks e1 e2) = do
-  let e1' = reduceTermPlus e1
-  case e1' of
-    (_, TermStructIntro eks)
-      | (_, xs, ks1) <- unzip3 xks,
-        (es, ks2) <- unzip eks,
-        ks1 == ks2 -> do
-        let sub = IntMap.fromList $ zip (map asInt xs) es
-        reduceTermPlus $ substTermPlus sub e2
-    _ -> (m, TermStructElim xks e1' e2)
-reduceTermPlus (m, TermCase indName e cxtes) = do
-  let e' = reduceTermPlus e
-  let cxtes'' =
-        flip map cxtes $ \((c, xts), body) -> do
-          let (ms, xs, ts) = unzip3 xts
-          let ts' = map reduceTermPlus ts
-          let body' = reduceTermPlus body
-          ((c, zip3 ms xs ts'), body')
-  (m, TermCase indName e' cxtes'')
-reduceTermPlus t = t
+      let app = TermPiElim e' es'
+      let valueCond = and $ map isValue es
+      case e' of
+        (_, TermPiIntro Nothing xts body) -- fixme: reduceできるだけreduceするようにする (partial evaluation)
+          | length xts == length es',
+            valueCond -> do
+            let xs = map (\(_, x, _) -> asInt x) xts
+            let sub = IntMap.fromList $ zip xs es'
+            reduceTermPlus $ substTermPlus sub body
+        _ -> (m, app)
+    (m, TermIter (mx, x, t) xts e)
+      | x `notElem` varTermPlus e ->
+        reduceTermPlus (m, termPiIntro xts e)
+      | otherwise -> do
+        let t' = reduceTermPlus t
+        let (ms, xs, ts) = unzip3 xts
+        let ts' = map reduceTermPlus ts
+        let e' = reduceTermPlus e
+        (m, TermIter (mx, x, t') (zip3 ms xs ts') e')
+    (m, TermEnumElim (e, t) les) -> do
+      let t' = reduceTermPlus t
+      let e' = reduceTermPlus e
+      let (ls, es) = unzip les
+      let les'' = zip (map snd ls) es
+      case e' of
+        (_, TermEnumIntro l) ->
+          case lookup (CaseValue l) les'' of
+            Just body -> reduceTermPlus body
+            Nothing ->
+              case lookup CaseDefault les'' of
+                Just body -> reduceTermPlus body
+                Nothing -> do
+                  let es' = map reduceTermPlus es
+                  let les' = zip ls es'
+                  (m, TermEnumElim (e', t') les')
+        _ -> do
+          let es' = map reduceTermPlus es
+          let les' = zip ls es'
+          (m, TermEnumElim (e', t') les')
+    (m, TermArray dom k) -> do
+      let dom' = reduceTermPlus dom
+      (m, TermArray dom' k)
+    (m, TermArrayIntro k es) -> do
+      let es' = map reduceTermPlus es
+      (m, TermArrayIntro k es')
+    (m, TermArrayElim k xts e1 e2) -> do
+      let e1' = reduceTermPlus e1
+      case e1 of
+        (_, TermArrayIntro k' es)
+          | length es == length xts,
+            k == k' -> do
+            let xs = map (\(_, x, _) -> asInt x) xts
+            let sub = IntMap.fromList $ zip xs es
+            reduceTermPlus $ substTermPlus sub e2
+        _ -> (m, TermArrayElim k xts e1' e2)
+    (m, TermStructIntro eks) -> do
+      let (es, ks) = unzip eks
+      let es' = map reduceTermPlus es
+      (m, TermStructIntro $ zip es' ks)
+    (m, TermStructElim xks e1 e2) -> do
+      let e1' = reduceTermPlus e1
+      case e1' of
+        (_, TermStructIntro eks)
+          | (_, xs, ks1) <- unzip3 xks,
+            (es, ks2) <- unzip eks,
+            ks1 == ks2 -> do
+            let sub = IntMap.fromList $ zip (map asInt xs) es
+            reduceTermPlus $ substTermPlus sub e2
+        _ -> (m, TermStructElim xks e1' e2)
+    (m, TermCase indName e cxtes) -> do
+      let e' = reduceTermPlus e
+      let cxtes'' =
+            flip map cxtes $ \((c, xts), body) -> do
+              let (ms, xs, ts) = unzip3 xts
+              let ts' = map reduceTermPlus ts
+              let body' = reduceTermPlus body
+              ((c, zip3 ms xs ts'), body')
+      (m, TermCase indName e' cxtes'')
+    _ -> term
 
 reduceIdentPlus :: IdentPlus -> IdentPlus
-reduceIdentPlus (m, x, t) = (m, x, reduceTermPlus t)
+reduceIdentPlus (m, x, t) =
+  (m, x, reduceTermPlus t)
 
 isValue :: TermPlus -> Bool
-isValue (_, TermTau) = True
-isValue (_, TermUpsilon _) = True
-isValue (_, TermPi {}) = True
-isValue (_, TermPiIntro {}) = True
-isValue (_, TermIter {}) = True
-isValue (_, TermConst x) = isValueConst x
-isValue (_, TermFloat _ _) = True
-isValue (_, TermEnum _) = True
-isValue (_, TermEnumIntro _) = True
-isValue (_, TermArray {}) = True
-isValue (_, TermArrayIntro _ es) = and $ map isValue es
-isValue (_, TermStruct {}) = True
-isValue (_, TermStructIntro eks) = and $ map (isValue . fst) eks
-isValue _ = False
+isValue term =
+  case term of
+    (_, TermTau) -> True
+    (_, TermUpsilon _) -> True
+    (_, TermPi {}) -> True
+    (_, TermPiIntro {}) -> True
+    (_, TermIter {}) -> True
+    (_, TermConst x) -> isValueConst x
+    (_, TermFloat _ _) -> True
+    (_, TermEnum _) -> True
+    (_, TermEnumIntro _) -> True
+    (_, TermArray {}) -> True
+    (_, TermArrayIntro _ es) -> and $ map isValue es
+    (_, TermStruct {}) -> True
+    (_, TermStructIntro eks) -> and $ map (isValue . fst) eks
+    _ -> False
 
 isValueConst :: T.Text -> Bool
 isValueConst x
