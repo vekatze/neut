794f0fdd4fb919a60e84c2bb0064b320fa6971ea
vekatze <vekatze@icloud.com>
2020-04-21 17:06:35 +0900

    update

--- src/Data/Tree.hs
+++ src/Data/Tree.hs
@@ -12,26 +12,45 @@ data Tree
 type TreePlus = (Meta, Tree)
 
 asLeaf :: TreePlus -> Maybe (Meta, T.Text)
-asLeaf (m, TreeLeaf x) = Just (m, x)
-asLeaf _ = Nothing
+asLeaf tree =
+  case tree of
+    (m, TreeLeaf x) ->
+      Just (m, x)
+    _ ->
+      Nothing
 
 atomListOf :: TreePlus -> S.Set T.Text
-atomListOf (_, TreeLeaf x) = S.singleton x
-atomListOf (_, TreeNode ts) = S.unions $ map atomListOf ts
+atomListOf tree =
+  case tree of
+    (_, TreeLeaf x) ->
+      S.singleton x
+    (_, TreeNode ts) ->
+      S.unions $ map atomListOf ts
 
 showAsSExp :: TreePlus -> T.Text
-showAsSExp (_, TreeLeaf x) = x
-showAsSExp (_, TreeNode ts) =
-  "(" <> T.intercalate " " (map showAsSExp ts) <> ")"
+showAsSExp tree =
+  case tree of
+    (_, TreeLeaf x) ->
+      x
+    (_, TreeNode ts) ->
+      "(" <> T.intercalate " " (map showAsSExp ts) <> ")"
 
 replaceMeta :: Meta -> TreePlus -> TreePlus
-replaceMeta m (m', TreeLeaf x) = (supMeta m m', TreeLeaf x)
-replaceMeta m (mt, TreeNode ts) = do
-  let ts' = map (replaceMeta m) ts
-  (supMeta m mt, TreeNode ts')
+replaceMeta m tree =
+  case tree of
+    (m', TreeLeaf x) ->
+      (supMeta m m', TreeLeaf x)
+    (mt, TreeNode ts) -> do
+      let ts' = map (replaceMeta m) ts
+      (supMeta m mt, TreeNode ts')
 
 substTree :: (T.Text, T.Text) -> TreePlus -> TreePlus
-substTree (from, to) (m, TreeLeaf x)
-  | x == from = (m, TreeLeaf to)
-  | otherwise = (m, TreeLeaf x)
-substTree sub (m, TreeNode ts) = (m, TreeNode $ map (substTree sub) ts)
+substTree sub@(from, to) tree =
+  case tree of
+    (m, TreeLeaf x)
+      | x == from ->
+        (m, TreeLeaf to)
+      | otherwise ->
+        (m, TreeLeaf x)
+    (m, TreeNode ts) ->
+      (m, TreeNode $ map (substTree sub) ts)
