199efa2e746255ac1d55428c10131fb5059e7d72
vekatze <vekatze@icloud.com>
2020-04-23 22:37:11 +0900

    warn unused var only after elaboration

--- src/Elaborate.hs
+++ src/Elaborate.hs
@@ -28,7 +28,8 @@ elaborate =
 elaborateStmt :: [WeakStmt] -> WithEnv Stmt
 elaborateStmt stmt =
   case stmt of
-    [] ->
+    [] -> do
+      warnUnusedVar
       StmtReturn . newMeta 1 1 <$> getCurrentFilePath
     WeakStmtLet m (mx, x, t) e : cont -> do
       (e', te) <- infer e
@@ -382,3 +383,22 @@ lookupEnumSet m name = do
       raiseError m $ "no such enum defined: " <> name
     Just xis ->
       return $ map fst xis
+
+warnUnusedVar :: WithEnv ()
+warnUnusedVar =
+  whenCheck $ do
+    set <- gets intactSet
+    let set' = S.map (\(m, x) -> (getPosInfo m, x)) set
+    warnUnusedVar' $ S.toList set'
+
+warnUnusedVar' :: [(PosInfo, T.Text)] -> WithEnv ()
+warnUnusedVar' infoList =
+  case infoList of
+    [] ->
+      return ()
+    ((pos, x) : pxs)
+      | T.all (`S.notMember` S.fromList "()") x -> do
+        warn pos $ "defined but not used: `" <> x <> "`"
+        warnUnusedVar' pxs
+      | otherwise ->
+        warnUnusedVar' pxs
