5d0be19c18d487703fb6c50a9221ad02b6b8aa4e
vekatze <vekatze@icloud.com>
2020-04-25 11:35:23 +0900

    update

--- src/Data/Basic.hs
+++ src/Data/Basic.hs
@@ -725,41 +725,7 @@ showInHex' w = do
 
 hex :: Int -> T.Text
 hex i =
-  case i of
-    0 ->
-      "0"
-    1 ->
-      "1"
-    2 ->
-      "2"
-    3 ->
-      "3"
-    4 ->
-      "4"
-    5 ->
-      "5"
-    6 ->
-      "6"
-    7 ->
-      "7"
-    8 ->
-      "8"
-    9 ->
-      "9"
-    10 ->
-      "a"
-    11 ->
-      "b"
-    12 ->
-      "c"
-    13 ->
-      "d"
-    14 ->
-      "e"
-    15 ->
-      "f"
-    _ ->
-      " "
+  T.singleton $ "0123456789abcdef" !! i
 
 fmap2 :: (Functor f, Functor g) => (a -> b) -> f (g a) -> f (g b)
 fmap2 f =
