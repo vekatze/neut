894a9f6a138bb4a2d228e6df4eaf7827cd36c107
u2zv1wx <u2zv1wx@protonmail.ch>
2020-04-25 16:09:55 +0900

    update

--- src/Clarify.hs
+++ src/Clarify.hs
@@ -208,15 +208,15 @@ clarifyConst tenv m x
     returnCartesianImmediate m
   | Just lowType <- asArrayAccessMaybe x =
     clarifyArrayAccess tenv m x lowType
-  | x == "os.file-descriptor" =
+  | x == "os" <> nsSep <> "file-descriptor" =
     returnCartesianImmediate m
-  | x == "os.stdin" =
+  | x == "os" <> nsSep <> "stdin" =
     clarify' tenv (m, TermInt 64 0)
-  | x == "os.stdout" =
+  | x == "os" <> nsSep <> "stdout" =
     clarify' tenv (m, TermInt 64 1)
-  | x == "os.stderr" =
+  | x == "os" <> nsSep <> "stderr" =
     clarify' tenv (m, TermInt 64 2)
-  | x == "unsafe.cast" =
+  | x == "unsafe" <> nsSep <> "cast" =
     clarifyCast tenv m
   | otherwise = do
     os <- getOS
@@ -345,62 +345,60 @@ knot m z cls = do
 asSysCallMaybe :: OS -> T.Text -> Maybe (Syscall, [Arg])
 asSysCallMaybe os name =
   case os of
-    OSLinux ->
-      case name of
-        "os.read" ->
-          return (Right ("read", 0), [ArgUnused, ArgImm, ArgArray, ArgImm])
-        "os.write" ->
-          return (Right ("write", 1), [ArgUnused, ArgImm, ArgArray, ArgImm])
-        "os.open" ->
-          return (Right ("open", 2), [ArgUnused, ArgArray, ArgImm, ArgImm])
-        "os.close" ->
-          return (Right ("close", 3), [ArgImm])
-        "os.socket" ->
-          return (Right ("socket", 41), [ArgImm, ArgImm, ArgImm])
-        "os.connect" ->
-          return (Right ("connect", 42), [ArgImm, ArgStruct, ArgImm])
-        "os.accept" ->
-          return (Right ("accept", 43), [ArgImm, ArgStruct, ArgArray])
-        "os.bind" ->
-          return (Right ("bind", 49), [ArgImm, ArgStruct, ArgImm])
-        "os.listen" ->
-          return (Right ("listen", 50), [ArgImm, ArgImm])
-        "os.fork" ->
-          return (Right ("fork", 57), [])
-        "os.exit" ->
-          return (Right ("exit", 60), [ArgUnused, ArgImm])
-        "os.wait4" ->
-          return (Right ("wait4", 61), [ArgImm, ArgArray, ArgImm, ArgStruct])
-        _ ->
-          Nothing
-    OSDarwin ->
-      case name of
-        "os.exit" ->
-          return (Left "exit", [ArgUnused, ArgImm]) -- 0x2000001
-        "os.fork" ->
-          return (Left "fork", []) -- 0x2000002
-        "os.read" ->
-          return (Left "read", [ArgUnused, ArgImm, ArgArray, ArgImm]) -- 0x2000003
-        "os.write" ->
-          return (Left "write", [ArgUnused, ArgImm, ArgArray, ArgImm]) -- 0x2000004
-        "os.open" ->
-          return (Left "open", [ArgUnused, ArgArray, ArgImm, ArgImm]) -- 0x2000005
-        "os.close" ->
-          return (Left "close", [ArgImm]) -- 0x2000006
-        "os.wait4" ->
-          return (Left "wait4", [ArgImm, ArgArray, ArgImm, ArgStruct]) -- 0x2000007
-        "os.accept" ->
-          return (Left "accept", [ArgImm, ArgStruct, ArgArray]) -- 0x2000030
-        "os.socket" ->
-          return (Left "socket", [ArgImm, ArgImm, ArgImm]) -- 0x2000097
-        "os.connect" ->
-          return (Left "connect", [ArgImm, ArgStruct, ArgImm]) -- 0x2000098
-        "os.bind" ->
-          return (Left "bind", [ArgImm, ArgStruct, ArgImm]) -- 0x2000104
-        "os.listen" ->
-          return (Left "listen", [ArgImm, ArgImm]) -- 0x2000106
-        _ ->
-          Nothing
+    OSLinux
+      | name == "os" <> nsSep <> "read" ->
+        return (Right ("read", 0), [ArgUnused, ArgImm, ArgArray, ArgImm])
+      | name == "os" <> nsSep <> "write" ->
+        return (Right ("write", 1), [ArgUnused, ArgImm, ArgArray, ArgImm])
+      | name == "os" <> nsSep <> "open" ->
+        return (Right ("open", 2), [ArgUnused, ArgArray, ArgImm, ArgImm])
+      | name == "os" <> nsSep <> "close" ->
+        return (Right ("close", 3), [ArgImm])
+      | name == "os" <> nsSep <> "socket" ->
+        return (Right ("socket", 41), [ArgImm, ArgImm, ArgImm])
+      | name == "os" <> nsSep <> "connect" ->
+        return (Right ("connect", 42), [ArgImm, ArgStruct, ArgImm])
+      | name == "os" <> nsSep <> "accept" ->
+        return (Right ("accept", 43), [ArgImm, ArgStruct, ArgArray])
+      | name == "os" <> nsSep <> "bind" ->
+        return (Right ("bind", 49), [ArgImm, ArgStruct, ArgImm])
+      | name == "os" <> nsSep <> "listen" ->
+        return (Right ("listen", 50), [ArgImm, ArgImm])
+      | name == "os" <> nsSep <> "fork" ->
+        return (Right ("fork", 57), [])
+      | name == "os" <> nsSep <> "exit" ->
+        return (Right ("exit", 60), [ArgUnused, ArgImm])
+      | name == "os" <> nsSep <> "wait4" ->
+        return (Right ("wait4", 61), [ArgImm, ArgArray, ArgImm, ArgStruct])
+      | otherwise ->
+        Nothing
+    OSDarwin
+      | name == "os" <> nsSep <> "exit" ->
+        return (Left "exit", [ArgUnused, ArgImm]) -- 0x2000001
+      | name == "os" <> nsSep <> "fork" ->
+        return (Left "fork", []) -- 0x2000002
+      | name == "os" <> nsSep <> "read" ->
+        return (Left "read", [ArgUnused, ArgImm, ArgArray, ArgImm]) -- 0x2000003
+      | name == "os" <> nsSep <> "write" ->
+        return (Left "write", [ArgUnused, ArgImm, ArgArray, ArgImm]) -- 0x2000004
+      | name == "os" <> nsSep <> "open" ->
+        return (Left "open", [ArgUnused, ArgArray, ArgImm, ArgImm]) -- 0x2000005
+      | name == "os" <> nsSep <> "close" ->
+        return (Left "close", [ArgImm]) -- 0x2000006
+      | name == "os" <> nsSep <> "wait4" ->
+        return (Left "wait4", [ArgImm, ArgArray, ArgImm, ArgStruct]) -- 0x2000007
+      | name == "os" <> nsSep <> "accept" ->
+        return (Left "accept", [ArgImm, ArgStruct, ArgArray]) -- 0x2000030
+      | name == "os" <> nsSep <> "socket" ->
+        return (Left "socket", [ArgImm, ArgImm, ArgImm]) -- 0x2000097
+      | name == "os" <> nsSep <> "connect" ->
+        return (Left "connect", [ArgImm, ArgStruct, ArgImm]) -- 0x2000098
+      | name == "os" <> nsSep <> "bind" ->
+        return (Left "bind", [ArgImm, ArgStruct, ArgImm]) -- 0x2000104
+      | name == "os" <> nsSep <> "listen" ->
+        return (Left "listen", [ArgImm, ArgImm]) -- 0x2000106
+      | otherwise ->
+        Nothing
 
 data Arg
   = ArgImm
