531518aea595b65e544bdd017a4b45c97d8f7b64
u2zv1wx <u2zv1wx@protonmail.ch>
2020-05-06 01:04:37 +0900

    update

--- src/LLVM.hs
+++ src/LLVM.hs
@@ -25,8 +25,8 @@ import Reduce.Code
 toLLVM :: CodePlus -> WithEnv LLVM
 toLLVM mainTerm@(m, _) = do
   modify (\env -> env {nameSet = S.empty})
-  mainTerm' <- reduceCodePlus mainTerm
-  -- let mainTerm' = mainTerm
+  -- mainTerm' <- reduceCodePlus mainTerm
+  let mainTerm' = mainTerm
   modify (\env -> env {nameSet = S.empty})
   mainTerm'' <- llvmCode mainTerm'
   -- the result of "main" must be i64, not i8*
