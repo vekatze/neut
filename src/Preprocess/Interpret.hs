f1059a407b11ac13a26abafba192505ad35c47c6
vekatze <vekatze@icloud.com>
2021-02-16 10:11:54 +0900

    update

--- src/Preprocess/Interpret.hs
+++ src/Preprocess/Interpret.hs
@@ -11,46 +11,70 @@ import Data.Tree
 
 interpretMetaTerm :: TreePlus -> WithEnv MetaTermPlus
 interpretMetaTerm tree =
+  interpretMetaTerm' 0 tree
+
+interpretMetaTerm' :: Int -> TreePlus -> WithEnv MetaTermPlus
+interpretMetaTerm' level tree =
   case tree of
-    (m, TreeLeaf atom) ->
-      return (m, MetaTermVar $ asIdent atom)
+    (m, TreeLeaf atom)
+      | level == 0 ->
+        return (m, MetaTermLeaf atom)
+      | otherwise ->
+        return (m, MetaTermVar $ asIdent atom)
     (m, TreeNode (leaf@(_, TreeLeaf headAtom) : rest)) ->
       case headAtom of
-        "implication-introduction"
+        "LAMBDA"
           | [(_, TreeNode xs), e] <- rest -> do
-            xs' <- mapM interpretIdent xs
-            e' <- interpretMetaTerm e
-            return (m, MetaTermImpIntro xs' e')
+            if level <= 0
+              then raiseError m "a meta-abstraction can only be used in the meta language"
+              else do
+                xs' <- mapM interpretIdent xs
+                e' <- interpretMetaTerm' level e
+                return (m, MetaTermImpIntro xs' e')
           | otherwise ->
-            raiseSyntaxError m "(implication-introduction (TREE*) TREE)"
-        "implication-elimination"
+            raiseSyntaxError m "(LAMBDA (TREE*) TREE)"
+        "APPLY"
           | e : es <- rest -> do
-            e' <- interpretMetaTerm e
-            es' <- mapM interpretMetaTerm es
-            return (m, MetaTermImpElim e' es')
+            if level <= 0
+              then raiseError m "a meta-application can only be used in the meta language"
+              else do
+                e' <- interpretMetaTerm' level e
+                es' <- mapM (interpretMetaTerm' level) es
+                return (m, MetaTermImpElim e' es')
           | otherwise ->
-            raiseSyntaxError m "(implication-elimination TREE TREE*)"
-        "necessity-introduction"
+            raiseSyntaxError m "(APPLY TREE TREE*)"
+        "QUOTE"
           | [e] <- rest -> do
-            e' <- interpretMetaTerm e
-            return (m, MetaTermNecIntro e')
+            if level <= 0
+              then raiseError m "a quotation can only be used in the meta language"
+              else do
+                e' <- interpretMetaTerm' (level - 1) e
+                return (m, MetaTermNecIntro e')
           | otherwise ->
             raiseSyntaxError m "(necessity-introduction TREE)"
-        "necessity-elimination"
+        "UNQUOTE"
           | [e] <- rest -> do
-            e' <- interpretMetaTerm e
+            e' <- interpretMetaTerm' (level + 1) e
             return (m, MetaTermNecElim e')
           | otherwise ->
-            raiseSyntaxError m "(necessity-elimination TREE)"
+            raiseSyntaxError m "(UNQUOTE TREE)"
         _ ->
-          interpretMetaTermAux m $ leaf : rest
+          interpretMetaTermAux level m $ leaf : rest
     (m, TreeNode es) ->
-      interpretMetaTermAux m es
+      interpretMetaTermAux level m es
 
-interpretMetaTermAux :: Hint -> [TreePlus] -> WithEnv MetaTermPlus
-interpretMetaTermAux m es = do
-  es' <- mapM interpretMetaTerm es
-  return (m, MetaTermNode es')
+interpretMetaTermAux :: Int -> Hint -> [TreePlus] -> WithEnv MetaTermPlus
+interpretMetaTermAux level m es = do
+  es' <- mapM (interpretMetaTerm' level) es
+  if level == 0
+    then return (m, MetaTermNode es')
+    else case es of
+      [] ->
+        raiseSyntaxError m "(TREE TREE*)"
+      f : args -> do
+        f' <- interpretMetaTerm' level f
+        args' <- mapM (interpretMetaTerm' level) args
+        return (m, MetaTermImpElim f' args')
 
 interpretIdent :: TreePlus -> WithEnv Ident
 interpretIdent tree =
