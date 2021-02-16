d1230c479da26b0f97aad6aa3b72925c302c6978
vekatze <vekatze@icloud.com>
2021-02-17 01:03:44 +0900

    (towards a meta calculus based on K-modal natural deduction)

--- src/Data/MetaTerm.hs
+++ src/Data/MetaTerm.hs
@@ -23,40 +23,29 @@ type SubstMetaTerm =
 
 substMetaTerm :: SubstMetaTerm -> MetaTermPlus -> MetaTermPlus
 substMetaTerm sub term =
-  substMetaTerm' 0 sub term
-
-substMetaTerm' :: Int -> SubstMetaTerm -> MetaTermPlus -> MetaTermPlus
-substMetaTerm' level sub term =
   case term of
-    (_, MetaTermVar x)
-      | level == 0 ->
-        case IntMap.lookup (asInt x) sub of
-          Nothing ->
-            term
-          Just e ->
-            e
-      | otherwise ->
-        term
-    (m, MetaTermImpIntro xs e)
-      | level == 0 -> do
-        let sub' = foldr IntMap.delete sub (map asInt xs)
-        let e' = substMetaTerm' level sub' e
-        (m, MetaTermImpIntro xs e')
-      | otherwise -> do
-        let e' = substMetaTerm' level sub e
-        (m, MetaTermImpIntro xs e')
+    (_, MetaTermVar x) ->
+      case IntMap.lookup (asInt x) sub of
+        Nothing ->
+          term
+        Just e ->
+          e
+    (m, MetaTermImpIntro xs e) -> do
+      let sub' = foldr IntMap.delete sub (map asInt xs)
+      let e' = substMetaTerm sub' e
+      (m, MetaTermImpIntro xs e')
     (m, MetaTermImpElim e es) -> do
-      let e' = substMetaTerm' level sub e
-      let es' = map (substMetaTerm' level sub) es
+      let e' = substMetaTerm sub e
+      let es' = map (substMetaTerm sub) es
       (m, MetaTermImpElim e' es')
     (m, MetaTermNecIntro e) -> do
-      let e' = substMetaTerm' (level - 1) sub e
+      let e' = substMetaTerm sub e
       (m, MetaTermNecIntro e')
     (m, MetaTermNecElim e) -> do
-      let e' = substMetaTerm' (level + 1) sub e
-      (m, MetaTermNecIntro e')
+      let e' = substMetaTerm sub e
+      (m, MetaTermNecElim e')
     (_, MetaTermLeaf _) ->
       term
     (m, MetaTermNode es) -> do
-      let es' = map (substMetaTerm' level sub) es
+      let es' = map (substMetaTerm sub) es
       (m, MetaTermNode es')
