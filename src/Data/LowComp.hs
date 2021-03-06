3c24131fbe53a382c74bdf5a31822e50ff76a062
vekatze <vekatze@icloud.com>
2021-03-06 22:36:38 +0900

    cleanup

--- src/Data/LowComp.hs
+++ src/Data/LowComp.hs
@@ -40,7 +40,6 @@ data LowOp
   | LowOpAlloc LowValue SizeInfo
   | LowOpFree LowValue SizeInfo Int -- (var, size-of-var, name-of-free)   (name-of-free is only for optimization)
   | LowOpPrimOp PrimOp [LowValue]
-  | LowOpMemCpy LowValue LowValue LowValue LowValue
   | LowOpSyscall
       Integer -- syscall number
       [LowValue] -- arguments
@@ -98,12 +97,6 @@ substLowOp sub llvmOp =
     LowOpFree d sizeInfo i -> do
       let d' = substLowValue sub d
       LowOpFree d' sizeInfo i
-    LowOpMemCpy dest src len isVol -> do
-      let dest' = substLowValue sub dest
-      let src' = substLowValue sub src
-      let len' = substLowValue sub len
-      let isVol' = substLowValue sub isVol
-      LowOpMemCpy dest' src' len' isVol'
     LowOpPrimOp op ds -> do
       let ds' = map (substLowValue sub) ds
       LowOpPrimOp op ds'
