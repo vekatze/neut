9907797e4d29e9866b80caa077df4f614885e71c
vekatze <vekatze@icloud.com>
2021-03-06 23:14:34 +0900

    cleanup

--- src/Data/Comp.hs
+++ src/Data/Comp.hs
@@ -42,6 +42,9 @@ type ValuePlus =
 type CompPlus =
   (Hint, Comp)
 
+type SubstValuePlus =
+  IntMap.IntMap ValuePlus
+
 asUpsilon :: ValuePlus -> Maybe Ident
 asUpsilon term =
   case term of
@@ -49,62 +52,3 @@ asUpsilon term =
       Just x
     _ ->
       Nothing
-
-type SubstValuePlus =
-  IntMap.IntMap ValuePlus
-
--- substValuePlus :: SubstValuePlus -> ValuePlus -> ValuePlus
--- substValuePlus sub term =
---   case term of
---     (m, ValueConst x) ->
---       (m, ValueConst x)
---     (m, ValueUpsilon s) ->
---       fromMaybe (m, ValueUpsilon s) (IntMap.lookup (asInt s) sub)
---     (m, ValueSigmaIntro vs) -> do
---       let vs' = map (substValuePlus sub) vs
---       (m, ValueSigmaIntro vs')
---     (m, ValueInt size l) ->
---       (m, ValueInt size l)
---     (m, ValueFloat size l) ->
---       (m, ValueFloat size l)
---     (m, ValueEnumIntro l) ->
---       (m, ValueEnumIntro l)
-
--- substCompPlus :: SubstValuePlus -> CompPlus -> CompPlus
--- substCompPlus sub term =
---   case term of
---     (m, CompPrimitive theta) -> do
---       let theta' = substPrimitive sub theta
---       (m, CompPrimitive theta')
---     (m, CompPiElimDownElim v ds) -> do
---       let v' = substValuePlus sub v
---       let ds' = map (substValuePlus sub) ds
---       (m, CompPiElimDownElim v' ds')
---     (m, CompSigmaElim xs v e) -> do
---       let v' = substValuePlus sub v
---       let sub' = foldr IntMap.delete sub (map asInt xs)
---       let e' = substCompPlus sub' e
---       (m, CompSigmaElim xs v' e')
---     (m, CompUpIntro v) -> do
---       let v' = substValuePlus sub v
---       (m, CompUpIntro v')
---     (m, CompUpElim x e1 e2) -> do
---       let e1' = substCompPlus sub e1
---       let sub' = IntMap.delete (asInt x) sub
---       let e2' = substCompPlus sub' e2
---       (m, CompUpElim x e1' e2')
---     (m, CompEnumElim v branchList) -> do
---       let v' = substValuePlus sub v
---       let (cs, es) = unzip branchList
---       let es' = map (substCompPlus sub) es
---       (m, CompEnumElim v' (zip cs es'))
-
--- substPrimitive :: SubstValuePlus -> Primitive -> Primitive
--- substPrimitive sub c =
---   case c of
---     PrimitivePrimOp op vs -> do
---       let vs' = map (substValuePlus sub) vs
---       PrimitivePrimOp op vs'
---     PrimitiveDerangement expKind ds -> do
---       let ds' = map (substValuePlus sub) ds
---       PrimitiveDerangement expKind ds'
