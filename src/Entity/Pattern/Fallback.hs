760e73062e2ca12573249fc1e633c5417002ff7e
vekatze <vekatze@icloud.com>
2022-12-02 20:39:22 +0900

    parametrize Fallback/Specialize with NominalEnv

--- src/Entity/Pattern/Fallback.hs
+++ src/Entity/Pattern/Fallback.hs
@@ -5,6 +5,7 @@ import qualified Context.Throw as Throw
 import Control.Comonad.Cofree
 import qualified Data.Vector as V
 import Entity.Ident
+import Entity.NominalEnv
 import Entity.Pattern
 import qualified Entity.WeakTerm as WT
 
@@ -13,25 +14,27 @@ class (Gensym.Context m, Throw.Context m) => Context m
 -- `cursor` is the variable `x` in `match x, y, z with (...) end`.
 getFallbackMatrix ::
   Context m =>
+  NominalEnv ->
   Ident ->
   PatternMatrix ([Ident], WT.WeakTerm) ->
   m (PatternMatrix ([Ident], WT.WeakTerm))
-getFallbackMatrix cursor mat = do
-  mapMaybeRowM (fallbackRow cursor) mat
+getFallbackMatrix nenv cursor mat = do
+  mapMaybeRowM (fallbackRow nenv cursor) mat
 
 fallbackRow ::
   Context m =>
+  NominalEnv ->
   Ident ->
   PatternRow ([Ident], WT.WeakTerm) ->
   m (Maybe (PatternRow ([Ident], WT.WeakTerm)))
-fallbackRow cursor (patternVector, (freedVars, body)) =
+fallbackRow nenv cursor (patternVector, (freedVars, body)) =
   case V.uncons patternVector of
     Nothing ->
-      Throw.raiseCritical' "specialization against the empty pattern matrix shouldn't happen"
-    Just ((m, WildcardVar), _) ->
-      Throw.raiseCritical m "specialization against a wildcard shouldn't happen"
+      Throw.raiseCritical' "defaulting against the empty pattern matrix shouldn't happen"
+    Just ((_, WildcardVar), rest) ->
+      return $ Just (rest, (freedVars, body))
     Just ((m, Var x), rest) -> do
-      h <- Gensym.newAster m []
+      h <- Gensym.newAster m (asHoleArgs nenv)
       let body' = m :< WT.Let (m, x, h) (m :< WT.Var cursor) body
       return $ Just (rest, (freedVars, body'))
     Just ((_, Cons {}), _) ->
--- src/Entity/Pattern/Specialize.hs
+++ src/Entity/Pattern/Specialize.hs
@@ -7,6 +7,7 @@ import qualified Data.Vector as V
 import qualified Entity.Arity as A
 import qualified Entity.DefiniteDescription as DD
 import Entity.Ident
+import Entity.NominalEnv
 import Entity.Pattern
 import qualified Entity.WeakTerm as WT
 
@@ -15,28 +16,31 @@ class (Gensym.Context m, Throw.Context m) => Context m
 -- `cursor` is the variable `x` in `match x, y, z with (...) end`.
 specialize ::
   Context m =>
+  NominalEnv ->
   Ident ->
   (DD.DefiniteDescription, A.Arity) ->
   PatternMatrix ([Ident], WT.WeakTerm) ->
   m (PatternMatrix ([Ident], WT.WeakTerm))
-specialize cursor cons mat = do
-  mapMaybeRowM (specializeRow cursor cons) mat
+specialize nenv cursor cons mat = do
+  mapMaybeRowM (specializeRow nenv cursor cons) mat
 
 specializeRow ::
   Context m =>
+  NominalEnv ->
   Ident ->
   (DD.DefiniteDescription, A.Arity) ->
   PatternRow ([Ident], WT.WeakTerm) ->
   m (Maybe (PatternRow ([Ident], WT.WeakTerm)))
-specializeRow cursor (dd, arity) (patternVector, (freedVars, body)) =
+specializeRow nenv cursor (dd, arity) (patternVector, (freedVars, body)) =
   case V.uncons patternVector of
     Nothing ->
       Throw.raiseCritical' "specialization against the empty pattern matrix shouldn't happen"
-    Just ((m, WildcardVar), _) ->
-      Throw.raiseCritical m "specialization against a wildcard shouldn't happen"
+    Just ((m, WildcardVar), rest) -> do
+      let wildcards = V.fromList $ replicate (fromInteger $ A.reify arity) (m, WildcardVar)
+      return $ Just (V.concat [wildcards, rest], (freedVars, body))
     Just ((m, Var x), rest) -> do
       let wildcards = V.fromList $ replicate (fromInteger $ A.reify arity) (m, WildcardVar)
-      h <- Gensym.newAster m []
+      h <- Gensym.newAster m (asHoleArgs nenv)
       let body' = m :< WT.Let (m, x, h) (m :< WT.Var cursor) body
       -- let body' = m :< WT.Let (m, cursor, h) (m :< WT.Var x) body
       return $ Just (V.concat [wildcards, rest], (freedVars, body'))
