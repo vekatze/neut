cadb775435d153d57a9a91b3e2b201a09da5e6d3
vekatze <vekatze@icloud.com>
2022-12-07 08:26:30 +0900

    add module-based tests

---
+++ test/data/test-all.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+for i in $(find . -d 1 -type d | sort); do
+    cd $i
+    echo $(basename $i)
+    neut clean
+    neut build
+    /opt/homebrew/opt/llvm/bin/clang -fsanitize=address -Wno-override-module ./target/artifact/*.ll -o ./target/a.out
+    ASAN_OPTIONS=detect_leaks=1 LSAN_OPTIONS=suppressions=../lsan.supp ./target/a.out
+    cd ..
+done
