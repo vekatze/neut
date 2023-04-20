64263c007c8489e7adcbba7f084df1b3f1665063
vekatze <vekatze@icloud.com>
2023-04-21 08:08:05 +0900

    update tests

---
+++ test/pfds/update-core.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# e.g. TARGET_PLATFORM=aarch64-darwin COMPILER_VERSION=0.2.0.0 ./test-aarch64-darwin.sh
+OLD_VERSION=0.2.0.2
+NEW_VERSION=0.2.0.3
+
+for i in $(find . -d 1 -type d | sort); do
+  cd $i
+  rg $OLD_VERSION > /dev/null
+  if [[ $? == 0 ]]; then
+    echo $(basename $i)
+    neut add core https://github.com/vekatze/neut-core/raw/main/release/${NEW_VERSION}.tar.zst
+    echo ""
+  fi
+  cd ..
+done
