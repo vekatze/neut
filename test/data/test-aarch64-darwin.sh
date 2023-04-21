cfee1ab091774bfd9c518e54e2be8978437440a5
vekatze <vekatze@icloud.com>
2023-04-21 14:06:00 +0900

    add missing env vars

--- test/data/test-aarch64-darwin.sh
+++ test/data/test-aarch64-darwin.sh
@@ -5,8 +5,8 @@
 for i in $(find . -d 1 -type d | sort); do
   cd $i
   echo $(basename $i)
-  neut clean
-  NEUT_TARGET_ARCH=aarch64 NEUT_CLANG=/opt/homebrew/opt/llvm/bin/clang neut build --clang-option "-fsanitize=address"
+  NEUT_TARGET_ARCH=arm64 neut clean
+  NEUT_TARGET_ARCH=arm64 NEUT_CLANG=/opt/homebrew/opt/llvm/bin/clang neut build --clang-option "-fsanitize=address"
   # https://stackoverflow.com/questions/64126942/malloc-nano-zone-abandoned-due-to-inability-to-preallocate-reserved-vm-space
   MallocNanoZone=0 ASAN_OPTIONS=detect_leaks=1 LSAN_OPTIONS=suppressions=../lsan.supp ./target/$TARGET_PLATFORM/$COMPILER_VERSION/executable/$(basename $i)
   cd ..
