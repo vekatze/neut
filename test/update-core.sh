542b0a9ae7605dce48f7b1283d7ae1224d41f2b3
vekatze <vekatze@icloud.com>
2023-05-17 13:20:54 +0900

    add core library dependencies to tests

--- test/update-core.sh
+++ test/update-core.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-# e.g. ./update-core.sh ./data
+# e.g. ./update-core.sh ./pfds
 
-OLD_VERSION=0.2.0.10
+# OLD_VERSION=0.2.0.10
 NEW_VERSION=0.2.0.11
 
 TARGET_DIR=$(cd "$1"; pwd)
@@ -10,11 +10,10 @@ cd $TARGET_DIR
 
 for i in $(find . -d 1 -type d | sort); do
   cd $i
-  rg $OLD_VERSION > /dev/null
-  if [[ $? == 0 ]]; then
-    echo $(basename $i)
-    neut add core https://github.com/vekatze/neut-core/raw/main/release/${NEW_VERSION}.tar.zst
-    echo ""
-  fi
+  # rg $OLD_VERSION > /dev/null
+  # if [[ $? == 0 ]]; then
+  echo $(basename $i)
+  neut add core https://github.com/vekatze/neut-core/raw/main/release/${NEW_VERSION}.tar.zst
+  # fi
   cd ..
 done
