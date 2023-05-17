cc8e425b249c19efa40eeba27f423a6bae787095
vekatze <vekatze@icloud.com>
2023-05-17 12:50:24 +0900

    make tests to exit with 0 by default

--- test/test-darwin.sh
+++ test/test-darwin.sh
@@ -9,6 +9,8 @@ LSAN_FILE=$SCRIPT_DIR/lsan.supp
 TARGET_DIR=$(cd "$1"; pwd)
 cd $TARGET_DIR
 
+exit_code=0
+
 for i in $(find . -d 1 -type d | sort); do
   cd $i
   echo $(basename $i)
@@ -16,5 +18,11 @@ for i in $(find . -d 1 -type d | sort); do
   NEUT_TARGET_ARCH=$TARGET_ARCH NEUT_CLANG=$CLANG_PATH $NEUT build --clang-option "-fsanitize=address"
   # https://stackoverflow.com/questions/64126942
   MallocNanoZone=0 ASAN_OPTIONS=detect_leaks=1 LSAN_OPTIONS=suppressions=$LSAN_FILE ./.build/$TARGET_ARCH-darwin/compiler-$COMPILER_VERSION/build-option-cLHs7km0Z68odNMcuhrD2pgQSCxXZuyQVSxpwrPsbVY=/executable/$(basename $i)
+  last_exit_code=$?
+  if [ $last_exit_code -eq 1 ]; then
+    exit_code=$last_exit_code
+  fi
   cd ..
 done
+
+exit $exit_code
--- test/test-linux.sh
+++ test/test-linux.sh
@@ -3,11 +3,19 @@
 TARGET_DIR=$(cd "$1"; pwd)
 cd $TARGET_DIR
 
+exit_code=0
+
 for i in $(find . -maxdepth 1 -mindepth 1 -type d | sort); do
   cd $i
   echo $(basename $i)
   NEUT_TARGET_ARCH=$TARGET_ARCH $NEUT clean
   NEUT_TARGET_ARCH=$TARGET_ARCH $NEUT build --clang-option "-fsanitize=address"
   ASAN_OPTIONS=detect_leaks=1 ./.build/$TARGET_ARCH-linux/compiler-$COMPILER_VERSION/build-option-cLHs7km0Z68odNMcuhrD2pgQSCxXZuyQVSxpwrPsbVY=/executable/$(basename $i)
+  last_exit_code=$?
+  if [ $last_exit_code -eq 1 ]; then
+    exit_code=$last_exit_code
+  fi
   cd ..
 done
+
+exit $exit_code
