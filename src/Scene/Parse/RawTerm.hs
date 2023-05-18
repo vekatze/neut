ee7141254b965d39dc3bbed4fcf7767e123e3694
vekatze <vekatze@icloud.com>
2023-05-18 19:17:44 +0900

    cleanup

--- src/Scene/Parse/RawTerm.hs
+++ src/Scene/Parse/RawTerm.hs
@@ -671,14 +671,6 @@ rawTermNoema = do
   t <- rawTermBasic
   return $ m :< RT.Noema t
 
--- rawTermUnit :: Parser RT.RawTerm
--- rawTermUnit = do
---   m <- getCurrentHint
---   keyword "unit"
---   unitVar <- lift $ Throw.liftEither $ DD.getLocatorPair m coreThreadUnitInner
---   t <- betweenParen rawTerm
---   return $ m :< RT.Unit unitVar t
-
 rawTermFlow :: Parser RT.RawTerm
 rawTermFlow = do
   m <- getCurrentHint
