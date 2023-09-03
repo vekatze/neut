ae131730b62ae061de9e4ab8c02dbc6567aa6f04
vekatze <vekatze@icloud.com>
2023-09-03 16:59:10 +0900

    don't forget to initialize the compiler

--- src/Act/Check.hs
+++ src/Act/Check.hs
@@ -5,9 +5,12 @@ import Context.Remark qualified as Remark
 import Context.Throw qualified as Throw
 import Entity.Config.Check
 import Scene.Check qualified as Check
+import Scene.Initialize qualified as Initialize
 
 check :: Config -> App ()
 check cfg = do
   let runner = if shouldInsertPadding cfg then id else Throw.run'
-  logs <- runner $ Check.check (mFilePathString cfg)
+  logs <- runner $ do
+    Initialize.initializeCompiler (remarkCfg cfg) Nothing
+    Check.check (mFilePathString cfg)
   Remark.printErrorList logs
