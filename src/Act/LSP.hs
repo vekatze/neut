56e6853fad0b132da1b5cbc8c7e870dcd6c89d88
vekatze <vekatze@icloud.com>
2023-11-01 20:12:49 +0900

    don't reuse the app env when running the LSP server

--- src/Act/LSP.hs
+++ src/Act/LSP.hs
@@ -3,10 +3,8 @@ module Act.LSP (lsp) where
 import Context.App
 import Control.Monad
 import Entity.Config.LSP
-import Scene.Initialize qualified as Initialize
 import Scene.LSP qualified as L
 
 lsp :: Config -> App ()
 lsp cfg = do
-  Initialize.initializeCompiler (remarkCfg cfg) Nothing
-  void L.lsp
+  void $ L.lsp $ remarkCfg cfg
