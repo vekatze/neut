ef3cd9441a1ecb3be1f833a49f72cfd9fbacdf55
vekatze <vekatze@icloud.com>
2023-11-19 15:36:42 +0900

    update the initial project

--- src/Scene/New.hs
+++ src/Scene/New.hs
@@ -70,4 +70,4 @@ createMainFile = do
   newModule <- Module.getMainModule
   Path.ensureDir $ getSourceDir newModule
   forM_ (getTargetPathList newModule) $ \mainFilePath -> do
-    Path.writeText mainFilePath "define main(): unit {\n  print(\"Hello, world!\\n\")\n}\n"
+    Path.writeText mainFilePath "import {\n- core\n}\n\ndefine main(): unit {\n  print(\"Hello, world!\\n\")\n}\n"
