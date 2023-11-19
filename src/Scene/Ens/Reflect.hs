0b0dfb51cb828dcd56fdf44dbfcae85a510505ec
vekatze <vekatze@icloud.com>
2023-11-19 14:44:46 +0900

    report duplicated keys as errors

--- src/Scene/Ens/Reflect.hs
+++ src/Scene/Ens/Reflect.hs
@@ -2,10 +2,14 @@ module Scene.Ens.Reflect (fromFilePath) where
 
 import Context.App
 import Context.Parse
+import Context.Throw qualified as Throw
 import Control.Comonad.Cofree
+import Control.Monad.Trans
 import Data.HashMap.Strict qualified as M
+import Data.Set qualified as S
 import Data.Text qualified as T
 import Entity.Ens qualified as E
+import Entity.Hint
 import Path
 import Scene.Parse.Core
 import Text.Megaparsec hiding (parse)
@@ -15,11 +19,12 @@ fromFilePath path = do
   fileContent <- readSourceFile path
   run parseEns path fileContent
 
-parseKeyValuePair :: Parser (T.Text, E.Ens)
+parseKeyValuePair :: Parser ((Hint, T.Text), E.Ens)
 parseKeyValuePair = do
+  m <- getCurrentHint
   k <- symbol
   v <- parseEns
-  return (k, v)
+  return ((m, k), v)
 
 parseEns :: Parser E.Ens
 parseEns = do
@@ -38,7 +43,9 @@ parseEns = do
 parseDictionary :: Parser (M.HashMap T.Text E.Ens)
 parseDictionary = do
   delimiter "{"
-  M.fromList <$> manyTill parseKeyValuePair (delimiter "}")
+  (mks, vs) <- unzip <$> manyTill parseKeyValuePair (delimiter "}")
+  lift $ ensureKeyLinearity mks S.empty
+  return $ M.fromList $ zip (map snd mks) vs
 
 parseList :: Parser [E.Ens]
 parseList = do
@@ -46,3 +53,14 @@ parseList = do
   vs <- many parseEns
   delimiter "]"
   return vs
+
+ensureKeyLinearity :: [(Hint, T.Text)] -> S.Set T.Text -> App ()
+ensureKeyLinearity mks foundKeySet =
+  case mks of
+    [] ->
+      return ()
+    (m, k) : rest
+      | S.member k foundKeySet ->
+          Throw.raiseError m $ "found a duplicated key: `" <> k <> "`"
+      | otherwise ->
+          ensureKeyLinearity rest (S.insert k foundKeySet)
