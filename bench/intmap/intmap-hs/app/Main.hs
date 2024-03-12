03c2bc11112c1e0094e8c71fa8dc8f0a327351f4
vekatze <vekatze@icloud.com>
2024-03-12 09:42:55 +0900

    intmap-hs

---
+++ bench/intmap/intmap-hs/app/Main.hs
@@ -0,0 +1,39 @@
+module Main (main) where
+
+import Data.IntMap.Strict qualified as M
+import System.Environment (getArgs)
+import System.Random
+import Text.Read
+
+makeBigDict :: Int -> M.IntMap Int -> IO (M.IntMap Int)
+makeBigDict len acc =
+  if len == 0
+    then return acc
+    else do
+      k <- randomRIO (0, 1000000)
+      v <- randomRIO (0, 1000000)
+      makeBigDict (len - 1) $ M.insert k v acc
+
+randomSum :: Int -> Int -> M.IntMap Int -> IO Int
+randomSum count acc bigDict = do
+  if count <= 0
+    then return acc
+    else do
+      k <- randomRIO (0, 1000000)
+      case M.lookup k bigDict of
+        Nothing ->
+          randomSum (count - 1) acc bigDict
+        Just val ->
+          randomSum (count - 1) (acc + val) bigDict
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [sizeStr]
+      | Just size <- readMaybe sizeStr -> do
+          bigDict <- makeBigDict size M.empty
+          val <- randomSum size 0 bigDict
+          print val
+    _ ->
+      putStrLn "usage: intmap-hs-exe SIZE"
