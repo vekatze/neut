f99868b1d0d0856cee02977f59ce9e6f35994ee5
vekatze <vekatze@icloud.com>
2024-03-12 20:35:14 +0900

    yo

--- bench/bench.sh
+++ bench/bench.sh
@@ -2,11 +2,11 @@
 
 SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
 
-for target_dir in $(find $SCRIPT_DIR -type d -d 1); do
+for target_dir in $(find $SCRIPT_DIR/action -type d -d 1 | sort); do
   cd $target_dir
   size=$(cat ./test-size.txt)
-  for executable in $(find $target_dir/bin -type f); do
+  for executable in $(find $target_dir/bin -type f | sort); do
     echo ${executable:t}
-    hyperfine -r 1 -P SIZE $((size/10)) $size -D $((size/10)) "${executable} {SIZE}" --export-json ../result/${executable:t}.json
+    hyperfine -r 1 -P SIZE $((size/10)) $size -D $((size/10)) "${executable} {SIZE}" --export-json ../../result/${executable:t}.json
   done
 done
