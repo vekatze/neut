181b23e20980ef0f42958f186862c526f3afae44
vekatze <vekatze@icloud.com>
2024-05-30 10:58:52 +0900

    add static files to archives

--- src/Scene/Collect.hs
+++ src/Scene/Collect.hs
@@ -7,6 +7,7 @@ where
 import Context.App
 import Context.Module qualified as Module
 import Context.Throw qualified as Throw
+import Data.HashMap.Strict qualified as Map
 import Data.Maybe
 import Data.Text qualified as T
 import Entity.Module
@@ -29,4 +30,5 @@ collectModuleFiles baseModule = do
   let relModuleSourceDir = Left $ moduleSourceDir baseModule
   let foreignContents = input $ moduleForeign baseModule
   let extraContents = moduleExtraContents baseModule
-  (moduleRootDir, relModuleSourceDir : foreignContents ++ extraContents)
+  let staticContents = map (\(_, path) -> Right path) $ Map.toList $ moduleStaticFiles baseModule
+  (moduleRootDir, relModuleSourceDir : foreignContents ++ staticContents ++ extraContents)
