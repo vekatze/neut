f844f4b44fb351f8726cec10e9ba3358710eb5f5
vekatze <vekatze@icloud.com>
2024-06-15 17:12:01 +0900

    fix: use the new syntax

--- bench/script/bench-linux.sh
+++ bench/script/bench-linux.sh
@@ -5,7 +5,8 @@ SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
 for target_dir in $(find "$SCRIPT_DIR/../action" -maxdepth 1 -mindepth 1 -type d | sort); do
   cd $target_dir
   stack install --local-bin-path ./bin
-  $NEUT build --install ./bin
+  $NEUT clean
+  $NEUT build "$(basename ${target_dir})-nt" --install ./bin
   size=$(cat ./test-size.txt)
   step=10
   for executable in $(find $target_dir/bin -type f | sort); do
