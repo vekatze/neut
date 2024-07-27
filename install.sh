a4b0c2db2deafe503c8cb7413f3695db0e5c67d5
vekatze <vekatze@icloud.com>
2024-07-27 09:40:02 +0900

    update the core library

--- install.sh
+++ install.sh
@@ -142,8 +142,8 @@ printf $BLUE "note: "
 echo "please restart your shell after adding the following environment variables to your shell config:"
 
 echo ""
-echo "export NEUT_CORE_MODULE_URL=\"https://github.com/vekatze/neut-core/raw/main/archive/0-50-19.tar.zst\""
-echo "export NEUT_CORE_MODULE_DIGEST=\"vNwqPlzlD08E9WZyI3I7nsu_eYYSSeVUBHv24ljHf2c\""
+echo "export NEUT_CORE_MODULE_URL=\"https://github.com/vekatze/neut-core/raw/main/archive/0-50-20.tar.zst\""
+echo "export NEUT_CORE_MODULE_DIGEST=\"vUEWL-sx5J-X_jZFhLapB28peC64IG_H-st9S6Jk_mw\""
 
 if command -v apt-get >/dev/null 2>&1; then
   echo "export NEUT_CLANG=$CLANG"
