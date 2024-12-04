78560ca787efecf35f14f52a5b43c66ebd99cc42
vekatze <vekatze@icloud.com>
2024-12-04 15:03:40 +0900

    update the core library (0-50-42)

--- book/src/manual-installation.md
+++ book/src/manual-installation.md
@@ -11,8 +11,8 @@ Add the below to your `.bashrc`, `.zshrc`, etc.
 
 ```sh
 # this sets the core module (or "prelude") that is used in `neut create`
-export NEUT_CORE_MODULE_URL="https://github.com/vekatze/neut-core/raw/main/archive/0-50-41.tar.zst"
-export NEUT_CORE_MODULE_DIGEST="dzZMB865Wo1e4zJrBxQlLqpb-D0_H_AcLiFOmDwWEcE"
+export NEUT_CORE_MODULE_URL="https://github.com/vekatze/neut-core/raw/main/archive/0-50-42.tar.zst"
+export NEUT_CORE_MODULE_DIGEST="to0_J79vnQD63lhOCK6exPHjVayNBL6GUgLOC256aHM"
 ```
 
 Then, get the compiler:
--- install.sh
+++ install.sh
@@ -142,8 +142,8 @@ printf $BLUE "note: "
 echo "please restart your shell after adding the following environment variables to your shell config:"
 
 echo ""
-echo "export NEUT_CORE_MODULE_URL=\"https://github.com/vekatze/neut-core/raw/main/archive/0-50-41.tar.zst\""
-echo "export NEUT_CORE_MODULE_DIGEST=\"dzZMB865Wo1e4zJrBxQlLqpb-D0_H_AcLiFOmDwWEcE\""
+echo "export NEUT_CORE_MODULE_URL=\"https://github.com/vekatze/neut-core/raw/main/archive/0-50-42.tar.zst\""
+echo "export NEUT_CORE_MODULE_DIGEST=\"to0_J79vnQD63lhOCK6exPHjVayNBL6GUgLOC256aHM\""
 
 if command -v apt-get >/dev/null 2>&1; then
   echo "export NEUT_CLANG=$CLANG"
