f45b263fa7c4bb2aa19062f73c3c3ed88e85ecd6
vekatze <vekatze@icloud.com>
2025-04-22 06:46:39 +0900

    add: Move.Context.EIO.{raiseError,raiseError'}

--- src/Move/Context/EIO.hs
+++ src/Move/Context/EIO.hs
@@ -1,10 +1,18 @@
-module Move.Context.EIO (EIO, toApp) where
+module Move.Context.EIO
+  ( EIO,
+    toApp,
+    raiseError,
+    raiseError',
+  )
+where
 
-import Control.Monad.Except (ExceptT, runExceptT)
+import Control.Monad.Except (ExceptT, MonadError (throwError), runExceptT)
 import Control.Monad.IO.Class (MonadIO (liftIO))
+import Data.Text qualified as T
 import Move.Context.App (App)
 import Move.Context.Throw (throw)
 import Rule.Error qualified as E
+import Rule.Hint (Hint)
 
 type EIO = ExceptT E.Error IO
 
@@ -17,3 +25,11 @@ toApp comp = do
       throw err
     Right result ->
       return result
+
+raiseError :: Hint -> T.Text -> EIO a
+raiseError m t =
+  throwError $ E.newError m t
+
+raiseError' :: T.Text -> EIO a
+raiseError' t =
+  throwError $ E.newError' t
