2b6602f18fdb4a8e116c09112fa1edcf28775743
vekatze <vekatze@icloud.com>
2025-04-24 12:41:09 +0900

    un-app: incrementStep

--- src/Move/Scene/Term/Inline.hs
+++ src/Move/Scene/Term/Inline.hs
@@ -1,9 +1,5 @@
 module Move.Scene.Term.Inline (inline) where
 
-import Move.Context.App
-import Move.Context.Definition qualified as Definition
-import Move.Context.Env qualified as Env
-import Move.Context.Throw qualified as Throw
 import Control.Comonad.Cofree
 import Control.Monad
 import Control.Monad.IO.Class
@@ -13,6 +9,12 @@ import Data.IORef
 import Data.IntMap qualified as IntMap
 import Data.Maybe (fromMaybe)
 import Data.Text qualified as T
+import Move.Context.App
+import Move.Context.Definition qualified as Definition
+import Move.Context.Env qualified as Env
+import Move.Context.Throw qualified as Throw
+import Move.Scene.Term.Refresh (refresh)
+import Move.Scene.Term.Subst qualified as Subst
 import Rule.Attr.DataIntro qualified as AttrDI
 import Rule.Attr.Lam qualified as AttrL
 import Rule.Binder
@@ -29,83 +31,81 @@ import Rule.Module (moduleInlineLimit)
 import Rule.Opacity qualified as O
 import Rule.Source (sourceModule)
 import Rule.Term qualified as TM
-import Move.Scene.Term.Refresh (refresh)
-import Move.Scene.Term.Subst qualified as Subst
 
-data Axis = Axis
+data Handle = Handle
   { dmap :: Map.HashMap DD.DefiniteDescription ([BinderF TM.Term], TM.Term),
     inlineLimit :: Int,
     currentStepRef :: IORef Int,
     location :: Hint
   }
 
-newAxis :: Hint -> App Axis
-newAxis m = do
+new :: Hint -> App Handle
+new m = do
   source <- Env.getCurrentSource
   dmap <- Definition.get
   let limit = fromMaybe defaultInlineLimit $ moduleInlineLimit (sourceModule source)
   currentStepRef <- liftIO $ newIORef 0
   return
-    Axis
+    Handle
       { dmap = dmap,
         inlineLimit = limit,
         currentStepRef = currentStepRef,
         location = m
       }
 
-incrementStep :: Axis -> App ()
-incrementStep axis = do
-  let Axis {currentStepRef} = axis
-  liftIO $ modifyIORef' currentStepRef (+ 1)
+incrementStep :: Handle -> IO ()
+incrementStep h = do
+  let Handle {currentStepRef} = h
+  modifyIORef' currentStepRef (+ 1)
 
-detectPossibleInfiniteLoop :: Axis -> App ()
-detectPossibleInfiniteLoop axis = do
-  let Axis {inlineLimit, currentStepRef, location} = axis
+detectPossibleInfiniteLoop :: Handle -> App ()
+detectPossibleInfiniteLoop h = do
+  let Handle {inlineLimit, currentStepRef, location} = h
   currentStep <- liftIO $ readIORef currentStepRef
   when (inlineLimit < currentStep) $ do
     Throw.raiseError location $ "Exceeded max recursion depth of " <> T.pack (show inlineLimit)
 
 inline :: Hint -> TM.Term -> App TM.Term
 inline m e = do
-  axis <- newAxis m
-  inline' axis e
+  h <- new m
+  inline' h e
 
-inline' :: Axis -> TM.Term -> App TM.Term
-inline' axis term = do
-  detectPossibleInfiniteLoop axis
-  incrementStep axis
+inline' :: Handle -> TM.Term -> App TM.Term
+inline' h term = do
+  detectPossibleInfiniteLoop h
+  liftIO $ incrementStep h
   case term of
     m :< TM.Pi impArgs expArgs cod -> do
       impArgs' <- do
         let (ms, xs, ts) = unzip3 impArgs
-        ts' <- mapM (inline' axis) ts
+        ts' <- mapM (inline' h) ts
         return $ zip3 ms xs ts'
       expArgs' <- do
         let (ms, xs, ts) = unzip3 expArgs
-        ts' <- mapM (inline' axis) ts
+        ts' <- mapM (inline' h) ts
         return $ zip3 ms xs ts'
-      cod' <- inline' axis cod
+      cod' <- inline' h cod
       return (m :< TM.Pi impArgs' expArgs' cod')
     m :< TM.PiIntro attr@(AttrL.Attr {lamKind}) impArgs expArgs e -> do
       impArgs' <- do
         let (ms, xs, ts) = unzip3 impArgs
-        ts' <- mapM (inline' axis) ts
+        ts' <- mapM (inline' h) ts
         return $ zip3 ms xs ts'
       expArgs' <- do
         let (ms, xs, ts) = unzip3 expArgs
-        ts' <- mapM (inline' axis) ts
+        ts' <- mapM (inline' h) ts
         return $ zip3 ms xs ts'
-      e' <- inline' axis e
+      e' <- inline' h e
       case lamKind of
         LK.Fix (mx, x, t) -> do
-          t' <- inline' axis t
+          t' <- inline' h t
           return (m :< TM.PiIntro (attr {AttrL.lamKind = LK.Fix (mx, x, t')}) impArgs' expArgs' e')
         _ ->
           return (m :< TM.PiIntro attr impArgs' expArgs' e')
     m :< TM.PiElim e es -> do
-      e' <- inline' axis e
-      es' <- mapM (inline' axis) es
-      let Axis {dmap} = axis
+      e' <- inline' h e
+      es' <- mapM (inline' h) es
+      let Handle {dmap} = h
       case e' of
         (_ :< TM.PiIntro (AttrL.Attr {lamKind = LK.Normal _}) impArgs expArgs body)
           | xts <- impArgs ++ expArgs,
@@ -115,10 +115,10 @@ inline' axis term = do
                   let (_, xs, _) = unzip3 xts
                   let sub = IntMap.fromList $ zip (map Ident.toInt xs) (map Right es')
                   _ :< body' <- Subst.subst sub body
-                  inline' axis $ m :< body'
+                  inline' h $ m :< body'
                 else do
                   (xts', _ :< body') <- Subst.subst' IntMap.empty xts body
-                  inline' axis $ bind (zip xts' es') (m :< body')
+                  inline' h $ bind (zip xts' es') (m :< body')
         (_ :< TM.VarGlobal _ dd)
           | Just (xts, body) <- Map.lookup dd dmap -> do
               if all TM.isValue es'
@@ -127,34 +127,34 @@ inline' axis term = do
                   let sub = IntMap.fromList $ zip (map Ident.toInt xs) (map Right es')
                   _ :< body' <- Subst.subst sub body
                   body'' <- refresh $ m :< body'
-                  inline' axis body''
+                  inline' h body''
                 else do
                   (xts', _ :< body') <- Subst.subst' IntMap.empty xts body
                   body'' <- refresh $ m :< body'
-                  inline' axis $ bind (zip xts' es') body''
+                  inline' h $ bind (zip xts' es') body''
         _ ->
           return (m :< TM.PiElim e' es')
     m :< TM.Data attr name es -> do
-      es' <- mapM (inline' axis) es
+      es' <- mapM (inline' h) es
       return $ m :< TM.Data attr name es'
     m :< TM.DataIntro attr consName dataArgs consArgs -> do
-      dataArgs' <- mapM (inline' axis) dataArgs
-      consArgs' <- mapM (inline' axis) consArgs
+      dataArgs' <- mapM (inline' h) dataArgs
+      consArgs' <- mapM (inline' h) consArgs
       return $ m :< TM.DataIntro attr consName dataArgs' consArgs'
     m :< TM.DataElim isNoetic oets decisionTree -> do
       let (os, es, ts) = unzip3 oets
-      es' <- mapM (inline' axis) es
-      ts' <- mapM (inline' axis) ts
+      es' <- mapM (inline' h) es
+      ts' <- mapM (inline' h) ts
       let oets' = zip3 os es' ts'
       if isNoetic
         then do
-          decisionTree' <- inlineDecisionTree axis decisionTree
+          decisionTree' <- inlineDecisionTree h decisionTree
           return $ m :< TM.DataElim isNoetic oets' decisionTree'
         else do
           case decisionTree of
             DT.Leaf _ letSeq e -> do
               let sub = IntMap.fromList $ zip (map Ident.toInt os) (map Right es')
-              Subst.subst sub (TM.fromLetSeq letSeq e) >>= inline' axis
+              Subst.subst sub (TM.fromLetSeq letSeq e) >>= inline' h
             DT.Unreachable ->
               return $ m :< TM.DataElim isNoetic oets' DT.Unreachable
             DT.Switch (cursor, _) (fallbackTree, caseList) -> do
@@ -164,87 +164,87 @@ inline' axis term = do
                   let newCursorList = zipWith (\(o, t) arg -> (o, arg, t)) newBaseCursorList consArgs
                   let sub = IntMap.singleton (Ident.toInt cursor) (Right e)
                   dataElim' <- Subst.subst sub $ m :< TM.DataElim isNoetic (oets'' ++ newCursorList) cont
-                  inline' axis dataElim'
+                  inline' h dataElim'
                 _ -> do
-                  decisionTree' <- inlineDecisionTree axis decisionTree
+                  decisionTree' <- inlineDecisionTree h decisionTree
                   return $ m :< TM.DataElim isNoetic oets' decisionTree'
     m :< TM.Box t -> do
-      t' <- inline' axis t
+      t' <- inline' h t
       return $ m :< TM.Box t'
     m :< TM.BoxIntro letSeq e -> do
       let (xts, es) = unzip letSeq
-      xts' <- mapM (inlineBinder axis) xts
-      es' <- mapM (inline' axis) es
-      e' <- inline' axis e
+      xts' <- mapM (inlineBinder h) xts
+      es' <- mapM (inline' h) es
+      e' <- inline' h e
       return $ m :< TM.BoxIntro (zip xts' es') e'
     m :< TM.Let opacity (mx, x, t) e1 e2 -> do
-      e1' <- inline' axis e1
+      e1' <- inline' h e1
       case opacity of
         O.Clear
           | TM.isValue e1' -> do
               let sub = IntMap.singleton (Ident.toInt x) (Right e1')
-              Subst.subst sub e2 >>= inline' axis
+              Subst.subst sub e2 >>= inline' h
         _ -> do
-          t' <- inline' axis t
-          e2' <- inline' axis e2
+          t' <- inline' h t
+          e2' <- inline' h e2
           return $ m :< TM.Let opacity (mx, x, t') e1' e2'
     (m :< TM.Magic magic) -> do
       case magic of
         M.Cast _ _ e ->
-          inline' axis e
+          inline' h e
         _ -> do
-          magic' <- traverse (inline' axis) magic
+          magic' <- traverse (inline' h) magic
           return (m :< TM.Magic magic')
     _ ->
       return term
 
-inlineBinder :: Axis -> BinderF TM.Term -> App (BinderF TM.Term)
-inlineBinder axis (m, x, t) = do
-  t' <- inline' axis t
+inlineBinder :: Handle -> BinderF TM.Term -> App (BinderF TM.Term)
+inlineBinder h (m, x, t) = do
+  t' <- inline' h t
   return (m, x, t')
 
 inlineDecisionTree ::
-  Axis ->
+  Handle ->
   DT.DecisionTree TM.Term ->
   App (DT.DecisionTree TM.Term)
-inlineDecisionTree axis tree =
+inlineDecisionTree h tree =
   case tree of
     DT.Leaf xs letSeq e -> do
-      letSeq' <- mapM (bimapM (inlineBinder axis) (inline' axis)) letSeq
-      e' <- inline' axis e
+      letSeq' <- mapM (bimapM (inlineBinder h) (inline' h)) letSeq
+      e' <- inline' h e
       return $ DT.Leaf xs letSeq' e'
     DT.Unreachable ->
       return DT.Unreachable
     DT.Switch (cursorVar, cursor) clauseList -> do
-      cursor' <- inline' axis cursor
-      clauseList' <- inlineCaseList axis clauseList
+      cursor' <- inline' h cursor
+      clauseList' <- inlineCaseList h clauseList
       return $ DT.Switch (cursorVar, cursor') clauseList'
 
 inlineCaseList ::
-  Axis ->
+  Handle ->
   DT.CaseList TM.Term ->
   App (DT.CaseList TM.Term)
-inlineCaseList axis (fallbackTree, clauseList) = do
-  fallbackTree' <- inlineDecisionTree axis fallbackTree
-  clauseList' <- mapM (inlineCase axis) clauseList
+inlineCaseList h (fallbackTree, clauseList) = do
+  fallbackTree' <- inlineDecisionTree h fallbackTree
+  clauseList' <- mapM (inlineCase h) clauseList
   return (fallbackTree', clauseList')
 
 inlineCase ::
-  Axis ->
+  Handle ->
   DT.Case TM.Term ->
   App (DT.Case TM.Term)
-inlineCase axis decisionCase = do
+inlineCase h decisionCase = do
   case decisionCase of
     DT.LiteralCase mPat i cont -> do
-      cont' <- inlineDecisionTree axis cont
+      cont' <- inlineDecisionTree h cont
       return $ DT.LiteralCase mPat i cont'
     DT.ConsCase record@(DT.ConsCaseRecord {..}) -> do
       let (dataTerms, dataTypes) = unzip dataArgs
-      dataTerms' <- mapM (inline' axis) dataTerms
-      dataTypes' <- mapM (inline' axis) dataTypes
+      dataTerms' <- mapM (inline' h) dataTerms
+      dataTypes' <- mapM (inline' h) dataTypes
       let (ms, xs, ts) = unzip3 consArgs
-      ts' <- mapM (inline' axis) ts
-      cont' <- inlineDecisionTree axis cont
+      ts' <- mapM (inline' h) ts
+      cont' <- inlineDecisionTree h cont
       return $
         DT.ConsCase
           record
