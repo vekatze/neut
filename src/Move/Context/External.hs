5d9f97cf4be781de128d9362a9f059a9c2b5f905
vekatze <vekatze@icloud.com>
2025-04-26 09:39:57 +0900

    cleanup

--- src/Move/Context/External.hs
+++ src/Move/Context/External.hs
@@ -4,32 +4,27 @@ module Move.Context.External
     runOrFail',
     expandText,
     ensureExecutable,
-    raiseIfProcessFailed,
     ExternalError (..),
   )
 where
 
 import Control.Monad.IO.Class
-import Data.ByteString qualified as B
 import Data.Text qualified as T
 import Data.Text.Encoding
-import GHC.IO.Handle
 import Move.Context.App
 import Move.Context.Debug qualified as Debug
 import Move.Context.EIO (toApp)
-import Move.Context.Throw (liftEither)
 import Move.Context.Throw qualified as Throw
 import Path
 import Rule.Error
 import Rule.ProcessRunner.Context.IO qualified as ProcessRunner (ioRunner)
 import Rule.ProcessRunner.Rule qualified as ProcessRunner
 import System.Directory
-import System.Exit
 import System.Process
 
 run :: String -> [String] -> App ()
 run procName optionList = do
-  runOrFail procName optionList >>= liftEither
+  runOrFail procName optionList >>= Throw.liftEither
 
 runOrFail :: String -> [String] -> App (Either Error ())
 runOrFail procName optionList = do
@@ -86,22 +81,3 @@ expandText t = do
       return $ decodeUtf8 value
     Left err ->
       Throw.throw $ ProcessRunner.toCompilerError err
-
-raiseIfProcessFailed :: T.Text -> ExitCode -> Handle -> App ()
-raiseIfProcessFailed procName exitCode h =
-  case exitCode of
-    ExitSuccess ->
-      return ()
-    ExitFailure i -> do
-      errStr <- liftIO $ decodeUtf8 <$> B.hGetContents h
-      Throw.raiseError' $
-        "The child process `"
-          <> procName
-          <> "` failed with the following message (exitcode = "
-          <> T.pack (show i)
-          <> "):\n"
-          <> indent errStr
-
-indent :: T.Text -> T.Text
-indent t =
-  T.intercalate "\n" $ map ("  " <>) $ T.splitOn "\n" t
