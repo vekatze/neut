0645f643c61b6ab0fce4983c8af4fa1ff0ca2e6e
vekatze <vekatze@icloud.com>
2026-04-22 13:21:22 +0900

    0xabc => 0xABC

--- src/Kernel/Parse/Internal/Util.hs
+++ src/Kernel/Parse/Internal/Util.hs
@@ -8,30 +8,36 @@ where
 
 import Data.Text qualified as T
 
+data NumeralSystem = NumeralSystem
+  { prefix :: T.Text,
+    base :: Integer,
+    digitTable :: [(Char, Integer)]
+  }
+
 readIntBinaryMaybe :: T.Text -> Maybe Integer
 readIntBinaryMaybe = do
-  readIntMaybe "0b" binaryMap
+  readIntMaybe binarySystem
 
 readIntOctalMaybe :: T.Text -> Maybe Integer
 readIntOctalMaybe = do
-  readIntMaybe "0o" octalMap
+  readIntMaybe octalSystem
 
 readIntDecimalMaybe :: T.Text -> Maybe Integer
 readIntDecimalMaybe = do
-  readIntMaybe "" decimalMap
+  readIntMaybe decimalSystem
 
 readIntHexadecimalMaybe :: T.Text -> Maybe Integer
 readIntHexadecimalMaybe = do
-  readIntMaybe "0x" hexadecimalMap
+  readIntMaybe hexadecimalSystem
 
-readIntMaybe :: T.Text -> [(Char, Integer)] -> T.Text -> Maybe Integer
-readIntMaybe prefix charMap t = do
+readIntMaybe :: NumeralSystem -> T.Text -> Maybe Integer
+readIntMaybe numeralSystem t = do
   (suffix, sign) <- readSign t
-  suffix' <- T.stripPrefix prefix suffix
+  suffix' <- T.stripPrefix (prefix numeralSystem) suffix
   if T.length suffix' <= 0
     then Nothing
     else do
-      v <- interpretAsInteger charMap 1 0 suffix'
+      v <- interpretAsInteger numeralSystem 1 0 suffix'
       return $ sign * v
 
 readSign :: T.Text -> Maybe (T.Text, Integer)
@@ -45,69 +51,89 @@ readSign t = do
     _ ->
       return (t, 1)
 
-binaryMap :: [(Char, Integer)]
-binaryMap =
-  [ ('0', 0),
-    ('1', 1)
-  ]
+binarySystem :: NumeralSystem
+binarySystem =
+  NumeralSystem
+    { prefix = "0b",
+      base = 2,
+      digitTable =
+        [ ('0', 0),
+          ('1', 1)
+        ]
+    }
 
-octalMap :: [(Char, Integer)]
-octalMap =
-  [ ('0', 0),
-    ('1', 1),
-    ('2', 2),
-    ('3', 3),
-    ('4', 4),
-    ('5', 5),
-    ('6', 6),
-    ('7', 7)
-  ]
+octalSystem :: NumeralSystem
+octalSystem =
+  NumeralSystem
+    { prefix = "0o",
+      base = 8,
+      digitTable =
+        [ ('0', 0),
+          ('1', 1),
+          ('2', 2),
+          ('3', 3),
+          ('4', 4),
+          ('5', 5),
+          ('6', 6),
+          ('7', 7)
+        ]
+    }
 
-decimalMap :: [(Char, Integer)]
-decimalMap =
-  [ ('0', 0),
-    ('1', 1),
-    ('2', 2),
-    ('3', 3),
-    ('4', 4),
-    ('5', 5),
-    ('6', 6),
-    ('7', 7),
-    ('8', 8),
-    ('9', 9)
-  ]
+decimalSystem :: NumeralSystem
+decimalSystem =
+  NumeralSystem
+    { prefix = "",
+      base = 10,
+      digitTable =
+        [ ('0', 0),
+          ('1', 1),
+          ('2', 2),
+          ('3', 3),
+          ('4', 4),
+          ('5', 5),
+          ('6', 6),
+          ('7', 7),
+          ('8', 8),
+          ('9', 9)
+        ]
+    }
 
-hexadecimalMap :: [(Char, Integer)]
-hexadecimalMap =
-  [ ('0', 0),
-    ('1', 1),
-    ('2', 2),
-    ('3', 3),
-    ('4', 4),
-    ('5', 5),
-    ('6', 6),
-    ('7', 7),
-    ('8', 8),
-    ('9', 9),
-    ('a', 10),
-    ('b', 11),
-    ('c', 12),
-    ('d', 13),
-    ('e', 14),
-    ('f', 15)
-  ]
+hexadecimalSystem :: NumeralSystem
+hexadecimalSystem =
+  NumeralSystem
+    { prefix = "0x",
+      base = 16,
+      digitTable =
+        [ ('0', 0),
+          ('1', 1),
+          ('2', 2),
+          ('3', 3),
+          ('4', 4),
+          ('5', 5),
+          ('6', 6),
+          ('7', 7),
+          ('8', 8),
+          ('9', 9),
+          ('A', 10),
+          ('B', 11),
+          ('C', 12),
+          ('D', 13),
+          ('E', 14),
+          ('F', 15)
+        ]
+    }
 
-interpretAsInteger :: [(Char, Integer)] -> Integer -> Integer -> T.Text -> Maybe Integer
-interpretAsInteger charMap base acc t = do
-  interpretAsInteger' charMap (fromIntegral (length charMap)) base acc t
+interpretAsInteger :: NumeralSystem -> Integer -> Integer -> T.Text -> Maybe Integer
+interpretAsInteger numeralSystem currentBase acc t = do
+  interpretAsInteger' numeralSystem currentBase acc t
 
-interpretAsInteger' :: [(Char, Integer)] -> Integer -> Integer -> Integer -> T.Text -> Maybe Integer
-interpretAsInteger' charMap charMapLen base acc t = do
+interpretAsInteger' :: NumeralSystem -> Integer -> Integer -> T.Text -> Maybe Integer
+interpretAsInteger' numeralSystem currentBase acc t = do
   case T.unsnoc t of
     Nothing ->
       Just acc
     Just (t', '_') -> do
-      interpretAsInteger' charMap charMapLen base acc t'
+      interpretAsInteger' numeralSystem currentBase acc t'
     Just (t', b) -> do
-      v <- lookup b charMap
-      interpretAsInteger' charMap charMapLen (charMapLen * base) (base * v + acc) t'
+      v <- lookup b (digitTable numeralSystem)
+      interpretAsInteger' numeralSystem (base numeralSystem * currentBase) (currentBase * v + acc) t'
